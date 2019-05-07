9e1c6bd70d43a1090ec88e3eb76c99a0080d5963
Audrius <nezinomas@gmail.com>
2019-05-07 14:33:42 +0300

    formating columns id admin

--- project/expenses/admin.py
+++ project/expenses/admin.py
@@ -1,10 +1,16 @@
+from django.conf.locale.en import formats as en_formats
 from django.contrib import admin
 
 from . import models
 
+en_formats.DATETIME_FORMAT = "Y-m-d H:i:s"
+en_formats.DATE_FORMAT = "Y-m-d"
+
 
 class ExpenseAdmin(admin.ModelAdmin):
-    pass
+    list_display = ('date', 'category', 'sub_category', 'quantity', 'account')
+    search_fields = ['category__title', 'sub_category__title', 'date']
+    # list_per_page = 25
 
 
 class ExpenseNameAdmin(admin.ModelAdmin):
