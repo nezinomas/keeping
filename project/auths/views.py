e17fd611de858e0794afbf458b1ea207b7bf392c
Audrius <nezinomas@gmail.com>
2019-05-09 12:06:21 +0300

    custom auth view where writing current year to session

--- project/auths/views.py
+++ project/auths/views.py
@@ -1,1 +1,13 @@
-# view
+from datetime import datetime
+
+from django.contrib.auth import login
+from django.contrib.auth import views as auth_views
+from django.http import HttpResponseRedirect
+
+
+class CustomLogin(auth_views.LoginView):
+    def form_valid(self, form):
+        login(self.request, form.get_user())
+        self.request.session['year'] = datetime.now().year
+
+        return HttpResponseRedirect(self.get_success_url())
