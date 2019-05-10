dbe109238826d29bf626eaa5ee4cdf6e8e38437b
Audrius <nezinomas@gmail.com>
2019-05-10 15:50:24 +0300

    Expense form

--- project/expenses/forms.py
+++ project/expenses/forms.py
@@ -1,8 +1,25 @@
+from bootstrap_datepicker_plus import DatePickerInput
+from crispy_forms.helper import FormHelper
 from django import forms
 
-from crispy_forms.helper import FormHelper
-
+from ..core.helpers.helper_forms import set_field_properties
 from .models import Expense, ExpenseName, ExpenseSubName
+
+
+class ExpenseForm(forms.ModelForm):
+    class Meta:
+        model = Expense
+        fields = '__all__'
+        widgets = {
+            'date': DatePickerInput(format='%Y-%m-%d'),
+            'checked': forms.HiddenInput(),
+        }
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        self.helper = FormHelper()
+        set_field_properties(self, self.helper)
 
 
 class ExpenseSubNameForm(forms.ModelForm):
