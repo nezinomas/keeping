cf9dd89c4624fe9a810140e17d84f8780774f3d7
Audrius <nezinomas@gmail.com>
2019-05-15 13:43:02 +0300

    views

--- project/expenses/views/expenses.py
+++ project/expenses/views/expenses.py
@@ -1,16 +1,25 @@
-from django.shortcuts import render
+from django.http import JsonResponse
+from django.shortcuts import render, reverse
+from django.template.loader import render_to_string
+
+from ..forms import ExpenseForm
+from ..helpers import helper_view_expenses as H_expenses
 from ..models import Expense
 
 
 def lists(request):
     qs = Expense.objects.all()
-    context = {'objects': qs}
+    form = ExpenseForm()
+    context = {'objects': qs, 'form': form}
 
     return render(request, 'expenses/expenses_list.html', context=context)
 
 
 def new(request):
-    pass
+    form = ExpenseForm(request.POST or None)
+    context = {'url': reverse('expenses:expenses_new')}
+
+    return H_expenses.save_data(request, context, form)
 
 
 def update(request, pk):
