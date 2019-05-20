c56d3095afc8068c065ef95f6ebfc56afd3651df
Audrius <nezinomas@gmail.com>
2019-05-20 16:10:58 +0300

    new class SaveDataMixin

--- project/expenses/helpers/helper_view_expenses.py
+++ project/expenses/helpers/helper_view_expenses.py
@@ -41,3 +41,64 @@ def save_data(request, context, form):
     })
 
     return JsonResponse(data)
+
+
+class SaveDataMixin(object):
+    def __init__(self, request, context, form):
+        self.__data = {}
+        self.__request = request
+        self.__context = context
+        self.__form = form
+
+        self.__context.update({'form': form})
+
+    @property
+    def form_template(self):
+        return self.__form_template
+
+    @form_template.setter
+    def form_template(self, value):
+        self.__form_template = value
+
+    @property
+    def items_template(self):
+        return self.__items_template
+
+    @items_template.setter
+    def items_template(self, value):
+        self.__items_template = value
+
+    @property
+    def items(self):
+        return self.__items
+
+    @items.setter
+    def items(self, value):
+        self.__items = value
+
+    def __update_data(self):
+        self.__data.update({
+            'form_is_valid': True,
+            'html_list': render_to_string(
+                self.__items_template,
+                {'objects': self.__items}
+            )
+        })
+
+    def GenJsonResponse(self):
+        if self.__request.method == 'POST':
+            if self.__form.is_valid():
+                self.__form.save()
+                self.__update_data()
+            else:
+                self.__data['form_is_valid'] = False
+
+        self.__data.update({
+            'html_form': render_to_string(
+                self.__form_template,
+                self.__context,
+                self.__request
+            )
+        })
+
+        return JsonResponse(self.__data)
