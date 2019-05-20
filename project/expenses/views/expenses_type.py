e1f7d3b4ded9d24c0d962f8bac0dc4dad3f15370
Audrius <nezinomas@gmail.com>
2019-05-20 16:48:53 +0300

    loads form and saves data form expenses type new

--- project/expenses/views/expenses_type.py
+++ project/expenses/views/expenses_type.py
@@ -1,4 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import render, reverse
+
+from ...core.mixins.save_data_mixin import SaveDataMixin
+from ..forms import ExpenseNameForm, ExpenseTypeForm
+from ..models import ExpenseName, ExpenseType
+
+
+def _items():
+    qs = ExpenseType.objects.all().prefetch_related('expensename_set')
+    return qs
+
+
+def _json_response(obj):
+    obj.form_template = 'expenses/includes/partial_expenses_type_form.html'
+    obj.items_template = 'expenses/includes/partial_expenses_type_list.html'
+    obj.items = _items()
+
+    return obj.GenJsonResponse()
 
 
 def lists(request):
@@ -6,7 +23,12 @@ def lists(request):
 
 
 def new(request):
-    pass
+    form = ExpenseTypeForm(request.POST or None)
+    context = {'url': reverse('expenses:expenses_type_new'), 'action': 'insert'}
+
+    obj = SaveDataMixin(request, context, form)
+
+    return _json_response(obj)
 
 
 def update(request, pk):
