833d10a66528ef266db32bcb4e3870ff9260b2c0
Audrius <nezinomas@gmail.com>
2019-05-21 12:31:32 +0300

    expense_name insert and update views

--- project/expenses/views/expenses_name.py
+++ project/expenses/views/expenses_name.py
@@ -1,9 +1,45 @@
-from django.shortcuts import render
+from django.shortcuts import render, reverse, get_object_or_404
+
+from ...core.mixins.save_data_mixin import SaveDataMixin
+from ..forms import ExpenseNameForm
+from ..models import ExpenseName
+
+
+def _items():
+    qs = ExpenseName.objects.all()
+    return qs
+
+
+def _json_response(obj):
+    obj.form_template = 'expenses/includes/partial_expenses_name_form.html'
+    obj.items_template = 'expenses/includes/partial_expenses_type_list.html'
+    obj.items_template_var_name = 'categories'
+    obj.items = _items()
+
+    return obj.GenJsonResponse()
 
 
 def new(request):
-    pass
+    form = ExpenseNameForm(request.POST or None)
+    context = {'url': reverse(
+        'expenses:expenses_name_new'), 'action': 'insert'}
+
+    obj = SaveDataMixin(request, context, form)
+
+    return _json_response(obj)
 
 
 def update(request, pk):
-    pass
+    object = get_object_or_404(ExpenseName, pk=pk)
+    form = ExpenseNameForm(request.POST or None, instance=object)
+    url = reverse(
+        'expenses:expenses_type_update',
+        kwargs={
+            'pk': pk
+        }
+    )
+    context = {'url': url, 'action': 'update'}
+
+    obj = SaveDataMixin(request, context, form)
+
+    return _json_response(obj)
