925d627633886579ced863cdf12003a473e1e173
Audrius <nezinomas@gmail.com>
2019-05-22 13:07:44 +0300

    accounts factory

---
+++ project/accounts/tests/factories.py
@@ -0,0 +1,11 @@
+import factory
+
+from ..models import Account
+
+
+class AccountFactory(factory.DjangoModelFactory):
+    class Meta:
+        model = Account
+        django_get_or_create = ('title',)
+
+    title = 'Account'
