48d64905094e6b86965f6f78f83a8e1a4aba3985
Audrius <nezinomas@gmail.com>
2019-05-22 09:50:07 +0300

    disable migrations

--- project/config/settings/testing.py
+++ project/config/settings/testing.py
@@ -8,3 +8,23 @@ DATABASES = {
 }
 
 EMAIL_BACKEND = 'django.core.mail.backends.locmem.EmailBackend'
+
+
+MIGRATION_MODULES = {
+    'auth': None,
+    'contenttypes': None,
+    'default': None,
+    'sessions': None,
+
+    'auths': None,
+    'accounts': None,
+    'bookkeeping': None,
+    'books': None,
+    'core': None,
+    'drinks': None,
+    'expenses': None,
+    'incomes': None,
+    'savings': None,
+    'plans': None,
+    'transactions': None
+}
