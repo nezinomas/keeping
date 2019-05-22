d188ecb50a3596b75142d724245f46ca030a078a
Audrius <nezinomas@gmail.com>
2019-05-22 09:50:37 +0300

    rewrited method for adding session

--- project/expenses/tests/helper_session.py
+++ project/expenses/tests/helper_session.py
@@ -1,12 +1,8 @@
-from django.contrib.sessions.middleware import SessionMiddleware
 
+def add_session(client, *args, **kwargs):
+    session = client.session
 
-def add_session_to_request(request, *args, **kwargs):
-    """Annotate a request object with a session"""
-    middleware = SessionMiddleware()
-    middleware.process_request(request)
+    for key, val in kwargs.items():
+        session[key] = val
 
-    if 'year' in kwargs:
-        request.session['year'] = kwargs['year']
-
-    request.session.save()
+    session.save()
