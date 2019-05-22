1717dd8ff3b2a0f4fb57bdddb8fc492fe1f65b51
Audrius <nezinomas@gmail.com>
2019-05-22 13:08:32 +0300

    new method in tests helper

--- project/expenses/tests/helper_session.py
+++ project/expenses/tests/helper_session.py
@@ -1,4 +1,5 @@
 
+from django.contrib.sessions.middleware import SessionMiddleware
 def add_session(client, *args, **kwargs):
     session = client.session
 
@@ -6,3 +7,14 @@ def add_session(client, *args, **kwargs):
         session[key] = val
 
     session.save()
+
+
+def add_session_to_request(request, *args, **kwargs):
+    """Annotate a request object with a session"""
+    middleware = SessionMiddleware()
+    middleware.process_request(request)
+
+    if 'year' in kwargs:
+        request.session['year'] = kwargs['year']
+
+    request.session.save()
