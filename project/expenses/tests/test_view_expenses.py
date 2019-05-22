d3924395fc8361882e496ddcc516e7199305ea55
Audrius <nezinomas@gmail.com>
2019-05-22 09:51:07 +0300

    new tests

--- project/expenses/tests/test_view_expenses.py
+++ project/expenses/tests/test_view_expenses.py
@@ -1,14 +1,43 @@
 import pytest
+from django.urls import resolve, reverse
 
+from ..models import ExpenseName
 from ..views import expenses
-from .helper_session import add_session_to_request
+from .factories import ExpenseNameFactory, ExpenseTypeFactory
+from .helper_session import add_session
 
 pytestmark = pytest.mark.django_db
 
 
-def test_load_expense_name_status_code(rf):
-    request = rf.get('/ajax/load_expense_name/?expense_type=1')
-    add_session_to_request(request, **{'year': 1999})
-    response = expenses.load_expense_name(request)
+@pytest.fixture(autouse=True)
+def db_data():
+    ExpenseTypeFactory.reset_sequence()
+    ExpenseNameFactory(title='F')
+    ExpenseNameFactory(title='S', valid_for=1999)
+
+
+def test_load_expense_name_status_code(client):
+    add_session(client, **{'year': 1999})
+
+    url = reverse('expenses:load_expense_name')
+    response = client.get(url, {'expense_type': 1})
 
     assert response.status_code == 200
+
+
+def test_load_expense_name_isnull_count(client):
+    add_session(client, **{'year': 1})
+
+    url = reverse('expenses:load_expense_name')
+    response = client.get(url, {'expense_type': 1})
+
+    assert 1 == response.context['objects'].count()
+
+
+def test_load_expense_name_all(client):
+    add_session(client, **{'year': 1999})
+
+    url = reverse('expenses:load_expense_name')
+    response = client.get(url, {'expense_type': 1})
+
+    assert 2 == response.context['objects'].count()
