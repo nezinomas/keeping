a6bb9a96d4e8c0d9628850b175b18252aff17c88
Audrius <nezinomas@gmail.com>
2019-05-22 13:08:06 +0300

    expense form tests

---
+++ project/expenses/tests/test_forms.py
@@ -0,0 +1,86 @@
+from datetime import date, datetime
+from decimal import Decimal
+
+import pytest
+
+from ...accounts.tests.factories import AccountFactory
+from .. import forms
+from .factories import ExpenseNameFactory, ExpenseTypeFactory
+from .helper_session import add_session, add_session_to_request
+
+pytestmark = pytest.mark.django_db
+
+
+@pytest.fixture()
+def _request(rf):
+    request = rf.get('/expenses/new/')
+    add_session_to_request(request, **{'year': 1999})
+    return request
+
+
+@pytest.fixture()
+def _expense_type():
+    ExpenseTypeFactory.reset_sequence()
+    return ExpenseTypeFactory()
+
+
+@pytest.fixture()
+def _expense_name():
+    return ExpenseNameFactory()
+
+
+@pytest.fixture()
+def _account():
+    return AccountFactory()
+
+
+def test_expense_form_init(client):
+    forms.ExpenseForm(request=client.request)
+
+
+@pytest.mark.xfail(raises=TypeError)
+def test_expense_form_init_without_request(client):
+    forms.ExpenseForm()
+
+
+def test_exepense_form_valid_data(_request, _expense_type, _expense_name, _account):
+    form = forms.ExpenseForm(
+        data={
+            'date': '1970-01-01',
+            'price': 1.12,
+            'quantity': 1,
+            'expense_type': 1,
+            'expense_name': 1,
+            'account': 1,
+            'remark': None,
+            'exception': None
+        },
+        request=_request
+    )
+
+    assert form.is_valid()
+
+    e = form.save()
+
+    assert e.date == date(1970, 1, 1)
+    assert e.price == round(Decimal(1.12), 2)
+    assert e.expense_type == _expense_type
+    assert e.expense_name == _expense_name
+    assert e.account == _account
+    assert e.quantity == 1
+
+
+def test_expenses_form_blank_data(_request):
+    form = forms.ExpenseForm(data={}, request=_request)
+
+    assert not form.is_valid()
+
+    errors = {
+        'date': ['This field is required.'],
+        'price': ['This field is required.'],
+        'quantity': ['This field is required.'],
+        'expense_type': ['This field is required.'],
+        'expense_name': ['This field is required.'],
+        'account': ['This field is required.'],
+    }
+    assert form.errors == errors
