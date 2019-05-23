a30ba757281c5fb481f351361cebc9316b6dbe1c
Audrius <nezinomas@gmail.com>
2019-05-23 13:15:56 +0300

    accounts form

---
+++ project/accounts/forms.py
@@ -0,0 +1,19 @@
+from crispy_forms.helper import FormHelper
+from django import forms
+
+from ..core.helpers.helper_forms import set_field_properties
+from .models import Account
+
+
+class AccountForm(forms.ModelForm):
+    class Meta:
+        model = Account
+        fields = ['title']
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        self.helper = FormHelper()
+        set_field_properties(self, self.helper)
+
+        self.fields['title'].label = 'Sąskaitos pavadinimas'
