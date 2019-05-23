00306364aaf7714c69a08303552611acc9c86779
Audrius <nezinomas@gmail.com>
2019-05-23 14:31:41 +0300

    generic_form in core upgrades

--- project/core/templatetags/form.py
+++ project/core/templatetags/form.py
@@ -3,10 +3,12 @@ from django import template
 register = template.Library()
 
 
-@register.inclusion_tag('expenses/includes/generic_form.html', takes_context=True)
-def generic_form(context, title):
+@register.inclusion_tag('core/includes/generic_form.html', takes_context=True)
+def generic_form(context, title, update_container):
     return {
         'title': title,
+        'update_container': update_container,
         'form': context['form'],
-        'submit_title': context['action']
+        'action': context['action'],
+        'url': context['url'],
     }
