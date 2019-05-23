6fca62f14c0f48402b8a6ee905d9ae7e6204bde4
Audrius <nezinomas@gmail.com>
2019-05-23 17:03:12 +0300

    upgraded generic_form templatetag

--- project/core/templatetags/form.py
+++ project/core/templatetags/form.py
@@ -4,11 +4,12 @@ register = template.Library()
 
 
 @register.inclusion_tag('core/includes/generic_form.html', takes_context=True)
-def generic_form(context, title, update_container):
+def generic_form(context, title, update_container, chained_dropdown=None):
     return {
         'title': title,
         'update_container': update_container,
         'form': context['form'],
         'action': context['action'],
         'url': context['url'],
+        'chained_dropdown': chained_dropdown
     }
