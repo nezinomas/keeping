3da525a8a4344575a7f35a4e4798cb798d75c116
Audrius <nezinomas@gmail.com>
2019-05-27 13:09:25 +0300

    books ulr and views

--- project/books/urls.py
+++ project/books/urls.py
@@ -1,7 +1,23 @@
 from django.urls import path
 
+from . import views
+
 app_name = 'books'
 
 urlpatterns = [
-
+    path(
+        'books/',
+        views.lists,
+        name='books_list'
+    ),
+    path(
+        'books/new/',
+        views.new,
+        name='books_new'
+    ),
+    path(
+        'books/update/<int:pk>/',
+        views.update,
+        name='books_update'
+    ),
 ]
