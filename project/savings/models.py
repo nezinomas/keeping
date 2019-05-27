2ce025912c024dae1e0153e99af1afc1fa6b81d6
Audrius <nezinomas@gmail.com>
2019-05-27 09:53:23 +0300

    saving models and migrations

--- project/savings/models.py
+++ project/savings/models.py
@@ -1,3 +1,45 @@
+from decimal import Decimal
+
+from django.core.validators import MinValueValidator
 from django.db import models
 
-# Create your models here.
+from ..accounts.models import Account
+from ..core.models import TitleAbstract
+
+
+class SavingType(TitleAbstract):
+    class Meta:
+        ordering = ['title']
+
+
+class Saving(models.Model):
+    date = models.DateField()
+    amount = models.DecimalField(
+        max_digits=8,
+        decimal_places=2,
+        validators=[MinValueValidator(Decimal('0.01'))]
+    )
+    fee = models.DecimalField(
+        max_digits=8,
+        decimal_places=2,
+        null=True,
+        blank=True,
+    )
+    remark = models.TextField(
+        max_length=1000,
+        blank=True
+    )
+    saving_type = models.ForeignKey(
+        SavingType,
+        on_delete=models.CASCADE
+    )
+    account = models.ForeignKey(
+        Account,
+        on_delete=models.CASCADE
+    )
+
+    class Meta:
+        ordering = ['-date', 'saving_type']
+
+    def __str__(self):
+        return str(self.saving_type)
