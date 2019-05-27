43a4a5704cd67ea2fe965d8ad835d35b5ae07f1d
Audrius <nezinomas@gmail.com>
2019-05-27 13:04:33 +0300

    books model and migrations

--- project/books/models.py
+++ project/books/models.py
@@ -1,3 +1,24 @@
 from django.db import models
+from django.core.validators import MinValueValidator
 
-# Create your models here.
+
+class Book(models.Model):
+    started = models.DateField()
+    ended = models.DateField(
+        null=True,
+        blank=True
+    )
+    author = models.CharField(
+        max_length=254,
+        validators=[MinValueValidator(3)]
+    )
+    title = models.CharField(
+        max_length=254,
+        validators=[MinValueValidator(3)]
+    )
+
+    class Meta:
+        ordering = ['-started', 'author']
+
+    def __str__(self):
+        return str(self.title)
