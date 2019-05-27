e2874cfe70adb2c80e4280f0857da0596514282f
Audrius <nezinomas@gmail.com>
2019-05-27 10:42:02 +0300

    savings and saving_type list views

--- project/savings/views.py
+++ project/savings/views.py
@@ -8,8 +8,35 @@ from .forms import SavingForm, SavingTypeForm
 
 
 # Saving views
+def _items(request):
+    qs = (
+        Saving.objects.
+        filter(date__year=request.session['year']).
+        prefetch_related('account', 'saving_type')
+    )
+    return qs
+
+
+def _json_response(request, obj):
+    obj.form_template = 'savings/includes/partial_savings_form.html'
+    obj.items_template = 'savings/includes/partial_savings_list.html'
+
+    obj.items = _items(request)
+
+    return obj.GenJsonResponse()
+
+
 def lists(request):
-    pass
+    qs = _items(request)
+
+    form = SavingForm()
+    context = {
+        'objects': qs,
+        'categories': type_lists(request),
+        'form': form
+    }
+
+    return render(request, 'savings/savings_list.html', context=context)
 
 
 def new(request):
@@ -21,8 +48,27 @@ def update(request, pk):
 
 
 # Saving Type views
+def _type_items():
+    return SavingType.objects.all()
+
+
+def _type_json_response(obj):
+    obj.form_template = 'savings/includes/partial_savings_type_form.html'
+    obj.items_template = 'savings/includes/partial_savings_type_list.html'
+
+    obj.items_template_var_name = 'categories'
+    obj.items = _type_items()
+
+    return obj.GenJsonResponse()
+
+
 def type_lists(request):
-    pass
+    qs = _type_items()
+    return render_to_string(
+        'savings/includes/partial_savings_type_list.html',
+        {'categories': qs},
+        request,
+    )
 
 
 def type_new(request):
