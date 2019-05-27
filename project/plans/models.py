1892d881c1eb688983c071dd89d3e5fbdf2ad07e
Audrius <nezinomas@gmail.com>
2019-05-27 16:14:40 +0300

    plans models and migrations

--- project/plans/models.py
+++ project/plans/models.py
@@ -1,3 +1,56 @@
+from django.core.validators import MaxValueValidator, MinValueValidator
 from django.db import models
 
-# Create your models here.
+from ..core.models import MonthAbstract
+from ..expenses.models import ExpenseType
+from ..incomes.models import IncomeType
+from ..savings.models import SavingType
+
+
+class ExpenseTypePlan(MonthAbstract):
+    year = models.PositiveIntegerField(
+        validators=[MinValueValidator(2000), MaxValueValidator(2050)]
+    )
+    expense_type = models.ForeignKey(
+        ExpenseType,
+        on_delete=models.CASCADE
+    )
+
+    class Meta:
+        ordering = ['expense_type']
+        unique_together = ('year', 'expense_type')
+
+
+class SavingPlan(MonthAbstract):
+    year = models.PositiveIntegerField(
+        validators=[MinValueValidator(2000), MaxValueValidator(2050)]
+    )
+    saving_type = models.ForeignKey(
+        SavingType,
+        on_delete=models.CASCADE
+    )
+
+    class Meta:
+        ordering = ['saving_type']
+        unique_together = ('year', 'saving_type')
+
+
+class IncomePlan(MonthAbstract):
+    year = models.PositiveIntegerField(
+        validators=[MinValueValidator(2000), MaxValueValidator(2050)]
+    )
+    income_type = models.ForeignKey(
+        IncomeType,
+        on_delete=models.CASCADE
+    )
+
+    class Meta:
+        ordering = ['income_type']
+        unique_together = ('year', 'income_type')
+
+
+class DayPlan(MonthAbstract):
+    year = models.PositiveIntegerField(
+        validators=[MinValueValidator(2000), MaxValueValidator(2050)],
+        unique=True
+    )
