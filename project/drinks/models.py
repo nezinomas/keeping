7f4c11420a094cd8344e9df03b1fa9ed76e67016
Audrius <nezinomas@gmail.com>
2019-05-27 13:44:03 +0300

    drinks model and migrations

--- project/drinks/models.py
+++ project/drinks/models.py
@@ -1,3 +1,9 @@
+from django.core.validators import MinValueValidator
 from django.db import models
 
-# Create your models here.
+
+class Drink(models.Model):
+    date = models.DateField()
+    quantity = models.IntegerField(
+        validators=[MinValueValidator(1)]
+    )
