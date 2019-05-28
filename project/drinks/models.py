e46528579edd0ea221e78f2f3fcf994ed5cd44ad
Audrius <nezinomas@gmail.com>
2019-05-28 13:45:16 +0300

    DrinkManager

--- project/drinks/models.py
+++ project/drinks/models.py
@@ -2,8 +2,15 @@ from django.core.validators import MinValueValidator
 from django.db import models
 
 
+class DrinkManager(models.Manager):
+    def items(self, year):
+        return self.get_queryset().filter(date__year=year)
+
+
 class Drink(models.Model):
     date = models.DateField()
     quantity = models.IntegerField(
         validators=[MinValueValidator(1)]
     )
+
+    objects = DrinkManager()
