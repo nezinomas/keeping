ae6de9367dae17c0b89a971955b62f7734fce9a0
Audrius <nezinomas@gmail.com>
2019-05-30 13:47:28 +0300

    using crud_views_mixin

--- project/drinks/views.py
+++ project/drinks/views.py
@@ -1,54 +1,37 @@
 from django.shortcuts import get_object_or_404, render, reverse
 
-from ..core.mixins.save_data_mixin import SaveDataMixin
+from ..core.mixins.crud_views_mixin import CrudMixin, CrudMixinSettings
 from .forms import DrinkForm
 from .models import Drink
 
 
-def _json_response(request, obj):
+def settings():
+    obj = CrudMixinSettings()
+
+    obj.model = Drink
+
+    obj.form = DrinkForm
     obj.form_template = 'drinks/includes/partial_drinks_form.html'
+
     obj.items_template = 'drinks/includes/partial_drinks_list.html'
+    obj.items_template_main = 'drinks/drinks_list.html'
 
-    obj.items = Drink.objects.items(request.user.profile.year)
+    obj.url_new = 'drinks:drinks_new'
+    obj.url_update = 'drinks:drinks_update'
 
-    return obj.GenJsonResponse()
+    return obj
 
 
 def lists(request):
-    qs = Drink.objects.items(request.user.profile.year)
-
-    form = DrinkForm()
-    context = {
-        'objects': qs,
-        'form': form
-    }
-
-    return render(request, 'drinks/drinks_list.html', context=context)
+    return CrudMixin(request, settings()).lists_as_html()
 
 
 def new(request):
-    form = DrinkForm(request.POST or None)
-    context = {
-        'url': reverse('drinks:drinks_new'),
-        'action': 'insert'
-    }
-
-    obj = SaveDataMixin(request, context, form)
-
-    return _json_response(request, obj)
+    return CrudMixin(request, settings()).new()
 
 
 def update(request, pk):
-    object = get_object_or_404(Drink, pk=pk)
-    form = DrinkForm(request.POST or None, instance=object)
-    url = reverse(
-        'drinks:drinks_update',
-        kwargs={
-            'pk': pk
-        }
-    )
-    context = {'url': url, 'action': 'update'}
+    _settings = settings()
+    _settings.item_id = pk
 
-    obj = SaveDataMixin(request, context, form)
-
-    return _json_response(request, obj)
+    return CrudMixin(request, _settings).update()
