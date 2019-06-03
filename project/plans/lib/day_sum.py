ef48e7bc31d6801bfa1c5acf02e85cb5fda99fee
Audrius <nezinomas@gmail.com>
2019-06-03 17:31:09 +0300

    DaySum class and tests

--- project/plans/lib/day_sum.py
+++ project/plans/lib/day_sum.py
@@ -1,6 +1,84 @@
+from calendar import monthrange
+from decimal import Decimal
+from time import strptime
+
 import pandas as pd
 from django_pandas.io import read_frame
 
+from ..models import ExpensePlan, ExpenseType, IncomePlan
+
 
 class DaySum(object):
-    pass
+    def __init__(self, year):
+        self._year = year
+
+        self._incomes = self._get_incomes().sum()
+        self._expenses = self._get_expenses()
+        self._expenses_necessary = self._get_expenses_necessary()
+
+        self._expenses_necessary_sum = self._calc_expenses_necessary_sum()
+        self._expenses_free = self._calc_expenses_free()
+        self._day_sum = self._calc_day_sum()
+
+    @property
+    def incomes(self):
+        return self._incomes.to_dict()
+
+    @property
+    def expenses_necessary(self):
+        return self._expenses_necessary.to_dict()
+
+    @property
+    def expenses_necessary_sum(self):
+        return self._expenses_necessary_sum.to_dict()
+
+    @property
+    def expenses_free(self):
+        return self._expenses_free.to_dict()
+
+    @property
+    def day_sum(self):
+        return self._day_sum
+
+    def _get_incomes(self):
+        qs = IncomePlan.objects.items(**{'year': self._year})
+        df = read_frame(qs)
+        df = df.reset_index(drop=True).set_index('income_type')
+
+        return df
+
+    def _get_expenses(self):
+        qs = ExpensePlan.objects.items(**{'year': self._year})
+        df = read_frame(qs)
+        df = df.reset_index(drop=True).set_index('expense_type')
+
+        return df
+
+    def _get_expenses_necessary(self):
+        qs = ExpenseType.objects.filter(necessary=True).values_list('title', flat=True)
+
+        return list(qs)
+
+    def _calc_expenses_necessary_sum(self):
+        df = self._expenses.loc[self._expenses_necessary, :]
+
+        return df.sum()
+
+    def _calc_expenses_free(self):
+        df = self._incomes.sub(self._expenses_necessary_sum, axis='rows')
+
+        return df
+
+    def _calc_day_sum(self):
+        df = self._incomes.sub(self._expenses_necessary_sum, axis='rows').to_dict()
+
+        for k, v in df.items():
+            df[k] = v / self._month(k)
+
+        return df
+
+    def _month(self, word):
+        new = word[0].upper() + word[1:3].lower()
+        num = strptime(new, '%b').tm_mon
+
+        return monthrange(self._year, num)[1]
--- project/plans/tests/test_lib_day_sum.py
+++ project/plans/tests/test_lib_day_sum.py
@@ -1,16 +1,93 @@
-import pytest
+from decimal import Decimal
+
 import pandas as pd
+import pytest
 
 from ..lib.day_sum import DaySum
 
 
-def expense_plans():
-    data = {
+def _round(number):
+    return round(number, 2)
 
+
+def incomes():
+    data = {
+        'income_type': ['a', 'b'],
+        'january': [100.01, 200.02],
+        'february': [100.01, 200.02],
     }
-    return pd.DataFrame(data)
+    return pd.DataFrame(data).set_index('income_type')
+
+
+def expenses_necessary():
+    return ['necessary1', 'necessary2']
+
+
+def expenses():
+    data = {
+        'expense_type': ['ordinary1', 'necessary1', 'ordinary2', 'necessary2'],
+        'january': [10.01, 20.02, 30.03, 40.04],
+        'february': [10.01, 20.02, 30.03, 40.04],
+    }
+    return pd.DataFrame(data).set_index('expense_type')
 
 
 @pytest.fixture(autouse=True)
-def mock_get_expense_plans(monkeypatch, request):
-    monkeypatch.setattr(DaySum, '_DaySum_get_expense_plans', lambda x: expense_plans())
+def mock_get_incomes(monkeypatch, request):
+    monkeypatch.setattr(
+        DaySum,
+        '_get_incomes',
+        lambda x: incomes()
+    )
+
+
+@pytest.fixture(autouse=True)
+def mock_get_expenses(monkeypatch, request):
+    monkeypatch.setattr(
+        DaySum,
+        '_get_expenses',
+        lambda x: expenses()
+    )
+
+
+@pytest.fixture(autouse=True)
+def mock_get_expenses_necessary(monkeypatch, request):
+    monkeypatch.setattr(
+        DaySum,
+        '_get_expenses_necessary',
+        lambda x: expenses_necessary()
+    )
+
+
+def test_expenses_necessary_sum():
+    actual = DaySum(1970).expenses_necessary_sum
+
+    assert 60.06 == actual['january']
+
+
+def test_incomes_sum():
+    actual = DaySum(1970).incomes
+    print('----------', actual)
+
+    assert 300.03 == _round(actual['january'])
+
+
+def test_expenses_free():
+    actual = DaySum(1970).expenses_free
+
+    assert 239.97 == _round(actual['january'])
+
+
+def test_day_sum1():
+    actual = DaySum(1970).day_sum
+
+    assert 7.74 == _round(actual['january'])
+    assert 8.57 == _round(actual['february'])
+
+
+# keliemeji metai
+def test_day_sum2():
+    actual = DaySum(2020).day_sum
+
+    assert 7.74 == _round(actual['january'])
+    assert 8.27 == _round(actual['february'])
