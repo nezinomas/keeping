3fba4ddf85ca31346eff5574fbf106c2a9f50f53
Audrius <nezinomas@gmail.com>
2019-06-11 11:03:27 +0300

    cell_decimal templatetag can change cell width from main template

--- project/core/templatetags/cell_decimal.py
+++ project/core/templatetags/cell_decimal.py
@@ -4,7 +4,8 @@ register = template.Library()
 
 
 @register.inclusion_tag('core/includes/cell_decimal.html')
-def td(value):
+def td(value, width='7%'):
     return {
         'value': value,
+        'width': width,
     }
