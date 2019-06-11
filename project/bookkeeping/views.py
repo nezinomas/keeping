16f52d8b19ea0560722a1365f238eadb283e53b5
Audrius <nezinomas@gmail.com>
2019-06-11 10:46:12 +0300

    balance table in view

--- project/bookkeeping/views.py
+++ project/bookkeeping/views.py
@@ -1,7 +1,19 @@
-from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
+from django.shortcuts import render
+
+from ..accounts.models import Account
+from ..expenses.models import Expense
+from ..incomes.models import Income
+from ..savings.models import Saving, SavingType
+from ..transactions.models import Transaction
+from .lib.get_data import GetObjects
+from .lib.stats_accounts import StatsAccounts
 
 
 @login_required()
 def index(request):
-    return render(request, 'bookkeeping/main.html')
+    objects = GetObjects([Account, Income, Expense, Saving, Transaction])
+    stats = StatsAccounts(request.user.profile.year, objects.data)
+    context = {'tbl': stats.balance}
+
+    return render(request, 'bookkeeping/main.html', context=context)
