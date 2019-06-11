b296b1a24af1520ce3330bdb499073be9141f253
Audrius <nezinomas@gmail.com>
2019-06-11 10:52:47 +0300

    renamed template tag: partial_generic_list_cell -> cell_decimal

--- project/core/templatetags/cell_decimal.py
+++ project/core/templatetags/cell_decimal.py
@@ -3,7 +3,7 @@ from django import template
 register = template.Library()
 
 
-@register.inclusion_tag('core/includes/partial_generic_list_cell.html')
+@register.inclusion_tag('core/includes/cell_decimal.html')
 def td(value):
     return {
         'value': value,
