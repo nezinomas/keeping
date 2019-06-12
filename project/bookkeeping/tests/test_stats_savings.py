b17a54a13c873a92d839a58a01bb6d97bfe1dee6
Audrius <nezinomas@gmail.com>
2019-06-12 10:18:03 +0300

    StatsSavings test

---
+++ project/bookkeeping/tests/test_stats_savings.py
@@ -0,0 +1,24 @@
+from ..lib.stats_savings import StatsSavings
+
+
+def test_savings_year_now(_data_savings):
+    actual = StatsSavings(1999, _data_savings).balance
+
+    expect = {
+        'Saving1': {
+            'past_amount': 1000.0,
+            'past_fee': 145.68,
+            'incomes': 500.0,
+            'fees': 30.5,
+            'invested': 469.5,
+        },
+        'Saving2': {
+            'past_amount': 100.0,
+            'past_fee': 15.5,
+            'incomes': 300,
+            'fees': 15.5,
+            'invested': 284.5,
+        },
+    }
+
+    assert expect == actual
