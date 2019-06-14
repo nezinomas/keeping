a53fa7ecb834ca9136c95fb71f6519b24e9d0733
Audrius <nezinomas@gmail.com>
2019-06-14 12:04:43 +0300

    deleted custom manager in accounts model

--- project/accounts/models.py
+++ project/accounts/models.py
@@ -3,13 +3,6 @@ from django.db import models
 from ..core.models import TitleAbstract
 
 
-class AccountManager(models.Manager):
-    def items(self, *args, **kwargs):
-        return self.get_queryset()
-
-
 class Account(TitleAbstract):
-    objects = AccountManager()
-
     class Meta:
         ordering = ['title']
