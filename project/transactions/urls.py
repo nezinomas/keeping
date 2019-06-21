ecef3cb0074c55fc22514c8d61458dd8f4576cf1
Audrius <nezinomas@gmail.com>
2019-06-21 16:58:15 +0300

    savings_close and savings_change urls and views

--- project/transactions/urls.py
+++ project/transactions/urls.py
@@ -7,6 +7,11 @@ app_name = 'transactions'
 urlpatterns = [
     path(
         'transactions/',
+        views.index,
+        name='transactions_index'
+    ),
+    path(
+        'transactions/lists/',
         views.lists,
         name='transactions_list'
     ),
@@ -19,5 +24,25 @@ urlpatterns = [
         'transactions/update/<int:pk>/',
         views.update,
         name='transactions_update'
+    ),
+    path(
+        'savings_close/new/',
+        views.savings_close_new,
+        name='savings_close_new'
+    ),
+    path(
+        'savings_close/update/<int:pk>/',
+        views.savings_close_update,
+        name='savings_close_update'
+    ),
+    path(
+        'savings_change/new/',
+        views.savings_close_new,
+        name='savings_change_new'
+    ),
+    path(
+        'savings_change/update/<int:pk>/',
+        views.savings_close_update,
+        name='savings_change_update'
     ),
 ]
--- project/transactions/views.py
+++ project/transactions/views.py
@@ -5,7 +5,7 @@ from django.template.loader import render_to_string
 from ..accounts.views import lists as accounts_list
 from ..core.mixins.crud_views_mixin import CrudMixin, CrudMixinSettings
 from .forms import TransactionForm
-from .models import Transaction
+from .models import Transaction, SavingChange, SavingClose
 
 
 def settings():
@@ -26,9 +26,17 @@ def settings():
 
 
 @login_required()
-def lists(request):
-    context = {'categories': accounts_list(request)}
+def index(request):
+    context = {
+        'categories': accounts_list(request),
+        'transactions': lists(request),
+    }
     return CrudMixin(request, settings()).lists_as_html(context)
+
+
+@login_required()
+def lists(request):
+    return CrudMixin(request, settings()).lists_as_str()
 
 
 @login_required()
@@ -39,6 +47,82 @@ def new(request):
 @login_required()
 def update(request, pk):
     _settings = settings()
+    _settings.item_id = pk
+
+    return CrudMixin(request, _settings).update()
+
+
+#
+# Savings Transactions from Savings to regular Account
+#
+def close_settings():
+    obj = CrudMixinSettings()
+
+    obj.model = SavingClose
+
+    # obj.form = TransactionForm
+    # obj.form_template = 'transactions/includes/partial_transactions_form.html'
+
+    # obj.items_template = 'transactions/includes/partial_transactions_list.html'
+    # obj.items_template_main = 'transactions/transactions_list.html'
+
+    # obj.url_new = 'transactions:transactions_new'
+    # obj.url_update = 'transactions:transactions_update'
+
+    return obj
+
+
+@login_required()
+def savings_close_lists(request):
+    return CrudMixin(request, close_settings()).lists_as_str()
+
+
+@login_required()
+def savings_close_new(request):
+    return CrudMixin(request, close_settings()).new()
+
+
+@login_required()
+def savings_close_update(request, pk):
+    _settings = close_settings()
+    _settings.item_id = pk
+
+    return CrudMixin(request, _settings).update()
+
+
+#
+# Savings Transactions betwenn Savings accounts
+#
+def change_settings():
+    obj = CrudMixinSettings()
+
+    obj.model = SavingChange
+
+    # obj.form = TransactionForm
+    # obj.form_template = 'transactions/includes/partial_transactions_form.html'
+
+    # obj.items_template = 'transactions/includes/partial_transactions_list.html'
+    # obj.items_template_main = 'transactions/transactions_list.html'
+
+    # obj.url_new = 'transactions:transactions_new'
+    # obj.url_update = 'transactions:transactions_update'
+
+    return obj
+
+
+@login_required()
+def savings_change_lists(request):
+    return CrudMixin(request, change_settings()).lists_as_str()
+
+
+@login_required()
+def savings_change_new(request):
+    return CrudMixin(request, change_settings()).new()
+
+
+@login_required()
+def savings_change_update(request, pk):
+    _settings = change_settings()
     _settings.item_id = pk
 
     return CrudMixin(request, _settings).update()
