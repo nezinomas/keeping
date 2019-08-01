d73096ff3a6923c47eb11e84e1e9bea8f6d96db4
Audrius <nezinomas@gmail.com>
2019-08-01 16:26:38 +0300

    refactored tests with custom assert method

--- project/bookkeeping/tests/test_stats_savings.py
+++ project/bookkeeping/tests/test_stats_savings.py
@@ -3,10 +3,13 @@ import pytest
 from ..lib.stats_savings import StatsSavings
 
 
-def round_(_list):
-    for key, arr in _list.items():
-        for _k, _v in _list[key].items():
-            _list[key][_k] = round(_v, 2)
+def assert_(expected, actual):
+    for saving_type, arr in expected.items():
+        for _k, expected_val in expected[saving_type].items():
+            actual_val = round(actual[saving_type][_k], 2)
+            msg = f'{saving_type}->{_k}. Expected={expected_val} Actual={actual_val}'
+
+            assert expected_val == actual_val, msg
 
 
 def test_savings_year_now(_data_savings):
@@ -28,9 +31,8 @@ def test_savings_year_now(_data_savings):
     }
 
     actual = StatsSavings(1999, _data_savings).balance
-    round_(actual)
 
-    assert expect == actual
+    assert_(expect, actual)
 
 
 def test_savings_year_past(_data_savings):
@@ -53,7 +55,7 @@ def test_savings_year_past(_data_savings):
         },
     }
 
-    assert expect == actual
+    assert_(expect, actual)
 
 
 def test_savings_change_only(_savings_change):
@@ -76,7 +78,7 @@ def test_savings_change_only(_savings_change):
         },
     }
 
-    assert expect == actual
+    assert_(expect, actual)
 
 
 def test_savings_cloce_only(_data_savings_close):
@@ -85,21 +87,21 @@ def test_savings_cloce_only(_data_savings_close):
     expect = {
         'Saving1': {
             'past_amount': -0.25,
-            'past_fee': 0.00,
-            'incomes': -0.50,
-            'fees': 0.00,
-            'invested': -0.50,
+            'past_fee': 0.0,
+            'incomes': -0.5,
+            'fees': 0.0,
+            'invested': -0.5,
         },
         'Saving2': {
-            'past_amount': 0.00,
-            'past_fee': 0.00,
-            'incomes': 0.00,
-            'fees': 0.00,
-            'invested': 0.00,
+            'past_amount': 0.0,
+            'past_fee': 0.0,
+            'incomes': 0.0,
+            'fees': 0.0,
+            'invested': 0.0,
         },
     }
 
-    assert expect == actual
+    assert_(expect, actual)
 
 
 def test_savings_empty():
