816e0efdb9a25ff24410f90c40286f0401597a91
Audrius <nezinomas@gmail.com>
2019-08-02 12:38:29 +0300

    refactored assert_ method: try convert dictionary value to float and round it

--- project/bookkeeping/tests/test_stats_savings.py
+++ project/bookkeeping/tests/test_stats_savings.py
@@ -6,7 +6,13 @@ from ..lib.stats_savings import StatsSavings
 def assert_(expected, actual):
     for saving_type, arr in expected.items():
         for _k, expected_val in expected[saving_type].items():
-            actual_val = round(actual[saving_type][_k], 2)
+            actual_val = actual[saving_type][_k]
+
+            try:
+                actual_val = round(float(actual_val), 2)
+            except:
+                pass
+
             msg = f'{saving_type}->{_k}. Expected={expected_val} Actual={actual_val}'
 
             assert expected_val == actual_val, msg
