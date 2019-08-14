2f397a5213a25f7b1278f90eceeec7ba7f3bac01
Audrius <nezinomas@gmail.com>
2019-08-14 14:27:07 +0300

    refactored accounts.views

--- project/accounts/views.py
+++ project/accounts/views.py
@@ -1,35 +1,28 @@
-from django.shortcuts import get_object_or_404, render, reverse
-from django.template.loader import render_to_string
+from django.shortcuts import render
 
-from ..core.mixins.ajax import AjaxCreateUpdateMixin
-from ..core.mixins.crud import CreateMixin, ListMixin, UpdateMixin
-from .forms import AccountForm
-from .models import Account
-
-
-class AccountMixin():
-    model = Account
-    form_class = AccountForm
-    template_name = 'accounts/includes/partial_accounts_form.html'
+from ..core.mixins.crud import CreateAjaxMixin, ListMixin, UpdateAjaxMixin
+from . import forms, models
 
 
 class Lists(ListMixin):
-    model = Account
+    model = models.Account
     template_name = 'accounts/includes/partial_accounts_list.html'
     context_object_name = 'categories'
 
 
-class New(AccountMixin, AjaxCreateUpdateMixin, CreateMixin):
-    pass
+class New(CreateAjaxMixin):
+    model = models.Account
+    form_class = forms.AccountForm
 
 
-class Update(AccountMixin, AjaxCreateUpdateMixin, UpdateMixin):
-    pass
+class Update(UpdateAjaxMixin):
+    model = models.Account
+    form_class = forms.AccountForm
 
 
 def load_to_account(request):
     id = request.GET.get('id')
-    objects = Account.objects.exclude(pk=id)
+    objects = models.Account.objects.exclude(pk=id)
     return render(
         request,
         'core/dropdown.html',
