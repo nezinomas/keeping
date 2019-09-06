9ebc766e42fa772db935bc2af9944898c3c0f1d8
Audrius <nezinomas@gmail.com>
2019-09-06 17:24:46 +0300

    refactoring tests

--- project/conftest.py
+++ project/conftest.py
@@ -1,7 +1,8 @@
-from datetime import date as dt
-from datetime import datetime as dtt
+from datetime import date, datetime
 
+import mock
 import pytest
+import pytz
 
 from .accounts.factories import AccountFactory
 from .bookkeeping.factories import AccountWorthFactory, SavingWorthFactory
@@ -31,32 +32,32 @@ def login(client, user):
 def incomes():
     IncomeFactory(
         price=5.25,
-        date=dt(1970, 1, 1),
+        date=date(1970, 1, 1),
         account=AccountFactory(title='Account1')
     )
     IncomeFactory(
         price=2.25,
-        date=dt(1970, 1, 1),
+        date=date(1970, 1, 1),
         account=AccountFactory(title='Account2')
     )
     IncomeFactory(
         price=2.25,
-        date=dt(1970, 1, 31),
+        date=date(1970, 1, 31),
         account=AccountFactory(title='Account2')
     )
     IncomeFactory(
         price=3.25,
-        date=dt(1999, 1, 1),
+        date=date(1999, 1, 1),
         account=AccountFactory(title='Account1')
     )
     IncomeFactory(
         price=2.25,
-        date=dt(1999, 1, 1),
+        date=date(1999, 1, 1),
         account=AccountFactory(title='Account2')
     )
     IncomeFactory(
         price=1.25,
-        date=dt(1999, 1, 31),
+        date=date(1999, 1, 31),
         account=AccountFactory(title='Account2')
     )
 
@@ -64,32 +65,32 @@ def incomes():
 @pytest.fixture()
 def expenses():
     ExpenseFactory(
-        date=dt(1999, 1, 1),
+        date=date(1999, 1, 1),
         price=0.25,
         account=AccountFactory(title='Account1')
     )
     ExpenseFactory(
-        date=dt(1999, 1, 1),
+        date=date(1999, 1, 1),
         price=0.25,
         account=AccountFactory(title='Account1')
     )
     ExpenseFactory(
-        date=dt(1999, 1, 1),
+        date=date(1999, 1, 1),
         price=1.25,
         account=AccountFactory(title='Account2')
     )
     ExpenseFactory(
-        date=dt(1970, 1, 1),
+        date=date(1970, 1, 1),
         price=1.25,
         account=AccountFactory(title='Account1')
     )
     ExpenseFactory(
-        date=dt(1970, 1, 1),
+        date=date(1970, 1, 1),
         price=1.25,
         account=AccountFactory(title='Account1')
     )
     ExpenseFactory(
-        date=dt(1970, 1, 1),
+        date=date(1970, 1, 1),
         price=2.25,
         account=AccountFactory(title='Account2')
     )
@@ -98,31 +99,31 @@ def expenses():
 @pytest.fixture()
 def transactions():
     TransactionFactory(
-        date=dt(1999, 1, 1),
+        date=date(1999, 1, 1),
         price=2.25,
         to_account=AccountFactory(title='Account2'),
         from_account=AccountFactory(title='Account1')
     )
     TransactionFactory(
-        date=dt(1999, 1, 1),
+        date=date(1999, 1, 1),
         price=2.25,
         to_account=AccountFactory(title='Account2'),
         from_account=AccountFactory(title='Account1')
     )
     TransactionFactory(
-        date=dt(1999, 1, 1),
+        date=date(1999, 1, 1),
         price=3.25,
         to_account=AccountFactory(title='Account1'),
         from_account=AccountFactory(title='Account2')
     )
     TransactionFactory(
-        date=dt(1970, 1, 1),
+        date=date(1970, 1, 1),
         price=1.25,
         to_account=AccountFactory(title='Account2'),
         from_account=AccountFactory(title='Account1')
     )
     TransactionFactory(
-        date=dt(1970, 1, 1),
+        date=date(1970, 1, 1),
         price=5.25,
         to_account=AccountFactory(title='Account1'),
         from_account=AccountFactory(title='Account2')
@@ -132,35 +133,35 @@ def transactions():
 @pytest.fixture()
 def savings():
     SavingFactory(
-        date=dt(1970, 1, 1),
+        date=date(1970, 1, 1),
         price=1.25,
         fee=0.25,
         account=AccountFactory(title='Account1'),
         saving_type=SavingTypeFactory(title='Saving1')
     )
     SavingFactory(
-        date=dt(1970, 1, 1),
+        date=date(1970, 1, 1),
         price=0.25,
         fee=0.0,
         account=AccountFactory(title='Account2'),
         saving_type=SavingTypeFactory(title='Saving2')
     )
     SavingFactory(
-        date=dt(1999, 1, 1),
+        date=date(1999, 1, 1),
         price=1.25,
         fee=0.25,
         account=AccountFactory(title='Account1'),
         saving_type=SavingTypeFactory(title='Saving1')
     )
     SavingFactory(
-        date=dt(1999, 1, 1),
+        date=date(1999, 1, 1),
         price=2.25,
         fee=0.25,
         account=AccountFactory(title='Account1'),
         saving_type=SavingTypeFactory(title='Saving1')
     )
     SavingFactory(
-        date=dt(1999, 1, 1),
+        date=date(1999, 1, 1),
         price=2.25,
         fee=0.25,
         account=AccountFactory(title='Account2'),
@@ -171,21 +172,21 @@ def savings():
 @pytest.fixture()
 def savings_close():
     SavingCloseFactory(
-        date=dt(1999, 1, 1),
+        date=date(1999, 1, 1),
         price=0.25,
         fee=0.05,
         to_account=AccountFactory(title='Account1'),
         from_account=SavingTypeFactory(title='Saving1')
     )
     SavingCloseFactory(
-        date=dt(1999, 1, 1),
+        date=date(1999, 1, 1),
         price=0.0,
         fee=0.0,
         to_account=AccountFactory(title='Account2'),
         from_account=SavingTypeFactory(title='Saving1')
     )
     SavingCloseFactory(
-        date=dt(1970, 1, 1),
+        date=date(1970, 1, 1),
         price=0.25,
         fee=0.05,
         to_account=AccountFactory(title='Account1'),
@@ -196,14 +197,14 @@ def savings_close():
 @pytest.fixture()
 def savings_change():
     SavingChangeFactory(
-        date=dt(1999, 1, 1),
+        date=date(1999, 1, 1),
         price=1.25,
         fee=0.05,
         to_account=SavingTypeFactory(title='Saving2'),
         from_account=SavingTypeFactory(title='Saving1'),
     )
     SavingChangeFactory(
-        date=dt(1970, 1, 1),
+        date=date(1970, 1, 1),
         price=2.25,
         fee=0.15,
         to_account=SavingTypeFactory(title='Saving2'),
@@ -213,42 +214,53 @@ def savings_change():
 
 @pytest.fixture()
 def accounts_worth():
-    AccountWorthFactory(
-        date=dt(1999, 1, 1),
-        price=3.25,
-        account=AccountFactory(title='Account1')
-    )
-    AccountWorthFactory(
-        date=dt(1999, 1, 1),
-        price=8.0,
-        account=AccountFactory(title='Account2')
-    )
-    AccountWorthFactory(
-        date=dt(1970, 1, 1),
-        price=10,
-        account=AccountFactory(title='Account1')
-    )
-    AccountWorthFactory(
-        date=dt(1970, 1, 1),
-        price=20,
-        account=AccountFactory(title='Account2')
-    )
+    with mock.patch('django.utils.timezone.now') as mock_now:
+        mock_now.return_value = datetime(1999, 1, 1, tzinfo=pytz.utc)
+
+        AccountWorthFactory(
+            date=datetime(1999, 1, 1, tzinfo=pytz.utc),
+            price=3.25,
+            account=AccountFactory(title='Account1')
+        )
+        AccountWorthFactory(
+            date=datetime(1999, 1, 1, tzinfo=pytz.utc),
+            price=8.0,
+            account=AccountFactory(title='Account2')
+        )
+
+    with mock.patch('django.utils.timezone.now') as mock_now:
+        mock_now.return_value = datetime(1970, 1, 1, tzinfo=pytz.utc)
+
+        AccountWorthFactory(
+            date=datetime(1970, 1, 1, tzinfo=pytz.utc),
+            price=10,
+            account=AccountFactory(title='Account1')
+        )
+        AccountWorthFactory(
+            date=datetime(1970, 1, 1, tzinfo=pytz.utc),
+            price=20,
+            account=AccountFactory(title='Account2')
+        )
 
 
 @pytest.fixture()
 def savings_worth():
-    SavingWorthFactory(
-        date=dt(1999, 1, 1),
-        price=0.15,
-        saving_type=SavingTypeFactory(title='Saving1')
-    )
-    SavingWorthFactory(
-        date=dt(1999, 1, 1),
-        price=6.15,
-        saving_type=SavingTypeFactory(title='Saving2')
-    )
-    SavingWorthFactory(
-        date=dt(1970, 1, 1),
-        price=10,
-        saving_type=SavingTypeFactory(title='Saving1')
-    )
+    with mock.patch('django.utils.timezone.now') as mock_now:
+        mock_now.return_value = datetime(1999, 1, 1, tzinfo=pytz.utc)
+
+        SavingWorthFactory(
+            price=0.15,
+            saving_type=SavingTypeFactory(title='Saving1')
+        )
+        SavingWorthFactory(
+            price=6.15,
+            saving_type=SavingTypeFactory(title='Saving2')
+        )
+
+    with mock.patch('django.utils.timezone.now') as mock_now:
+        mock_now.return_value = datetime(1970, 1, 1, tzinfo=pytz.utc)
+
+        SavingWorthFactory(
+            price=10,
+            saving_type=SavingTypeFactory(title='Saving1')
+        )
