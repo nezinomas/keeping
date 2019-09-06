dadf372fb79b9d5e82ea04cc2873b612509b994f
Audrius <nezinomas@gmail.com>
2019-09-06 16:49:24 +0300

    bookkeeping/tests/test_models.py uses global fixtures

--- project/bookkeeping/tests/test_models.py
+++ project/bookkeeping/tests/test_models.py
@@ -1,94 +1,46 @@
 from datetime import datetime as dt
+from decimal import Decimal
 
 import mock
 import pytest
 import pytz
 
 from ...accounts.factories import AccountFactory
+from ...core.tests.utils import equal_list_of_dictionaries as assert_
 from ...savings.factories import SavingTypeFactory
 from .. import factories, models
 
 pytestmark = pytest.mark.django_db
 
 
-@pytest.fixture()
-def _account_worth():
-    account = AccountFactory(title='A')
-
-    with mock.patch('django.utils.timezone.now') as mock_now:
-        mock_now.return_value = dt(1970, 1, 1, tzinfo=pytz.utc)
-        factories.AccountWorthFactory(
-            price=1.0,
-            account=account
-        )
-
-    with mock.patch('django.utils.timezone.now') as mock_now:
-        mock_now.return_value = dt(1999, 1, 1, tzinfo=pytz.utc)
-        factories.AccountWorthFactory(
-            price=2.0,
-            account=account
-        )
-
-
-@pytest.fixture()
-def _saving_worth():
-    saving_type = SavingTypeFactory()
-
-    with mock.patch('django.utils.timezone.now') as mock_now:
-        mock_now.return_value = dt(1970, 1, 1, tzinfo=pytz.utc)
-        factories.SavingWorthFactory(
-            price=1.0,
-            saving_type=saving_type
-        )
-
-    with mock.patch('django.utils.timezone.now') as mock_now:
-        mock_now.return_value = dt(1999, 1, 1, tzinfo=pytz.utc)
-        factories.SavingWorthFactory(
-            price=2.0,
-            saving_type=saving_type
-        )
-
-
-def test_account_worth_latest(_account_worth):
-    actual = models.AccountWorth.objects.items()
-
-    assert 1 == len(actual)
-    assert 2.0 == actual[0]['price']
-
-
-def test_account_worth_latest_values(_account_worth):
+def test_account_worth_latest_values(accounts_worth):
     actual = list(models.AccountWorth.objects.items())
 
     expect = [
-        {'account': 'A', 'price': 2.0}
+        {'account': 'Account1', 'price': 3.25},
+        {'account': 'Account2', 'price': 8.0},
     ]
 
-    assert expect == actual
+    assert_(expect, actual)
 
 
-def test_account_worth_queries(django_assert_num_queries, _account_worth):
+def test_account_worth_queries(django_assert_num_queries, accounts_worth):
     with django_assert_num_queries(1) as captured:
         list(models.AccountWorth.objects.items())
 
 
-def test_saving_worth_latest(_saving_worth):
-    actual = models.SavingWorth.objects.items()
-
-    assert 1 == len(actual)
-    assert 2.0 == actual[0]['price']
-
-
-def test_saving_worth_latest_values(_saving_worth):
+def test_saving_worth_latest_values(savings_worth):
     actual = list(models.SavingWorth.objects.items())
 
     expect = [
-        {'saving': 'Savings', 'price': 2.0}
+        {'saving': 'Saving1', 'price': 0.15},
+        {'saving': 'Saving2', 'price': 6.15},
     ]
 
-    assert expect == actual
+    assert_(expect, actual)
 
 
-def test_saving_worth_queries(django_assert_num_queries, _saving_worth):
+def test_saving_worth_queries(django_assert_num_queries, savings_worth):
     with django_assert_num_queries(1) as captured:
         list(models.SavingWorth.objects.items())
 
