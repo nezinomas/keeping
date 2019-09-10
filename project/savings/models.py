b34e523d8a01c4f6e95f8de546291d3493c63a15
Audrius <nezinomas@gmail.com>
2019-09-10 14:51:36 +0300

    savings balance calculated in QuerySet class model tests

--- project/savings/models.py
+++ project/savings/models.py
@@ -2,15 +2,90 @@ from decimal import Decimal
 
 from django.core.validators import MinValueValidator
 from django.db import models
+from django.db.models import Count, F
 from django_pandas.managers import DataFrameManager
 
 from ..accounts.models import Account
+from ..core.mixins.queryset_balance import QuerySetBalanceMixin
 from ..core.models import TitleAbstract
+
+
+class SavingTypeQuerySet(QuerySetBalanceMixin, models.QuerySet):
+    def incomes(self, year):
+        return self.annotate_(year, 'saving', 'i')
+
+    def fees(self, year):
+        return self.annotate_(year, 'saving', 'f', 'fee')
+
+    def savings_change_to(self, year):
+        return self.annotate_(year, 'savings_change_to', 'change_to')
+
+    def savings_change_to_fee(self, year):
+        return self.annotate_(year, 'savings_change_to', 'change_to_fee', 'fee')
+
+    def savings_change_from(self, year):
+        return self.annotate_(year, 'savings_change_from', 'change_from')
+
+    def savings_change_from_fee(self, year):
+        return self.annotate_(year, 'savings_change_from', 'change_from_fee', 'fee')
+
+    def savings_close_from(self, year):
+        return self.annotate_(year, 'savings_close_from', 'close_from')
+
+    def balance_year(self, year):
+        return (
+            self
+            .annotate(a=Count('title', distinct=True))
+            .values('id')
+            .values(title=F('title'))
+            .incomes(year)
+            .fees(year)
+            .savings_change_to(year)
+            .savings_change_to_fee(year)
+            .savings_change_from(year)
+            .savings_change_from_fee(year)
+            .savings_close_from(year)
+            .annotate(past_amount=(
+                0
+                + F('i_past')
+                + F('change_to_past')
+                - F('change_from_past')
+                - F('close_from_past')
+            ))
+            .annotate(past_fee=(
+                0
+                + F('f_past')
+                + F('change_to_fee_past')
+                + F('change_from_fee_past')
+            ))
+            .annotate(incomes=(
+                0
+                + F('i_now')
+                + F('past_amount')
+                + F('change_to_now')
+                - F('change_from_now')
+                - F('close_from_now')
+            ))
+            .annotate(fees=(
+                0
+                + F('f_now')
+                + F('past_fee')
+                + F('change_to_fee_now')
+                + F('change_from_fee_now')
+            ))
+            .annotate(invested=(
+                F('incomes') - F('fees')
+            ))
+            .values('title', 'incomes', 'past_amount', 'past_fee', 'fees', 'invested')
+        )
 
 
 class SavingType(TitleAbstract):
     class Meta:
         ordering = ['title']
+
+    # Managers
+    objects = SavingTypeQuerySet.as_manager()
 
 
 class SavingQuerySet(models.QuerySet):
