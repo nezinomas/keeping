d22637e3ceefd984767336cc5027587f6ca52b07
Audrius <nezinomas@gmail.com>
2019-09-12 12:27:53 +0300

    ištrinta užkomentuota nereikalinga eilutė

--- project/config/settings/base.py
+++ project/config/settings/base.py
@@ -78,7 +78,6 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
-                # 'project.products.context.show_categories',
             ],
         },
     },
