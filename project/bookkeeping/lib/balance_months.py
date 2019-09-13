79fa190c1834df652112f227b246dd30dd59d3f2
Audrius <nezinomas@gmail.com>
2019-09-13 14:53:55 +0300

    refactoring, methods extracted to Mixin class

--- project/bookkeeping/lib/balance_months.py
+++ project/bookkeeping/lib/balance_months.py
@@ -1,7 +1,58 @@
 import pandas as pd
 
 
-class BalanceMonths():
+class CalcBalanceMixin():
+    def convert_to_df(self, list_):
+        year = list_[0]['date'].year
+
+        df = pd.DataFrame(list_)
+
+        # convert to float and datetime.date
+        for col in df.columns:
+            if col == 'date':
+                df[col] = pd.to_datetime(df[col])
+            else:
+                df[col] = pd.to_numeric(df[col])
+
+        df.set_index(df.date, inplace=True)
+
+        # create empty DataFrame object with index containing all months
+        date_range = pd.date_range(f'{year}', periods=12, freq='MS')
+        data = pd.DataFrame(date_range, columns=['date'])
+        data.set_index('date', inplace=True)
+
+        # concat two dataframes
+        df = pd.concat([data, df], axis=1).fillna(0.0)
+        df.drop(['date'], axis=1, inplace=True)
+
+        return df
+
+    def average(self, list_):
+        val = None
+        avg = list_.copy()
+
+        if not avg.empty:
+            # replace 0.0 to None
+            # average will be calculated only for months with non zero values
+            avg.replace(0.0, pd.NaT, inplace=True)
+            avg = avg.mean(skipna=True)
+
+            val = avg.to_dict()
+
+        return val
+
+    def totals(self, list_):
+        val = None
+        total = list_.copy()
+
+        if not total.empty:
+            total = total.sum()
+            val = total.to_dict()
+
+        return val
+
+
+class BalanceMonths(CalcBalanceMixin):
     def __init__(self, incomes, expenses, amount_start=0.0):
         try:
             amount_start = float(amount_start)
@@ -50,58 +101,15 @@ class BalanceMonths():
 
     @property
     def totals(self):
-        val = None
-        total = self._balance.copy()
-
-        if not total.empty:
-            total = total.sum()
-            val = total.to_dict()
-
-        return val
+        return super().totals(self._balance)
 
     @property
     def average(self):
-        val = None
-        avg = self._balance.copy()
-
-        if not avg.empty:
-            # replace 0.0 to None
-            # average will be calculated only for months with non zero values
-            avg.replace(0.0, pd.NaT, inplace=True)
-            avg = avg.mean(skipna=True)
-
-            val = avg.to_dict()
-
-        return val
-
-    def _convert_to_df(self, list_):
-        year = list_[0]['date'].year
-
-        df = pd.DataFrame(list_)
-
-        # convert to float and datetime.date
-        for col in df.columns:
-            if col == 'date':
-                df[col] = pd.to_datetime(df[col])
-            else:
-                df[col] = pd.to_numeric(df[col])
-
-        df.set_index(df.date, inplace=True)
-
-        # create empty DataFrame object with index containing all months
-        date_range = pd.date_range(f'{year}', periods=12, freq='MS')
-        data = pd.DataFrame(date_range, columns=['date'])
-        data.set_index('date', inplace=True)
-
-        # concat two dataframes
-        df = pd.concat([data, df], axis=1).fillna(0.0)
-        df.drop(['date'], axis=1, inplace=True)
-
-        return df
+        return super().average(self._balance)
 
     def _calc(self, incomes, expenses):
-        incomes = self._convert_to_df(incomes)
-        expenses = self._convert_to_df(expenses)
+        incomes = super().convert_to_df(incomes)
+        expenses = super().convert_to_df(expenses)
 
         df = incomes.join(
             expenses,
