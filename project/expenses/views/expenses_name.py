a4f268f4fac101e119d094e294f461fbbdcab5aa
Audrius <nezinomas@gmail.com>
2019-09-18 15:07:29 +0300

    expenses_name views UpdateContextMixin to get ExpenseType objects, not ExpenseName

--- project/expenses/views/expenses_name.py
+++ project/expenses/views/expenses_name.py
@@ -2,11 +2,20 @@ from ...core.mixins.views import CreateAjaxMixin, UpdateAjaxMixin
 from .. import forms, models
 
 
-class New(CreateAjaxMixin):
+class UpdateContext():
+    list_template_name = 'expenses/includes/expenses_type_list.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context[self.context_object_name] = models.ExpenseType.objects.items()
+        return context
+
+
+class New(UpdateContext, CreateAjaxMixin):
     model = models.ExpenseName
     form_class = forms.ExpenseNameForm
 
 
-class Update(UpdateAjaxMixin):
+class Update(UpdateContext, UpdateAjaxMixin):
     model = models.ExpenseName
     form_class = forms.ExpenseNameForm
