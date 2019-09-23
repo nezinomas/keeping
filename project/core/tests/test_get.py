f82ac3f1ce111ec8abcce2f6ffc13b6585817f13
Audrius <nezinomas@gmail.com>
2019-09-23 10:05:20 +0300

    refactored tests, using decorator instead with

--- project/core/tests/test_get.py
+++ project/core/tests/test_get.py
@@ -13,56 +13,56 @@ class Dummy(GetQuerysetMixin, MultipleObjectMixin):
         self.request = request
 
 
-def test_get_execute_objects_year(rf):
-    with mock.patch('project.incomes.models.Income') as mock_obj:
-        mock_obj.objects = mock.MagicMock()
+@mock.patch('project.incomes.models.Income')
+def test_get_execute_objects_year(mock_obj, rf):
+    mock_obj.objects = mock.MagicMock()
 
-        mock_obj.objects.year = mock.MagicMock()
-        mock_obj.objects.year.return_value = 1
+    mock_obj.objects.year = mock.MagicMock()
+    mock_obj.objects.year.return_value = 1
 
-        request = rf.get('/fake/')
-        request.user = UserFactory.build()
+    request = rf.get('/fake/')
+    request.user = UserFactory.build()
 
-        a = Dummy(mock_obj, request)
-        b = a.get_queryset()
+    a = Dummy(mock_obj, request)
+    b = a.get_queryset()
 
-        assert b == 1
-
-
-def test_get_execute_objects_items(rf):
-    with mock.patch('project.incomes.models.Income') as mock_obj:
-        mock_obj.objects = mock.MagicMock()
-
-        mock_obj.objects.year = mock.MagicMock()
-        mock_obj.objects.year.side_effect = Exception('Unknown')
-
-        mock_obj.objects.items = mock.MagicMock()
-        mock_obj.objects.items.return_value = 2
-
-        request = rf.get('/fake/')
-        request.user = UserFactory.build()
-
-        a = Dummy(mock_obj, request).get_queryset()
-
-        assert a == 2
+    assert b == 1
 
 
-def test_get_exexute_objects_all(rf):
-    with mock.patch('project.incomes.models.Income') as mock_obj:
-        mock_obj.objects = mock.MagicMock()
+@mock.patch('project.incomes.models.Income')
+def test_get_execute_objects_items(mock_obj, rf):
+    mock_obj.objects = mock.MagicMock()
 
-        mock_obj.objects.year = mock.MagicMock()
-        mock_obj.objects.year.side_effect = Exception('Unknown1')
+    mock_obj.objects.year = mock.MagicMock()
+    mock_obj.objects.year.side_effect = Exception('Unknown')
 
-        mock_obj.objects.items = mock.MagicMock()
-        mock_obj.objects.items.side_effect = Exception('Unknown2')
+    mock_obj.objects.items = mock.MagicMock()
+    mock_obj.objects.items.return_value = 2
 
-        mock_obj.objects.all = mock.MagicMock()
-        mock_obj.objects.all.return_value = 3
+    request = rf.get('/fake/')
+    request.user = UserFactory.build()
 
-        request = rf.get('/fake/')
-        request.user = UserFactory.build()
+    a = Dummy(mock_obj, request).get_queryset()
 
-        a = Dummy(mock_obj, request).get_queryset()
+    assert a == 2
 
-        assert a == 3
+
+@mock.patch('project.incomes.models.Income')
+def test_get_exexute_objects_all(mock_obj, rf):
+    mock_obj.objects = mock.MagicMock()
+
+    mock_obj.objects.year = mock.MagicMock()
+    mock_obj.objects.year.side_effect = Exception('Unknown1')
+
+    mock_obj.objects.items = mock.MagicMock()
+    mock_obj.objects.items.side_effect = Exception('Unknown2')
+
+    mock_obj.objects.all = mock.MagicMock()
+    mock_obj.objects.all.return_value = 3
+
+    request = rf.get('/fake/')
+    request.user = UserFactory.build()
+
+    a = Dummy(mock_obj, request).get_queryset()
+
+    assert a == 3
