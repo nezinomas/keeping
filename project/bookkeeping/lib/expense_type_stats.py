17b70f1e1d62902e95bf2aec78b24c7bc436f342
Audrius <nezinomas@gmail.com>
2019-09-24 17:02:15 +0300

    deleted not necessary code

--- project/bookkeeping/lib/expense_type_stats.py
+++ project/bookkeeping/lib/expense_type_stats.py
@@ -28,15 +28,6 @@ class MonthExpenseType(BalanceStats):
 
         self._balance = calc(expenses, self._balance)
 
-    def _calc(self, expenses: List[Dict]) -> None:
-        # copy values from expenses to data_frame
-        for _dict in expenses:
-            self._balance.at[_dict['date'], _dict['title']] = float(_dict['sum'])
-
-        self._balance.fillna(0.0, inplace=True)
-
-        self._balance['total'] = self._balance.sum(axis=1)
-
 
 class MonthsExpenseType(BalanceStats):
     def __init__(self, year, expenses: List[Dict]):
@@ -62,13 +53,3 @@ class MonthsExpenseType(BalanceStats):
             # transfort arr for pie chart
             rtn = [{'name': key, 'y': value} for key, value in arr.items()]
         return rtn
-
-    def _calc(self, expenses: List[Dict]) -> None:
-        # copy values from expenses to data_frame
-        for _dict in expenses:
-            self._balance.at[_dict['date'],
-                             _dict['title']] = float(_dict['sum'])
-
-        self._balance.fillna(0.0, inplace=True)
-
-        self._balance['total'] = self._balance.sum(axis=1)
