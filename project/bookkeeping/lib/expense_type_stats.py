37717d6d5f3bd2f02c6ccc967dac028ea0638885
Audrius <nezinomas@gmail.com>
2019-09-24 16:51:44 +0300

    renamed files: month_expense_type -> expense_type_stats test_month_expense_type -> test_expense_type_stats
    copied code
    months_expense_type -> expense_type_stats
    test_months_expense_type -> test_expense_type_stats

---
+++ project/bookkeeping/lib/expense_type_stats.py
@@ -0,0 +1,62 @@
+from datetime import datetime
+from typing import Dict, List
+
+import pandas as pd
+
+from ..mixins.calc_balance import (BalanceStats, df_days_of_month,
+                                   df_months_of_year)
+
+
+class MonthExpenseType(BalanceStats):
+    def __init__(self, year: int, month: int, expenses: List[Dict]):
+        self._balance = df_days_of_month(year, month)
+
+        if not expenses:
+            return
+
+        self._calc(expenses)
+
+    def _calc(self, expenses: List[Dict]) -> None:
+        # copy values from expenses to data_frame
+        for _dict in expenses:
+            self._balance.at[_dict['date'], _dict['title']] = float(_dict['sum'])
+
+        self._balance.fillna(0.0, inplace=True)
+
+        self._balance['total'] = self._balance.sum(axis=1)
+
+
+class MonthsExpenseType(BalanceStats):
+    def __init__(self, year, expenses: List[Dict]):
+        self._balance = df_months_of_year(year)
+
+        if not expenses:
+            return
+
+        self._calc(expenses)
+
+    @property
+    def chart_data(self) -> List[Dict[str, float]]:
+        rtn = []
+        arr = super().totals
+
+        if arr:
+            # delete total cell
+            del arr['total']
+
+            # sort dictionary
+            arr = dict(sorted(arr.items(), key=lambda x: x[1], reverse=True))
+
+            # transfort arr for pie chart
+            rtn = [{'name': key, 'y': value} for key, value in arr.items()]
+        return rtn
+
+    def _calc(self, expenses: List[Dict]) -> None:
+        # copy values from expenses to data_frame
+        for _dict in expenses:
+            self._balance.at[_dict['date'],
+                             _dict['title']] = float(_dict['sum'])
+
+        self._balance.fillna(0.0, inplace=True)
+
+        self._balance['total'] = self._balance.sum(axis=1)
---
+++ project/bookkeeping/tests/test_expense_type_stats.py
@@ -0,0 +1,101 @@
+from datetime import date
+from decimal import Decimal
+
+import pytest
+
+from ..lib.expense_type_stats import MonthExpenseType, MonthsExpenseType
+
+
+@pytest.fixture
+def _ex():
+    return ([
+        {'date': date(1999, 1, 1), 'sum': Decimal(0.5), 'title': 'T1'},
+        {'date': date(1999, 1, 1), 'sum': Decimal(0.25), 'title': 'T2'},
+        {'date': date(1999, 1, 2), 'sum': Decimal(0.75), 'title': 'T1'},
+    ])
+
+
+def test_balance_lenght_empty_expenses():
+    actual = MonthExpenseType(1999, 1, []).balance
+
+    assert 31 == len(actual)
+
+
+def test_balance_lenght_none_expenses():
+    actual = MonthExpenseType(1999, 1, None).balance
+
+    assert 31 == len(actual)
+
+
+@pytest.fixture
+def _ex():
+    return ([
+        {'date': date(1999, 1, 1), 'sum': Decimal(0.5), 'title': 'T1'},
+        {'date': date(1999, 1, 1), 'sum': Decimal(0.25), 'title': 'T2'},
+        {'date': date(1999, 12, 1), 'sum': Decimal(0.75), 'title': 'T1'},
+        {'date': date(1999, 12, 1), 'sum': Decimal(0.35), 'title': 'T2'},
+    ])
+
+
+def test_months_expense_type(_ex):
+    expect = [
+        {'date': date(1999, 1, 1), 'T1': 0.5, 'T2': 0.25, 'total': 0.75},
+        {'date': date(1999, 2, 1), 'T1': 0.0, 'T2': 0.0, 'total': 0.0},
+        {'date': date(1999, 3, 1), 'T1': 0.0, 'T2': 0.0, 'total': 0.0},
+        {'date': date(1999, 4, 1), 'T1': 0.0, 'T2': 0.0, 'total': 0.0},
+        {'date': date(1999, 5, 1), 'T1': 0.0, 'T2': 0.0, 'total': 0.0},
+        {'date': date(1999, 6, 1), 'T1': 0.0, 'T2': 0.0, 'total': 0.0},
+        {'date': date(1999, 7, 1), 'T1': 0.0, 'T2': 0.0, 'total': 0.0},
+        {'date': date(1999, 8, 1), 'T1': 0.0, 'T2': 0.0, 'total': 0.0},
+        {'date': date(1999, 9, 1), 'T1': 0.0, 'T2': 0.0, 'total': 0.0},
+        {'date': date(1999, 10, 1), 'T1': 0.0, 'T2': 0.0, 'total': 0.0},
+        {'date': date(1999, 11, 1), 'T1': 0.0, 'T2': 0.0, 'total': 0.0},
+        {'date': date(1999, 12, 1), 'T1': 0.75, 'T2': 0.35, 'total': 1.1},
+    ]
+
+    actual = MonthsExpenseType(1999, _ex).balance
+
+    assert expect == actual
+
+
+def test_months_expense_type_totals(_ex):
+    expect = {'T1': 1.25, 'T2': 0.6, 'total': 1.85}
+
+    actual = MonthsExpenseType(1999, _ex).totals
+
+    assert expect == actual
+
+
+def test_months_expense_type_average(_ex):
+    expect = {'T1': 0.625, 'T2': 0.3, 'total': 0.925}
+
+    actual = MonthsExpenseType(1999, _ex).average
+
+    assert expect == actual
+
+
+def test_months_expense_chart_data(_ex):
+    expect = [
+        {'name': 'T1', 'y': 1.25},
+        {'name': 'T2', 'y': 0.6}
+    ]
+
+    actual = MonthsExpenseType(1999, _ex).chart_data
+
+    assert expect == actual
+
+
+def test_months_expense_chart_data_none():
+    expect = []
+
+    actual = MonthsExpenseType(1999, None).chart_data
+
+    assert expect == actual
+
+
+def test_months_expense_chart_data_empty():
+    expect = []
+
+    actual = MonthsExpenseType(1999, []).chart_data
+
+    assert expect == actual
