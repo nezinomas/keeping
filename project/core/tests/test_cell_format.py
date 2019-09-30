bf34811e3486ebb8777b5f07972820349d28ebf9
Audrius <nezinomas@gmail.com>
2019-09-30 16:34:56 +0300

    new custom filter comparison and it's tests

--- project/core/tests/test_cell_format.py
+++ project/core/tests/test_cell_format.py
@@ -1,5 +1,9 @@
+from decimal import Decimal
+
 import pytest
-from ..templatetags.cell_format import negative, positive
+
+from ..templatetags.cell_format import (comparison, negative, positive,
+                                        positive_negative)
 
 
 def test_value_positive_str():
@@ -79,3 +83,51 @@ def test_value_str_1():
     actual = positive('xxx')
 
     assert not actual
+
+
+def test_comparison_bigger_str():
+    actual = comparison('1', '2')
+
+    assert 'table-success' == actual
+
+
+def test_comparison_bigger_int():
+    actual = comparison(1, 2)
+
+    assert 'table-success' == actual
+
+
+def test_comparison_smaller_str():
+    actual = comparison('1', '0')
+
+    assert 'table-danger' == actual
+
+
+def test_comparison_smaller_int():
+    actual = comparison(1, 0)
+
+    assert 'table-danger' == actual
+
+
+def test_comparison_smaller_decimal():
+    actual = comparison(1, Decimal(0.1))
+
+    assert 'table-danger' == actual
+
+
+def test_comparison_none():
+    actual = comparison(None, None)
+
+    assert not actual
+
+
+def test_positive_negative_positive():
+    actual = positive_negative('1')
+
+    assert 'table-success' == actual
+
+
+def test_positive_negative_negative():
+    actual = positive_negative('-1')
+
+    assert 'table-danger' == actual
