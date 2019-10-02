1268c0b9f5ddd3b4d452ab6e7e4d2fbd27aa164b
Audrius <nezinomas@gmail.com>
2019-10-02 15:26:42 +0300

    new rewrited DaySum class for plans module

---
+++ project/plans/lib/day_sum_new.py
@@ -0,0 +1,169 @@
+import calendar
+from collections import namedtuple
+from dataclasses import dataclass, field
+from datetime import datetime
+from decimal import Decimal
+from time import strptime
+from typing import Dict, List
+
+import pandas as pd
+from django.db.models import F
+
+from ..models import DayPlan, ExpensePlan, IncomePlan, SavingPlan
+
+
+def months():
+    return [x.lower() for x in calendar.month_name[1:]]
+
+
+def month_days(year: int, month_name: str) -> int:
+    month = datetime.strptime(month_name, "%B").month
+    return calendar.monthlen(year, month)
+
+
+class CollectData:
+    _incomes: List[Dict[str, Decimal]] = []
+    _expenses: List[Dict[str, Decimal]] = []
+    _savings: List[Dict[str, Decimal]] = []
+    _days: List[Dict[str, Decimal]] = []
+
+    def __init__(self, year: int = 1970):
+        self._year = year
+
+        self._incomes = (
+            IncomePlan.objects.year(self._year)
+            .values(*months()))
+
+        self._expenses = (
+            ExpensePlan.objects.year(self._year)
+            .values(*months(), necessary=F('expense_type__necessary')))
+
+        self._savings = (
+            SavingPlan.objects.year(self._year)
+            .values(*months()))
+
+        self._days = (
+            DayPlan.objects.year(self._year)
+            .values(*months()))
+
+    @property
+    def incomes(self) -> List[Dict[str, Decimal]]:
+        return self._incomes
+
+    @property
+    def expenses(self) -> List[Dict[str, Decimal]]:
+        return self._expenses
+
+    @property
+    def savings(self) -> List[Dict[str, Decimal]]:
+        return self._savings
+
+    @property
+    def days(self) -> List[Dict[str, Decimal]]:
+        return self._days
+
+
+class CalcDaySum():
+    def __init__(self, year: int):
+        self._year = year
+        self._data = self._get_data()
+
+        self._calc_df()
+
+    @property
+    def incomes(self) -> Dict[str, float]:
+        return self._df.loc['incomes'].to_dict()
+
+    @property
+    def savings(self) -> Dict[str, float]:
+        return self._df.loc['savings'].to_dict()
+
+    @property
+    def expenses_free(self) -> Dict[str, float]:
+        return self._df.loc['expenses_free'].to_dict()
+
+    @property
+    def expenses_necessary(self) -> Dict[str, float]:
+        return self._df.loc['expenses_necessary'].to_dict()
+
+    @property
+    def day_calced(self) -> Dict[str, float]:
+        return self._df.loc['day_calced'].to_dict()
+
+    @property
+    def day_input(self) -> Dict[str, float]:
+        return self._df.loc['day_input'].to_dict()
+
+    @property
+    def remains(self) -> Dict[str, float]:
+        return self._df.loc['remains'].to_dict()
+
+    @property
+    def plans_stats(self):
+        dicts = [
+            dict({'type': 'Būtinos išlaidos'}, **self.expenses_necessary),
+            dict({'type': 'Lieka kasdienybei', **self.expenses_free}),
+            dict({'type': 'Suma dienai, max galima'}, **self.day_calced),
+            dict({'type': 'Likutis'}, **self.remains),
+        ]
+
+        # list of dictionaries convert to list of objects
+        return [namedtuple("Items", item.keys())(*item.values()) for item in dicts]
+
+    def _get_data(self):
+        return CollectData(self._year)
+
+    def _sum(self, arr: List, row_name: str, necessary: int = -1):
+        df = pd.DataFrame(arr, dtype=float)
+
+        if df.empty:
+            return
+
+        if necessary >= 0:
+            # filter expensy_type by necessary/ordinary
+            df = df.loc[df['necessary'] == necessary]
+
+        df.loc['sum', :] = df.sum(axis=0)
+
+        self._df.loc[row_name, :] = df.loc['sum', :]
+
+    def _create_df(self) -> pd.DataFrame:
+        df = pd.DataFrame(columns=months(), dtype=float)
+
+        df.loc['incomes', :] = 0.0
+        df.loc['savings', :] = 0.0
+        df.loc['expenses_necessary', :] = 0.0
+        df.loc['expenses_free', :] = 0.0
+        df.loc['day_calced', :] = 0.0
+        df.loc['day_input', :] = 0.0
+        df.loc['remains', :] = 0.0
+
+        df.loc['m', :] = df.apply(
+            lambda x: month_days(self._year, x.name), axis=0)
+
+        return df
+
+    def _calc_df(self) -> None:
+        self._df = self._create_df()
+
+        self._sum(self._data.incomes, 'incomes')
+        self._sum(self._data.savings, 'savings')
+        self._sum(self._data.days, 'day_input')
+        self._sum(self._data.expenses, 'expenses_necessary', 1)
+        self._sum(self._data.expenses, 'expenses_free', 0)
+
+        self._df.loc['expenses_necessary'] = (
+            self._df.loc['expenses_necessary'] + self._df.loc['savings']
+        )
+        self._df.loc['expenses_free'] = (
+            self._df.loc['incomes'] - self._df.loc['expenses_necessary']
+        )
+        self._df.loc['day_calced'] = (
+            self._df.loc['expenses_free'] / self._df.loc['m']
+        )
+        self._df.loc['remains'] = (
+            self._df.loc['expenses_free'] - self._df.loc['day_input'] * self._df.loc['m']
+        )
+
+        # fill cell with NaN
+        self._df.fillna(0.0, inplace=True)
---
+++ project/plans/tests/test_day_sum_new.py
@@ -0,0 +1,152 @@
+from decimal import Decimal
+
+import pytest
+
+from ..lib.day_sum_new import CalcDaySum, CollectData
+
+
+@pytest.fixture()
+def data(monkeypatch):
+
+    monkeypatch.setattr(
+        CollectData,
+        '__init__',
+        lambda x, y: None
+    )
+
+    obj = CollectData(2020)  # leap year
+    obj._incomes = [
+        {'january': Decimal(400.01), 'february': Decimal(500.02)},
+        {'january': Decimal(500.02), 'february': Decimal(500.02)},
+    ]
+
+    obj._expenses = [
+        {'january': Decimal(10.01), 'february': Decimal(10.01), 'necessary': False},
+        {'january': Decimal(20.02), 'february': Decimal(20.02), 'necessary': True},
+        {'january': Decimal(30.03), 'february': Decimal(30.03), 'necessary': False},
+        {'january': Decimal(40.04), 'february': Decimal(40.04), 'necessary': True},
+    ]
+
+    obj._savings = [
+        {'january': Decimal(32.33), 'february': Decimal(32.33)},
+        {'january': Decimal(32.33), 'february': Decimal(32.33)},
+    ]
+
+    obj._days = [
+        {'january': Decimal(25.0), 'february': Decimal(26.0)},
+    ]
+
+    monkeypatch.setattr(
+        CalcDaySum,
+        '_get_data',
+        lambda x: obj
+    )
+
+
+@pytest.fixture()
+def data_empty(monkeypatch):
+
+    monkeypatch.setattr(
+        CollectData,
+        '__init__',
+        lambda x, y: None
+    )
+
+    obj = CollectData('xxx')
+
+    monkeypatch.setattr(
+        CalcDaySum,
+        '_get_data',
+        lambda x: obj
+    )
+
+
+def test_incomes(data):
+    actual = CalcDaySum(2020).incomes
+
+    assert 900.03 == round(actual['january'], 2)
+    assert 0.0 == actual['december']
+
+
+def test_incomes_no_data(data_empty):
+    actual = CalcDaySum(2020).incomes
+
+    assert 0.0 == actual['january']
+    assert 0.0 == actual['december']
+
+
+def test_savings(data):
+    actual = CalcDaySum(2020).savings
+
+    assert 64.66 == round(actual['january'], 2)
+    assert 64.66 == round(actual['february'], 2)
+
+
+def test_expenses_free(data):
+    actual = CalcDaySum(2020).expenses_free
+
+    assert 775.31 == round(actual['january'], 2)
+    assert 875.32 == round(actual['february'], 2)
+
+
+def test_expenses_necessary(data):
+    actual = CalcDaySum(2020).expenses_necessary
+
+    assert 124.72 == actual['january']
+    assert 124.72 == actual['february']
+
+
+def test_day_calced(data):
+    actual = CalcDaySum(2020).day_calced
+
+    assert 25.01 == round(actual['january'], 2)
+    assert 30.18 == round(actual['february'], 2)
+
+
+def test_day_input(data):
+    actual = CalcDaySum(2020).day_input
+
+    assert 25.0 == round(actual['january'], 2)
+    assert 26.0 == round(actual['february'], 2)
+
+
+def test_remains(data):
+    actual = CalcDaySum(2020).remains
+
+    assert 0.31 == round(actual['january'], 2)
+    assert 121.32 == round(actual['february'], 2)
+
+
+def test_plans_stats_list(data):
+    actual = CalcDaySum(2020).plans_stats
+
+    assert 4 == len(actual)
+
+
+def test_plans_stats_expenses_necessary(data):
+    actual = CalcDaySum(2020).plans_stats
+
+    assert 'Būtinos išlaidos' == actual[0].type
+    assert 124.72 == actual[0].january
+    assert 124.72 == actual[0].february
+
+
+def test_plans_stats_expenses_free(data):
+    actual = CalcDaySum(2020).plans_stats
+    assert 'Lieka kasdienybei' == actual[1].type
+    assert 775.31 == round(actual[1].january, 2)
+    assert 875.32 == round(actual[1].february, 2)
+
+
+def test_plans_stats_day_sum(data):
+    actual = CalcDaySum(2020).plans_stats
+    assert 'Suma dienai' in actual[2].type
+    assert 25.01 == round(actual[2].january, 2)
+    assert 30.18 == round(actual[2].february, 2)
+
+
+def test_plans_stats_remains(data):
+    actual = CalcDaySum(2020).plans_stats
+    assert 'Likutis' == actual[3].type
+    assert 0.31 == round(actual[3].january, 2)
+    assert 121.32 == round(actual[3].february, 2)
