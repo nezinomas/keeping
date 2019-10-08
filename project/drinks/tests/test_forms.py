035eb71dc15129306a3a9cfce603d8859124d9a5
Audrius <nezinomas@gmail.com>
2019-10-08 13:25:07 +0300

    DrinkForm tests

--- project/drinks/tests/test_forms.py
+++ project/drinks/tests/test_forms.py
@@ -1,7 +1,37 @@
+from datetime import date
+
 import pytest
 
 from ..factories import DrinkTargetFactory
 from ..forms import DrinkForm, DrinkTargetForm
+
+
+def test_drink_init():
+    DrinkForm()
+
+
+@pytest.mark.django_db
+def test_drink_valid_data():
+    form = DrinkForm(data={
+        'date': '1974-01-01',
+        'quantity': 1.0
+    })
+
+    assert form.is_valid()
+
+    data = form.save()
+
+    assert data.date == date(1974, 1, 1)
+    assert data.quantity == 1.0
+
+
+def test_drink_blank_data():
+    form = DrinkForm(data={})
+
+    assert not form.is_valid()
+
+    assert 'date' in form.errors
+    assert 'quantity' in form.errors
 
 
 def test_drink_target_init():
