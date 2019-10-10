d1f4f70fe7b1df0c35c9a08e655064b49aeecd6f
Audrius <nezinomas@gmail.com>
2019-10-10 13:27:43 +0300

    refactoring extracted cotext_to_reload to lib/views_helper module

---
+++ project/drinks/lib/views_helper.py
@@ -0,0 +1,76 @@
+from datetime import datetime
+
+from django.template.loader import render_to_string
+
+from .. import models
+from .drinks_stats import DrinkStats
+
+
+def context_to_reload(request, context):
+    year = request.user.profile.year
+
+    qs_target = models.DrinkTarget.objects.year(year)
+    qs_drinks = models.Drink.objects.month_sum(year)
+    qs_drinks_days = models.Drink.objects.day_sum(year)
+    qs_last = models.Drink.objects.latest()
+
+    _DrinkStats = DrinkStats(qs_drinks)
+
+    # values
+    avg = qs_drinks_days.get('per_day', 0)
+    qty = qs_drinks_days.get('qty', 0)
+    target_val = qs_target[0].quantity
+
+    context['chart_quantity'] = render_to_string(
+        'drinks/includes/chart_quantity_per_month.html',
+        {'data': _DrinkStats.quantity},
+        request
+    )
+
+    context['chart_consumsion'] = render_to_string(
+        'drinks/includes/chart_consumsion_per_month.html', {
+            'data': _DrinkStats.consumsion,
+            'target': target_val,
+            'avg': avg,
+            'avg_label_y': _avg_label_position(avg, target_val),
+            'target_label_y': _target_label_position(avg, target_val),
+        },
+        request
+    )
+
+    context['tbl_consumsion'] = render_to_string(
+        'drinks/includes/tbl_consumsion.html',
+        {'qty': qty, 'avg': avg},
+        request)
+
+    context['tbl_last_day'] = render_to_string(
+        'drinks/includes/tbl_last_day.html', {
+            'date': qs_last.date,
+            'delta': (datetime.now().date() - qs_last.date).days
+        },
+        request
+    )
+
+    context['tbl_alcohol'] = render_to_string(
+        'drinks/includes/tbl_alcohol.html',
+        {'l': qty * 0.025},
+        request
+    )
+
+
+def _avg_label_position(avg, target):
+    y = -5
+
+    if avg >= target - 50 and avg <= target:
+        y = 15
+
+    return y
+
+
+def _target_label_position(avg, target):
+    y = -5
+
+    if target >= avg - 50 and target <= avg:
+        y = 15
+
+    return y
--- project/drinks/views.py
+++ project/drinks/views.py
@@ -1,74 +1,10 @@
-from datetime import datetime
-
 from django.shortcuts import render
 from django.template.loader import render_to_string
 
 from ..core.mixins.views import (CreateAjaxMixin, IndexMixin, ListMixin,
                                  UpdateAjaxMixin)
 from . import forms, models
-from .lib.drinks_stats import DrinkStats
-
-
-def context_to_reload(request, context):
-    year = request.user.profile.year
-
-    qs_target = models.DrinkTarget.objects.year(year)
-    qs_drinks = models.Drink.objects.month_sum(year)
-    qs_drinks_days = models.Drink.objects.day_sum(year)
-    qs_last = models.Drink.objects.latest()
-
-    _DrinkStats = DrinkStats(qs_drinks)
-
-    # values
-    avg_val = qs_drinks_days.get('per_day', 0)
-    qty_val = qs_drinks_days.get('qty', 0)
-    target_val = qs_target[0].quantity
-
-    # calculate target and average label postions
-    avg_label_y_position = -5
-    target_label_y_position = -5
-
-    if avg_val >= target_val - 25 and avg_val <= target_val:
-        avg_label_y_position = 15
-
-    if target_val >= avg_val - 25 and target_val <= avg_val:
-        target_label_y_position = 15
-
-    context['chart_quantity'] = render_to_string(
-        'drinks/includes/chart_quantity_per_month.html',
-        {'data': _DrinkStats.quantity},
-        request
-    )
-
-    context['chart_consumsion'] = render_to_string(
-        'drinks/includes/chart_consumsion_per_month.html', {
-            'data': _DrinkStats.consumsion,
-            'target': target_val,
-            'avg': avg_val,
-            'avg_label_y_position': avg_label_y_position,
-            'target_label_y_position': target_label_y_position,
-        },
-        request
-    )
-
-    context['tbl_consumsion'] = render_to_string(
-        'drinks/includes/tbl_consumsion.html',
-        {'qty': qty_val, 'avg': avg_val},
-        request)
-
-    context['tbl_last_day'] = render_to_string(
-        'drinks/includes/tbl_last_day.html', {
-            'date': qs_last.date,
-            'delta': (datetime.now().date() - qs_last.date).days
-        },
-        request
-    )
-
-    context['tbl_alcohol'] = render_to_string(
-        'drinks/includes/tbl_alcohol.html',
-        {'l': qty_val * 0.025},
-        request
-    )
+from .lib.views_helper import context_to_reload
 
 
 def reload_stats(request):
@@ -88,7 +24,6 @@ class Index(IndexMixin):
         qs_target = models.DrinkTarget.objects.year(year)
 
         context = super().get_context_data(**kwargs)
-
         context_to_reload(self.request, context)
 
         context['drinks_list'] = Lists.as_view()(
