1b720877a3d2bca6aeb856f38fb18c3e2de5c154
Audrius <nezinomas@gmail.com>
2019-10-10 15:02:41 +0300

    refactoring, renamed variable

--- project/drinks/lib/views_helper.py
+++ project/drinks/lib/views_helper.py
@@ -19,7 +19,7 @@ def context_to_reload(request, context):
     # values
     avg = qs_drinks_days.get('per_day', 0)
     qty = qs_drinks_days.get('qty', 0)
-    target_val = qs_target[0].quantity
+    target = qs_target[0].quantity
 
     context['chart_quantity'] = render_to_string(
         'drinks/includes/chart_quantity_per_month.html',
@@ -30,17 +30,17 @@ def context_to_reload(request, context):
     context['chart_consumsion'] = render_to_string(
         'drinks/includes/chart_consumsion_per_month.html', {
             'data': _DrinkStats.consumsion,
-            'target': target_val,
+            'target': target,
             'avg': avg,
-            'avg_label_y': _avg_label_position(avg, target_val),
-            'target_label_y': _target_label_position(avg, target_val),
+            'avg_label_y': _avg_label_position(avg, target),
+            'target_label_y': _target_label_position(avg, target),
         },
         request
     )
 
     context['tbl_consumsion'] = render_to_string(
         'drinks/includes/tbl_consumsion.html',
-        {'qty': qty, 'avg': avg, 'target': target_val},
+        {'qty': qty, 'avg': avg, 'target': target},
         request)
 
     context['tbl_last_day'] = render_to_string(
