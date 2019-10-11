eeb63cd76fc5f80a6cde04f62a6bb4f9adcbfb5e
Audrius <nezinomas@gmail.com>
2019-10-11 10:09:11 +0300

    Revert "nsvrb"

--- project/core/mixins/formset.py
+++ project/core/mixins/formset.py
@@ -8,9 +8,7 @@ class FormsetMixin():
         _list = []
 
         # get self.model ForeignKey field name
-        foreign_key = [
-            f.name for f in self.model._meta.get_fields() if (f.many_to_one)
-        ]
+        foreign_key = [f.name for f in self.model._meta.get_fields() if (f.many_to_one)]
 
         if not foreign_key:
             return _list
@@ -41,8 +39,7 @@ class FormsetMixin():
             initial = self._formset_initial()
             _formset = _formset(
                 queryset=self.model.objects.none(),
-                initial=initial
-            )
+                initial=initial)
 
             _formset.extra += len(initial)
 
