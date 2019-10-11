cffc841bbdbfacb9f2df657e42c115d7510fc014
Audrius <nezinomas@gmail.com>
2019-10-11 14:48:42 +0300

    books factories

---
+++ project/books/factories.py
@@ -0,0 +1,14 @@
+from datetime import date
+
+import factory
+
+from .models import Book
+
+
+class BookFactory(factory.DjangoModelFactory):
+    class Meta:
+        model = Book
+
+    started = date(1999, 1, 1)
+    author = 'Author'
+    title = 'Book Title'
