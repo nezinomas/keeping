4b608a3e27466936b843a67ab08b741a2b028111
Audrius <nezinomas@gmail.com>
2019-10-18 10:48:11 +0300

    Book models with remark field tests

--- project/books/tests/test_models.py
+++ project/books/tests/test_models.py
@@ -25,3 +25,17 @@ def test_book_year():
     BookFactory(started=date(2000, 1, 1))
 
     assert 1 == Book.objects.year(1999).count()
+
+
+@pytest.mark.django_db
+def test_book_fields():
+    BookFactory(ended=date(1999, 1, 31))
+
+    actual = list(Book.objects.items())[0]
+
+    assert 'Author' == actual.author
+    assert 'Book Title' == actual.title
+    assert 'Remark' == actual.remark
+
+    assert date(1999, 1, 1) == actual.started
+    assert date(1999, 1, 31) == actual.ended
