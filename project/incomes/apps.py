73a3e8e850db83c99b3e47af5ff58e928062e87a
Audrius <nezinomas@gmail.com>
2019-10-22 21:29:32 +0300

    register post_save signal

--- project/incomes/apps.py
+++ project/incomes/apps.py
@@ -2,7 +2,7 @@ from django.apps import AppConfig
 
 
 class IncomesConfig(AppConfig):
-    name = 'incomes'
+    name = 'project.incomes'
 
     def ready(self):
         from ..core.signals import post_save_account_stats
