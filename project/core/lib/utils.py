53ac83297cb9ab00fdde10569bea90c6738434ce
Audrius <nezinomas@gmail.com>
2019-10-23 13:24:57 +0300

    methods for sum list of dictionaries: sum_col and sum_all tests

--- project/core/lib/utils.py
+++ project/core/lib/utils.py
@@ -1,7 +1,20 @@
-from typing import Dict
+from typing import Any, Dict, List
 
 from .date import monthname
 
 
 def get_value_from_dict(arr: Dict, month: int) -> float:
     return float(arr.get(monthname(month), 0.0)) if arr else 0.0
+
+
+def sum_col(arr: List[Dict], key: Any) -> Dict:
+    return sum(x[key] for x in arr)
+
+
+def sum_all(arr: List[Dict]) -> Dict:
+    d = {}
+    keys = arr[0].keys()
+    for key in keys:
+        d[key] = sum_col(arr, key)
+
+    return d
---
+++ project/core/tests/test_utils.py
@@ -0,0 +1,24 @@
+import pytest
+
+from ..lib import utils as T
+
+
+@pytest.fixture()
+def arr():
+    return ([
+        {'key1': 1, 'key2': 2.0},
+        {'key1': 2, 'key2': 4.0},
+    ])
+
+
+def test_sum_column(arr):
+    actual = T.sum_col(arr, 'key1')
+
+    assert 3 == actual
+
+
+def test_sum_all(arr):
+    actual = T.sum_all(arr)
+
+    assert 3 == actual['key1']
+    assert 6.0 == actual['key2']
