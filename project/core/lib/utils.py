8d41c3c5be5a913bd032bfe5421e14d1c2709be8
Audrius <nezinomas@gmail.com>
2019-10-23 15:53:35 +0300

    refactored sum_col and sum_all tests

--- project/core/lib/utils.py
+++ project/core/lib/utils.py
@@ -1,5 +1,5 @@
 from typing import Any, Dict, List
-
+from django.db.models.query import QuerySet
 from .date import monthname
 
 
@@ -7,11 +7,25 @@ def get_value_from_dict(arr: Dict, month: int) -> float:
     return float(arr.get(monthname(month), 0.0)) if arr else 0.0
 
 
-def sum_col(arr: List[Dict], key: Any) -> Dict:
-    return sum(x[key] for x in arr)
+def sum_col(arr: List[Dict], key: Any) -> float:
+    rtn = 0.0
+
+    if isinstance(arr, QuerySet):
+        arr = [*arr.values()]
+
+    for row in arr:
+        try:
+            rtn += float(row[key])
+        except:
+            break
+
+    return rtn
 
 
 def sum_all(arr: List[Dict]) -> Dict:
+    if isinstance(arr, QuerySet):
+        arr = [*arr.values()]
+
     d = {}
 
     if not arr:
--- project/core/tests/test_utils.py
+++ project/core/tests/test_utils.py
@@ -1,13 +1,15 @@
 import pytest
 
+from ...accounts.factories import AccountBalanceFactory
+from ...accounts.models import AccountBalance
 from ..lib import utils as T
 
 
 @pytest.fixture()
 def arr():
     return ([
-        {'key1': 1, 'key2': 2.0},
-        {'key1': 2, 'key2': 4.0},
+        {'key1': 1, 'key2': 2.0, 'key3': 'a'},
+        {'key1': 2, 'key2': 4.0, 'key3': 'b'},
     ])
 
 
@@ -15,6 +17,17 @@ def test_sum_column(arr):
     actual = T.sum_col(arr, 'key1')
 
     assert 3 == actual
+
+
+@pytest.mark.django_db
+def test_sum_column_query_set():
+    AccountBalanceFactory()
+
+    qs = AccountBalance.objects.all()
+
+    actual = T.sum_col(qs, 'past')
+
+    assert 1 == actual
 
 
 def test_sum_all(arr):
@@ -28,3 +41,14 @@ def test_sum_all_empty_list():
     actual = T.sum_all([])
 
     assert {} == actual
+
+
+@pytest.mark.django_db
+def test_sum_all_query_set():
+    AccountBalanceFactory()
+
+    qs = AccountBalance.objects.all()
+
+    actual = T.sum_all(qs)
+
+    assert 1 == actual['past']
