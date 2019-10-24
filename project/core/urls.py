edef5b3ee7f2024b6db2c0120348e9860bebbd2a
Audrius <nezinomas@gmail.com>
2019-10-24 14:31:02 +0300

    regenerate accounts and savings views tests

--- project/core/urls.py
+++ project/core/urls.py
@@ -13,4 +13,20 @@ urlpatterns = [
         'set/month/<int:month>/<str:view_name>/',
         views.set_month,
         name='set_month'),
+    path(
+        'set/accounts/',
+        views.regenerate_accounts_balance,
+        name='regenerate_accounts'),
+    path(
+        'set/accounts/<int:year>/',
+        views.regenerate_accounts_balance_current_year,
+        name='regenerate_accounts_current_year'),
+    path(
+        'set/savings/',
+        views.regenerate_savings_balance,
+        name='regenerate_savings'),
+    path(
+        'set/savings/<int:year>/',
+        views.regenerate_savings_balance_current_year,
+        name='regenerate_savings_current_year'),
 ]
--- project/core/views.py
+++ project/core/views.py
@@ -1,5 +1,10 @@
+from types import SimpleNamespace
+
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import redirect, render, reverse
+
+from ..core.signals import _account_update_or_create
+from .lib.date import years
 
 
 @login_required()
@@ -26,3 +31,38 @@ def set_month(request, month, view_name):
 @login_required
 def index(request):
     return render(request, 'core/index.html')
+
+
+@login_required
+def regenerate_accounts_balance(request):
+    _years = years()
+
+    for year in _years:
+        _account_update_or_create(SimpleNamespace(), year)
+
+    return redirect(
+        reverse('bookkeeping:index', kwargs={})
+    )
+
+
+@login_required
+def regenerate_accounts_balance_current_year(request, year):
+    _account_update_or_create(SimpleNamespace(), year)
+
+    return redirect(
+        reverse('bookkeeping:index', kwargs={})
+    )
+
+
+@login_required
+def regenerate_savings_balance(request):
+    return redirect(
+        reverse('bookkeeping:index', kwargs={})
+    )
+
+
+@login_required
+def regenerate_savings_balance_current_year(request, year):
+    return redirect(
+        reverse('bookkeeping:index', kwargs={})
+    )
