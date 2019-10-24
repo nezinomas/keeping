059cb4f33536bd3d086f4049a7e60695a137c53e
Audrius <nezinomas@gmail.com>
2019-10-24 13:00:41 +0300

    refactoring, too long lines

--- project/core/urls.py
+++ project/core/urls.py
@@ -5,6 +5,12 @@ app_name = 'core'
 
 urlpatterns = [
     path('core/', views.index, name='core_index'),
-    path('set/year/<int:year>/<str:view_name>/', views.set_year, name='set_year'),
-    path('set/month/<int:month>/<str:view_name>/', views.set_month, name='set_month'),
+    path(
+        'set/year/<int:year>/<str:view_name>/',
+        views.set_year,
+        name='set_year'),
+    path(
+        'set/month/<int:month>/<str:view_name>/',
+        views.set_month,
+        name='set_month'),
 ]
