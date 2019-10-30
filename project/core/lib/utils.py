d822da265991afd3f049094482e62f451c8d0468
Audrius <nezinomas@gmail.com>
2019-10-30 22:30:08 +0200

    refactored sum_all and sum_col functions

--- project/core/lib/utils.py
+++ project/core/lib/utils.py
@@ -1,5 +1,8 @@
+from collections import Counter
 from typing import Any, Dict, List
+
 from django.db.models.query import QuerySet
+
 from .date import monthname
 
 
@@ -7,32 +10,20 @@ def get_value_from_dict(arr: Dict, month: int) -> float:
     return float(arr.get(monthname(month), 0.0)) if arr else 0.0
 
 
-def sum_col(arr: List[Dict], key: Any) -> float:
-    rtn = 0.0
-
-    if isinstance(arr, QuerySet):
-        arr = [*arr.values()]
-
-    for row in arr:
-        try:
-            rtn += float(row[key])
-        except:
-            break
-
-    return rtn
-
-
 def sum_all(arr: List[Dict]) -> Dict:
     if isinstance(arr, QuerySet):
         arr = [*arr.values()]
 
-    d = {}
-
-    if not arr:
-        return d
-
-    keys = arr[0].keys()
-    for key in keys:
-        d[key] = sum_col(arr, key)
+    d = Counter()
+    {d.update(row) for row in arr}
 
     return d
+
+
+def sum_col(arr: List[Dict], key: Any) -> float:
+    if isinstance(arr, QuerySet):
+        arr = [*arr.values()]
+
+    rtn = sum_all(arr)
+
+    return rtn.get(key, 0.0)
