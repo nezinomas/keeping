790d2e8e9f7fde80220ed52272b2c198e6de5b9c
Audrius <nezinomas@gmail.com>
2019-11-04 12:54:28 +0200

    nsvrb

--- project/bookkeeping/tests/test_views.py
+++ project/bookkeeping/tests/test_views.py
@@ -12,28 +12,15 @@ from .. import views
 X_Req = {'HTTP_X_REQUESTED_WITH': 'XMLHttpRequest'}
 
 
+#
+# =============================================================
+#                                                         Index
+# =============================================================
+#
 def test_view_index_func():
     view = resolve('/')
 
     assert views.Index == view.func.view_class
-
-
-def test_view_month_func():
-    view = resolve('/month/')
-
-    assert views.Month == view.func.view_class
-
-
-def test_view_savings_worth_func():
-    view = resolve('/bookkeeping/savings_worth/new/')
-
-    assert views.SavingsWorthNew == view.func.view_class
-
-
-def test_view_accounts_worth_func():
-    view = resolve('/bookkeeping/accounts_worth/new/')
-
-    assert views.AccountsWorthNew == view.func.view_class
 
 
 @pytest.mark.django_db
@@ -43,6 +30,17 @@ def test_view_index_200(login, client):
     assert response.status_code == 200
 
 
+#
+# =============================================================
+#                                                         Month
+# =============================================================
+#
+def test_view_month_func():
+    view = resolve('/month/')
+
+    assert views.Month == view.func.view_class
+
+
 @pytest.mark.django_db
 def test_view_month_200(login, client):
     response = client.get('/month/')
@@ -50,16 +48,93 @@ def test_view_month_200(login, client):
     assert response.status_code == 200
 
 
-@pytest.mark.django_db
-def test_view_saving_worth_200(login, client):
-    response = client.get('/bookkeeping/savings_worth/new/')
+#
+# =============================================================
+#                                                 Account Worth
+# =============================================================
+#
+def test_view_accounts_worth_func():
+    view = resolve('/bookkeeping/accounts_worth/new/')
 
-    assert response.status_code == 200
+    assert views.AccountsWorthNew == view.func.view_class
 
 
 @pytest.mark.django_db
 def test_view_account_worth_200(login, client):
     response = client.get('/bookkeeping/accounts_worth/new/')
+
+    assert response.status_code == 200
+
+
+@pytest.mark.django_db
+def test_view_account_worth_formset(login, client):
+    AccountFactory()
+
+    url = reverse('bookkeeping:accounts_worth_new')
+    response = client.get(url, {}, **X_Req)
+
+    json_str = response.content
+    actual = json.loads(json_str)
+
+    assert 200 == response.status_code
+    assert 'Sąskaitų vertė' in actual['html_form']
+    assert '<option value="1" selected>Account1</option>' in actual['html_form']
+
+
+@pytest.mark.django_db()
+def test_view_account_worth_new(client, login):
+    i = AccountFactory()
+    data = {
+        'form-TOTAL_FORMS': 1,
+        'form-INITIAL_FORMS': 0,
+        'form-0-price': '999',
+        'form-0-account': i.pk
+    }
+
+    url = reverse('bookkeeping:accounts_worth_new')
+
+    response = client.post(url, data, **X_Req)
+
+    json_str = response.content
+    actual = json.loads(json_str)
+
+    assert actual['form_is_valid']
+    assert '999' in actual['html_list']
+
+
+@pytest.mark.django_db()
+def test_view_account_worth_invalid_data(client, login):
+    data = {
+        'form-TOTAL_FORMS': 1,
+        'form-INITIAL_FORMS': 0,
+        'form-0-price': 'x',
+        'form-0-account': 0
+    }
+
+    url = reverse('bookkeeping:accounts_worth_new')
+
+    response = client.post(url, data, **X_Req)
+
+    json_str = response.content
+    actual = json.loads(json_str)
+
+    assert not actual['form_is_valid']
+
+
+#
+# =============================================================
+#                                                  Saving Worth
+# =============================================================
+#
+def test_view_savings_worth_func():
+    view = resolve('/bookkeeping/savings_worth/new/')
+
+    assert views.SavingsWorthNew == view.func.view_class
+
+
+@pytest.mark.django_db
+def test_view_saving_worth_200(login, client):
+    response = client.get('/bookkeeping/savings_worth/new/')
 
     assert response.status_code == 200
 
@@ -110,61 +185,6 @@ def test_view_saving_worth_invalid_data(client, login):
     }
 
     url = reverse('bookkeeping:savings_worth_new')
-
-    response = client.post(url, data, **X_Req)
-
-    json_str = response.content
-    actual = json.loads(json_str)
-
-    assert not actual['form_is_valid']
-
-
-@pytest.mark.django_db
-def test_view_account_worth_formset(login, client):
-    AccountFactory()
-
-    url = reverse('bookkeeping:accounts_worth_new')
-    response = client.get(url, {}, **X_Req)
-
-    json_str = response.content
-    actual = json.loads(json_str)
-
-    assert 200 == response.status_code
-    assert 'Sąskaitų vertė' in actual['html_form']
-    assert '<option value="1" selected>Account1</option>' in actual['html_form']
-
-
-@pytest.mark.django_db()
-def test_view_account_worth_new(client, login):
-    i = AccountFactory()
-    data = {
-        'form-TOTAL_FORMS': 1,
-        'form-INITIAL_FORMS': 0,
-        'form-0-price': '999',
-        'form-0-account': i.pk
-    }
-
-    url = reverse('bookkeeping:accounts_worth_new')
-
-    response = client.post(url, data, **X_Req)
-
-    json_str = response.content
-    actual = json.loads(json_str)
-
-    assert actual['form_is_valid']
-    assert '999' in actual['html_list']
-
-
-@pytest.mark.django_db()
-def test_view_account_worth_invalid_data(client, login):
-    data = {
-        'form-TOTAL_FORMS': 1,
-        'form-INITIAL_FORMS': 0,
-        'form-0-price': 'x',
-        'form-0-account': 0
-    }
-
-    url = reverse('bookkeeping:accounts_worth_new')
 
     response = client.post(url, data, **X_Req)
 
