17901d765cb128db8056f9c6771192edb0c80bf4
Audrius <nezinomas@gmail.com>
2019-11-07 13:54:26 +0200

    changes in production settings

--- project/config/settings/production.py
+++ project/config/settings/production.py
@@ -7,9 +7,33 @@ TEMPLATE_DEBUG = DEBUG
 
 
 # ================   project CONFIGURATION
-ALLOWED_HOSTS = ['*']
-
+ALLOWED_HOSTS = env.list('DJANGO_ALLOWED_HOSTS',
+                         default=['buh.unknownbug.net', ])
 
 # ================   APP CONFIGURATION
 INSTALLED_APPS += [
 ]
+
+
+# ================   TEMPLATE CONFIGURATION
+TEMPLATES[0]['OPTIONS']['loaders'] = [
+    ['django.template.loaders.cached.Loader', [
+            'django.template.loaders.filesystem.Loader',
+            'django.template.loaders.app_directories.Loader', ], ],
+]
+
+
+# security configuration
+SECURE_HSTS_SECONDS = 60
+SECURE_HSTS_INCLUDE_SUBDOMAINS = env.bool(
+    'DJANGO_SECURE_HSTS_INCLUDE_SUBDOMAINS', default=True)
+SECURE_CONTENT_TYPE_NOSNIFF = env.bool(
+    'DJANGO_SECURE_CONTENT_TYPE_NOSNIFF', default=True)
+SECURE_BROWSER_XSS_FILTER = True
+SESSION_COOKIE_SECURE = True
+SESSION_COOKIE_HTTPONLY = True
+SECURE_SSL_REDIRECT = env.bool('DJANGO_SECURE_SSL_REDIRECT', default=True)
+CSRF_COOKIE_SECURE = True
+CSRF_COOKIE_HTTPONLY = True
+X_FRAME_OPTIONS = 'DENY'
+ADMIN_URL = env('DJANGO_ADMIN_URL')
