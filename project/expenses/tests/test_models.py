59e36521a361b417096a38f103dfdcc676f8cc1a
Audrius <nezinomas@gmail.com>
2019-11-13 21:21:32 +0200

    changed methods places in file

--- project/expenses/tests/test_models.py
+++ project/expenses/tests/test_models.py
@@ -5,6 +5,7 @@ import pytest
 
 from ...accounts.factories import AccountFactory
 from ...accounts.models import AccountBalance
+from ...auths.factories import UserFactory
 from ...expenses.factories import ExpenseFactory
 from ..factories import ExpenseFactory, ExpenseNameFactory, ExpenseTypeFactory
 from ..models import Expense, ExpenseName, ExpenseType
@@ -20,6 +21,15 @@ def expenses_more():
         account=AccountFactory(title='Account1'),
         exception=True
     )
+
+
+# ----------------------------------------------------------------------------
+#                                                                 Expense Type
+# ----------------------------------------------------------------------------
+def test_expense_type_str():
+    e = ExpenseTypeFactory.build()
+
+    assert 'Expense Type' == str(e)
 
 
 def test_month_expense_type(expenses):
@@ -53,58 +63,6 @@ def test_day_expense_type(expenses_january):
     assert expect == actual
 
 
-def test_expense_str():
-    e = ExpenseFactory.build()
-
-    assert '1999-01-01/Expense Type/Expense Name' == str(e)
-
-
-def test_expense_type_str():
-    e = ExpenseTypeFactory.build()
-
-    assert 'Expense Type' == str(e)
-
-
-def test_expnese_name_str():
-    e = ExpenseNameFactory.build()
-
-    assert 'Expense Name' == str(e)
-
-
-def test_expense_year():
-    ExpenseFactory(date=date(1999, 1, 1))
-    ExpenseFactory(date=date(2000, 1, 1))
-
-    actual = Expense.objects.year(2000)
-
-    assert 1 == actual.count()
-
-
-def test_expense_year_query_count(django_assert_max_num_queries):
-    ExpenseFactory(date=date(1999, 1, 1))
-    ExpenseFactory(date=date(2000, 1, 1))
-
-    with django_assert_max_num_queries(1):
-        list(Expense.objects.year(2000).values())
-
-
-def test_expense_items():
-    ExpenseFactory(date=date(1999, 1, 1))
-    ExpenseFactory(date=date(2000, 1, 1))
-
-    actual = Expense.objects.items()
-
-    assert 2 == actual.count()
-
-
-def test_expense_items_query_count(django_assert_max_num_queries):
-    ExpenseFactory(date=date(1999, 1, 1))
-    ExpenseFactory(date=date(2000, 1, 1))
-
-    with django_assert_max_num_queries(1):
-        list(Expense.objects.items().values('expense_type__title'))
-
-
 def test_expense_type_items():
     ExpenseTypeFactory(title='T1')
     ExpenseTypeFactory(title='T2')
@@ -120,6 +78,24 @@ def test_expense_type_items_query_count(django_assert_max_num_queries):
 
     with django_assert_max_num_queries(1):
         list(ExpenseType.objects.items().values())
+
+
+def test_post_save_expense_type_insert_new(mock_crequest, expenses):
+    obj = ExpenseType(title='e1')
+    obj.save()
+
+    actual = AccountBalance.objects.items()
+
+    assert 2 == actual.count()
+
+
+# ----------------------------------------------------------------------------
+#                                                                 Expense Name
+# ----------------------------------------------------------------------------
+def test_expnese_name_str():
+    e = ExpenseNameFactory.build()
+
+    assert 'Expense Name' == str(e)
 
 
 def test_expense_name_items():
@@ -174,26 +150,49 @@ def test_expense_name_no_dublicates():
     ExpenseName(title='N1', parent=p1).save()
 
 
-def test_summary(expenses):
-    expect = [{
-        'title': 'Account1',
-        'e_past': 2.5,
-        'e_now': 0.5,
-
-    }, {
-        'title': 'Account2',
-        'e_past': 2.25,
-        'e_now': 1.25,
-    }]
-
-    actual = [*Expense.objects.summary(1999).order_by('account__title')]
-
-    assert expect == actual
-
-
 # ----------------------------------------------------------------------------
-#                                                             post_save signal
+#                                                                      Expense
 # ----------------------------------------------------------------------------
+def test_expense_str():
+    e = ExpenseFactory.build()
+
+    assert '1999-01-01/Expense Type/Expense Name' == str(e)
+
+
+def test_expense_year():
+    ExpenseFactory(date=date(1999, 1, 1))
+    ExpenseFactory(date=date(2000, 1, 1))
+
+    actual = Expense.objects.year(2000)
+
+    assert 1 == actual.count()
+
+
+def test_expense_year_query_count(django_assert_max_num_queries):
+    ExpenseFactory(date=date(1999, 1, 1))
+    ExpenseFactory(date=date(2000, 1, 1))
+
+    with django_assert_max_num_queries(1):
+        list(Expense.objects.year(2000).values())
+
+
+def test_expense_items():
+    ExpenseFactory(date=date(1999, 1, 1))
+    ExpenseFactory(date=date(2000, 1, 1))
+
+    actual = Expense.objects.items()
+
+    assert 2 == actual.count()
+
+
+def test_expense_items_query_count(django_assert_max_num_queries):
+    ExpenseFactory(date=date(1999, 1, 1))
+    ExpenseFactory(date=date(2000, 1, 1))
+
+    with django_assert_max_num_queries(1):
+        list(Expense.objects.items().values('expense_type__title'))
+
+
 def test_month_name_sum():
     ExpenseFactory(
         date=date(1974, 1, 1),
@@ -237,13 +236,18 @@ def test_month_name_sum():
     assert expect == [*actual]
 
 
-# ----------------------------------------------------------------------------
-#                                                             post_save signal
-# ----------------------------------------------------------------------------
-def test_post_save_expense_type_insert_new(mock_crequest, expenses):
-    obj = ExpenseType(title='e1')
-    obj.save()
+def test_summary(expenses):
+    expect = [{
+        'title': 'Account1',
+        'e_past': 2.5,
+        'e_now': 0.5,
 
-    actual = AccountBalance.objects.items()
+    }, {
+        'title': 'Account2',
+        'e_past': 2.25,
+        'e_now': 1.25,
+    }]
 
-    assert 2 == actual.count()
+    actual = [*Expense.objects.summary(1999).order_by('account__title')]
+
+    assert expect == actual
