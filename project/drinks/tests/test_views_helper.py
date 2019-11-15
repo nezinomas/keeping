b9ab52b7c83456dc72eccb4b5ff8ff374f2b1c78
Audrius <nezinomas@gmail.com>
2019-11-15 22:56:23 +0200

    test for _avg_label_position and _target_label_position

--- project/drinks/tests/test_views_helper.py
+++ project/drinks/tests/test_views_helper.py
@@ -25,3 +25,39 @@ def test_dry_days_no_records(get_user):
     actual = T._dry_days(2000)
 
     assert actual == {}
+
+
+def test_target_label_position_between():
+    actual = T._target_label_position(avg=55, target=50)
+
+    assert actual == 15
+
+
+def test_target_label_position_higher():
+    actual = T._target_label_position(avg=55, target=500)
+
+    assert actual == -5
+
+
+def test_target_label_position_lower():
+    actual = T._target_label_position(avg=500, target=50)
+
+    assert actual == -5
+
+
+def test_avg_label_position_between():
+    actual = T._avg_label_position(avg=50, target=55)
+
+    assert actual == 15
+
+
+def test_avg_label_position_higher():
+    actual = T._avg_label_position(avg=55, target=500)
+
+    assert actual == -5
+
+
+def test_avg_label_position_lower():
+    actual = T._avg_label_position(avg=500, target=50)
+
+    assert actual == -5
