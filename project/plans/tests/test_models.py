27874942abefa04e893c6c57abeb69f64c741332
Audrius <nezinomas@gmail.com>
2019-11-18 09:52:41 +0200

    imports

--- project/plans/tests/test_models.py
+++ project/plans/tests/test_models.py
@@ -1,9 +1,14 @@
 import pytest
 
+from ...auths.factories import UserFactory
 from ...expenses.factories import ExpenseTypeFactory
 from ...incomes.factories import IncomeTypeFactory
 from ...savings.factories import SavingTypeFactory
-from .. import factories, models
+from ..factories import (
+    DayPlanFactory, ExpensePlanFactory, IncomePlanFactory,
+    NecessaryPlanFactory, SavingPlanFactory)
+from ..models import (DayPlan, ExpensePlan, IncomePlan, NecessaryPlan,
+                      SavingPlan)
 
 pytestmark = pytest.mark.django_db
 
@@ -12,7 +17,7 @@ pytestmark = pytest.mark.django_db
 #                                                                  Income Plan
 # ----------------------------------------------------------------------------
 def test_income_str():
-    actual = factories.IncomePlanFactory.build(year=2000)
+    actual = IncomePlanFactory.build(year=2000)
 
     assert str(actual) == '2000/Income Type'
 
@@ -20,15 +25,15 @@ def test_income_str():
 @pytest.mark.xfail
 def test_income_no_dublicates():
     type_ = IncomeTypeFactory(title='X')
-    models.IncomePlan(year=2000, income_type=type_).save()
-    models.IncomePlan(year=2000, income_type=type_).save()
+    IncomePlan(year=2000, income_type=type_).save()
+    IncomePlan(year=2000, income_type=type_).save()
 
 
 # ----------------------------------------------------------------------------
 #                                                                 Expense Plan
 # ----------------------------------------------------------------------------
 def test_expense_str():
-    actual = factories.ExpensePlanFactory.build(year=2000)
+    actual = ExpensePlanFactory.build(year=2000)
 
     assert str(actual) == '2000/Expense Type'
 
@@ -36,36 +41,36 @@ def test_expense_str():
 @pytest.mark.xfail
 def test_expense_no_dublicates():
     type_ = ExpenseTypeFactory(title='X')
-    models.ExpensePlan(year=2000, expense_type=type_).save()
-    models.ExpensePlan(year=2000, expense_type=type_).save()
+    ExpensePlan(year=2000, expense_type=type_).save()
+    ExpensePlan(year=2000, expense_type=type_).save()
 
 
 def test_expense_year():
-    factories.ExpensePlanFactory(year=1999)
-    factories.ExpensePlanFactory(year=2010)
+    ExpensePlanFactory(year=1999)
+    ExpensePlanFactory(year=2010)
 
-    actual = models.ExpensePlan.objects.year(2010)
+    actual = ExpensePlan.objects.year(2010)
 
     assert actual.count() == 1
 
 
 def test_expense_year_query_count(django_assert_max_num_queries):
     with django_assert_max_num_queries(1):
-        list(models.ExpensePlan.objects.year(1999))
+        list(ExpensePlan.objects.year(1999))
 
 
 def test_expense_items():
-    factories.ExpensePlanFactory(year=1999)
-    factories.ExpensePlanFactory(year=2010)
+    ExpensePlanFactory(year=1999)
+    ExpensePlanFactory(year=2010)
 
-    actual = models.ExpensePlan.objects.items()
+    actual = ExpensePlan.objects.items()
 
     assert actual.count() == 1
 
 
 def test_expense_items_query_count(django_assert_max_num_queries):
     with django_assert_max_num_queries(1):
-        list(models.ExpensePlan.objects.items())
+        list(ExpensePlan.objects.items())
 
 
 
@@ -73,7 +78,7 @@ def test_expense_items_query_count(django_assert_max_num_queries):
 #                                                                  Saving Plan
 # ----------------------------------------------------------------------------
 def test_saving_str():
-    actual = factories.SavingPlanFactory.build(year=2000)
+    actual = SavingPlanFactory.build(year=2000)
 
     assert str(actual) == '2000/Savings'
 
@@ -81,35 +86,35 @@ def test_saving_str():
 @pytest.mark.xfail
 def test_saving_no_dublicates():
     type_ = SavingTypeFactory(title='X')
-    models.SavingPlan(year=2000, saving_type=type_).save()
-    models.SavingPlan(year=2000, saving_type=type_).save()
+    SavingPlan(year=2000, saving_type=type_).save()
+    SavingPlan(year=2000, saving_type=type_).save()
 
 
 # ----------------------------------------------------------------------------
 #                                                                     Day Plan
 # ----------------------------------------------------------------------------
 def test_day_str():
-    actual = factories.DayPlanFactory.build(year=2000)
+    actual = DayPlanFactory.build(year=2000)
 
     assert str(actual) == '2000'
 
 
 @pytest.mark.xfail(raises=Exception)
 def test_day_no_dublicates():
-    factories.DayPlanFactory(year=2000)
-    factories.DayPlanFactory(year=2000)
+    DayPlanFactory(year=2000)
+    DayPlanFactory(year=2000)
 
 
 # ----------------------------------------------------------------------------
 #                                                               Necessary Plan
 # ----------------------------------------------------------------------------
 def test_necessary_str():
-    actual = factories.NecessaryPlanFactory.build(year=2000, title='N')
+    actual = NecessaryPlanFactory.build(year=2000, title='N')
 
     assert str(actual) == '2000/N'
 
 
 @pytest.mark.xfail(raises=Exception)
 def test_necessary_no_dublicates():
-    factories.NecessaryPlanFactory(year=2000, title='N')
-    factories.NecessaryPlanFactory(year=2000, title='N')
+    NecessaryPlanFactory(year=2000, title='N')
+    NecessaryPlanFactory(year=2000, title='N')
