dbebaaa4673c5634602dc0e6043a4608e2edf15c
Audrius <nezinomas@gmail.com>
2019-11-18 17:15:59 +0200

    books models with multiuser

--- project/books/models.py
+++ project/books/models.py
@@ -1,13 +1,28 @@
-from django.db import models
 from django.core.validators import MinLengthValidator
+from django.db import models
+
+from ..auths.models import User
+from ..core.lib import utils
 
 
 class BooksQuerySet(models.QuerySet):
+    def related(self):
+        user = utils.get_user()
+        return (
+            self
+            .select_related('user')
+            .filter(user=user)
+        )
+
     def year(self, year):
-        return self.filter(started__year=year)
+        return (
+            self
+            .related()
+            .filter(started__year=year)
+        )
 
     def items(self):
-        return self.all()
+        return self.related()
 
 
 class Book(models.Model):
@@ -27,6 +42,11 @@ class Book(models.Model):
     remark = models.TextField(
         max_length=200,
         blank=True
+    )
+    user = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name='books'
     )
 
     # objects = BookManager()
--- project/books/tests/test_models.py
+++ project/books/tests/test_models.py
@@ -2,6 +2,7 @@ from datetime import date
 
 import pytest
 
+from ...auths.factories import UserFactory
 from ..factories import BookFactory
 from ..models import Book
 
@@ -13,21 +14,32 @@ def test_book_str():
 
     assert str(book) == 'Book Title'
 
-
-def test_book_items():
+def test_book_related(get_user):
     BookFactory()
+    BookFactory(title='B1', user=UserFactory(username='XXX'))
+
+    actual = Book.objects.related()
+
+    assert len(actual) == 1
+    assert actual[0].title == 'Book Title'
+
+
+def test_book_items(get_user):
+    BookFactory()
+    BookFactory(title='B1', user=UserFactory(username='XXX'))
 
     assert Book.objects.items().count() == 1
 
 
-def test_book_year():
+def test_book_year(get_user):
     BookFactory()
+    BookFactory(user=UserFactory(username='XXX'))
     BookFactory(started=date(2000, 1, 1))
 
     assert Book.objects.year(1999).count() == 1
 
 
-def test_book_fields():
+def test_book_fields(get_user):
     BookFactory(ended=date(1999, 1, 31))
 
     actual = list(Book.objects.items())[0]
