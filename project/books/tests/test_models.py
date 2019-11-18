768e5ec93193933393505aaa4313db0fe5c34230
Audrius <nezinomas@gmail.com>
2019-11-18 17:04:42 +0200

    books test_models.py revisited with pylint

--- project/books/tests/test_models.py
+++ project/books/tests/test_models.py
@@ -5,37 +5,36 @@ import pytest
 from ..factories import BookFactory
 from ..models import Book
 
+pytestmark = pytest.mark.django_db
+
 
 def test_book_str():
     book = BookFactory.build()
 
-    assert 'Book Title' == str(book)
+    assert str(book) == 'Book Title'
 
 
-@pytest.mark.django_db
 def test_book_items():
     BookFactory()
 
-    assert 1 == Book.objects.items().count()
+    assert Book.objects.items().count() == 1
 
 
-@pytest.mark.django_db
 def test_book_year():
     BookFactory()
     BookFactory(started=date(2000, 1, 1))
 
-    assert 1 == Book.objects.year(1999).count()
+    assert Book.objects.year(1999).count() == 1
 
 
-@pytest.mark.django_db
 def test_book_fields():
     BookFactory(ended=date(1999, 1, 31))
 
     actual = list(Book.objects.items())[0]
 
-    assert 'Author' == actual.author
-    assert 'Book Title' == actual.title
-    assert 'Remark' == actual.remark
+    assert actual.author == 'Author'
+    assert actual.title == 'Book Title'
+    assert actual.remark == 'Remark'
 
     assert date(1999, 1, 1) == actual.started
     assert date(1999, 1, 31) == actual.ended
