031d53ac2f9de7e7278e66dce2366d9210bb4324
Audrius <nezinomas@gmail.com>
2019-11-19 17:10:41 +0200

    test_views.py revisited with pylint

--- project/incomes/tests/test_views.py
+++ project/incomes/tests/test_views.py
@@ -1,8 +1,5 @@
 import json
-from datetime import date, datetime
-from decimal import Decimal
 
-import pandas as pd
 import pytest
 from django.urls import resolve, reverse
 from freezegun import freeze_time
@@ -14,6 +11,9 @@ from ..factories import IncomeFactory, IncomeTypeFactory
 X_Req = {'HTTP_X_REQUESTED_WITH': 'XMLHttpRequest'}
 
 
+# ----------------------------------------------------------------------------
+#                                                                      Incomes
+# ----------------------------------------------------------------------------
 def test_incomes_index_func():
     view = resolve('/incomes/')
 
@@ -66,7 +66,7 @@ def test_income_load_form(client_logged):
     json_str = response.content
     actual = json.loads(json_str)
 
-    assert 200 == response.status_code
+    assert response.status_code == 200
     assert '2000-01-01' in actual['html_form']
 
 
@@ -129,7 +129,7 @@ def test_income_update_to_another_year(client_logged):
 
     response = client_logged.post(url, data, **X_Req)
 
-    assert 200 == response.status_code
+    assert response.status_code == 200
 
     json_str = response.content
     actual = json.loads(json_str)
@@ -153,7 +153,7 @@ def test_income_update(client_logged):
 
     response = client_logged.post(url, data, **X_Req)
 
-    assert 200 == response.status_code
+    assert response.status_code == 200
 
     json_str = response.content
     actual = json.loads(json_str)
@@ -164,10 +164,9 @@ def test_income_update(client_logged):
     assert 'Pastaba' in actual['html_list']
 
 
-#
-# IncomeType
-#
-
+# ----------------------------------------------------------------------------
+#                                                                 Income Type
+# ----------------------------------------------------------------------------
 @freeze_time('2000-01-01')
 @pytest.mark.django_db()
 def test_type_load_form(client_logged):
@@ -175,10 +174,7 @@ def test_type_load_form(client_logged):
 
     response = client_logged.get(url, {}, **X_Req)
 
-    json_str = response.content
-    actual = json.loads(json_str)
-
-    assert 200 == response.status_code
+    assert response.status_code == 200
 
 
 @pytest.mark.django_db()
@@ -221,7 +217,7 @@ def test_type_update(client_logged):
 
     response = client_logged.post(url, data, **X_Req)
 
-    assert 200 == response.status_code
+    assert response.status_code == 200
 
     json_str = response.content
     actual = json.loads(json_str)
