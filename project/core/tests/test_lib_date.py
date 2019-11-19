1bbb98c94023519becf66dac3d0894de896c9aa8
Audrius <nezinomas@gmail.com>
2019-11-19 10:36:32 +0200

    upgraded tests

--- project/core/tests/test_lib_date.py
+++ project/core/tests/test_lib_date.py
@@ -1,13 +1,12 @@
-from datetime import date, datetime
+from datetime import date
 
-import pytest
 from freezegun import freeze_time
 
-from ..lib.date import *
+from ..lib import date as T
 
 
 def test_year_month_list():
-    actual = year_month_list(1970)
+    actual = T.year_month_list(1970)
 
     assert len(actual) == 12
     assert actual[0] == date(1970, 1, 1)
@@ -16,7 +15,7 @@ def test_year_month_list():
 
 @freeze_time("1999-01-11")
 def test_year_month_list_year_none():
-    actual = year_month_list(None)
+    actual = T.year_month_list(None)
 
     assert len(actual) == 12
     assert actual[0] == date(1999, 1, 1)
@@ -25,20 +24,20 @@ def test_year_month_list_year_none():
 
 @freeze_time("1970-01-11")
 def test_current_day():
-    actual = current_day(1970, 1)
+    actual = T.current_day(1970, 1)
 
     assert actual == 11
 
 
 @freeze_time("1970-01-11")
-def test_current_day():
-    actual = current_day(1970, 12)
+def test_current_day_1():
+    actual = T.current_day(1970, 12)
 
     assert actual == 31
 
 
 @freeze_time("1970-01-11")
-def test_current_day():
-    actual = current_day(2020, 2)
+def test_current_day_2():
+    actual = T.current_day(2020, 2)
 
     assert actual == 29
