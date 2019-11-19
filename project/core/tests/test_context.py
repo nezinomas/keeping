268fc9c62813ab2e681f016c2266ccf98ede20e6
Audrius <nezinomas@gmail.com>
2019-11-19 12:43:28 +0200

    upgraded tests after resolved #24

--- project/core/tests/test_context.py
+++ project/core/tests/test_context.py
@@ -6,10 +6,10 @@ from ..context import years
 
 @freeze_time('2006-01-01')
 def test_years(rf):
-    expect = {'years': [2007, 2006, 2005, 2004]}
+    expect = {'years': [2007, 2006]}
     r = rf.get('/fake/')
 
     actual = years(r)
 
-    assert 4 == len(actual['years'])
+    assert len(actual['years']) == 2
     assert expect == actual
