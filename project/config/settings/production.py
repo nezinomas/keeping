89f43c75cf465aeae5c889ac174064c4bb208e11
Audrius <nezinomas@gmail.com>
2019-11-19 21:29:35 +0200

    deleted comments

--- project/config/settings/production.py
+++ project/config/settings/production.py
@@ -1,29 +1,19 @@
 from .base import *
 
-
-# ================   DEBUG CONFIGURATION
 DEBUG = False
 TEMPLATE_DEBUG = DEBUG
 
-
-# ================   project CONFIGURATION
 ALLOWED_HOSTS = ['buh.unknownbug.net']
 
+INSTALLED_APPS += []
 
-# ================   APP CONFIGURATION
-INSTALLED_APPS += [
-]
-
-
-# ================   TEMPLATE CONFIGURATION
 TEMPLATES[0]['OPTIONS']['loaders'] = [
     ['django.template.loaders.cached.Loader', [
-            'django.template.loaders.filesystem.Loader',
-            'django.template.loaders.app_directories.Loader', ], ],
+        'django.template.loaders.filesystem.Loader',
+        'django.template.loaders.app_directories.Loader', ],
+    ],
 ]
 
-
-# security configuration
 SECURE_HSTS_SECONDS = 60
 SECURE_HSTS_INCLUDE_SUBDOMAINS = True
 SECURE_CONTENT_TYPE_NOSNIFF = True
