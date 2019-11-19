1fbbfe47b8a539ef2099b062f5c4c926e237ee98
Audrius <nezinomas@gmail.com>
2019-11-19 11:08:38 +0200

    method years start date not hardcoded, but start from date_joined

--- project/core/tests/test_lib_date.py
+++ project/core/tests/test_lib_date.py
@@ -1,6 +1,9 @@
 from datetime import date
 
+import pytest
 from freezegun import freeze_time
+from mock import patch
+from types import SimpleNamespace
 
 from ..lib import date as T
 
@@ -41,3 +44,22 @@ def test_current_day_2():
     actual = T.current_day(2020, 2)
 
     assert actual == 29
+
+
+@freeze_time("2001-01-01")
+@pytest.mark.django_db
+def test_years_user_logged(get_user):
+    get_user.date_joined = date(1999, 1, 1)
+
+    actual = T.years()
+
+    assert actual == [1999, 2000, 2001, 2002]
+
+
+@pytest.mark.django_db
+@freeze_time("2001-01-01")
+@patch('project.core.lib.utils.get_user', return_value=SimpleNamespace())
+def test_years_user_anonymous_user(mck):
+    actual = T.years()
+
+    assert actual == [2001, 2002]
