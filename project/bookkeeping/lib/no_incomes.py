0f15400570d9817359dc6aa6e027b906740fbd99
Audrius <nezinomas@gmail.com>
2020-01-27 14:30:16 +0200

    refactoring no_incomes class

--- project/bookkeeping/lib/no_incomes.py
+++ project/bookkeeping/lib/no_incomes.py
@@ -1,4 +1,4 @@
-from dataclasses import dataclass, field
+from dataclasses import dataclass
 from typing import Dict, List
 
 
@@ -8,14 +8,13 @@ class NoIncomes():
     fund: float
     pension: float
     avg_expenses: float
-    avg_type_expenses: Dict[str, float] = field(default_factory=dict)
-    not_use: List[str] = field(default_factory=list)
+    cut_sum: float
 
     @property
     def summary(self) -> List[Dict[str, float]]:
         i1 = self.money + self.fund
         i2 = i1 + self.pension
-        save_sum = 0 if not self.save_sum else self.save_sum
+        cut_sum = 0 if not self.cut_sum else self.cut_sum
 
         return [{
             'label': 'money',
@@ -27,21 +26,9 @@ class NoIncomes():
             'money_fund_pension': self._div(i2, self.avg_expenses)
         }, {
             'label': 'cut',
-            'money_fund': self._div(i1, (self.avg_expenses - save_sum)),
-            'money_fund_pension': self._div(i2, (self.avg_expenses - save_sum))
+            'money_fund': self._div(i1, (self.avg_expenses - cut_sum)),
+            'money_fund_pension': self._div(i2, (self.avg_expenses - cut_sum))
         }]
-
-    @property
-    def save_sum(self) -> float:
-        _ret = None
-
-        if self.not_use and self.avg_expenses:
-            _ret = sum(self.avg_type_expenses.get(name, 0.0) for name in self.not_use)
-
-        if _ret == 0:
-            _ret = None
-
-        return _ret
 
     def _div(self, incomes: float, expenses: float) -> float:
         _val = 0.0
