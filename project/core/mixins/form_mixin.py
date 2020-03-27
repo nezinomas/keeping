c1bf14addfc5ca425d4f4a49f13f6f0e9a248aed
Audrius <nezinomas@gmail.com>
2020-03-27 09:05:41 +0200

    nsvrb

--- project/core/mixins/form_mixin.py
+++ project/core/mixins/form_mixin.py
@@ -5,6 +5,8 @@ class FormMixin():
     def save(self, commit=True):
         instance = super().save(commit=False)
         instance.user = utils.get_user()
+
         if commit:
             instance.save()
+
         return instance
