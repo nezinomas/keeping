2742a5c5d348f57335c88dc6e86ee995f4e6d561
Audrius <nezinomas@gmail.com>
2020-06-24 20:55:27 +0300

    nsvrb

--- project/bookkeeping/views.py
+++ project/bookkeeping/views.py
@@ -171,7 +171,7 @@ class Summary(LoginRequiredMixin, TemplateView):
 
         context['drinks_categories'] = drink_years
         context['drinks_data_ml'] = [x['per_day'] for x in qs]
-        context['drinks_data_alcohol'] = [x['qty']*0.025 for x in qs]
+        context['drinks_data_alcohol'] = [x['qty'] * 0.025 for x in qs]
         context['drinks_cnt'] = len(drink_years) - offset
 
         # data for balance summary
