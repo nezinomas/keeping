bc9893329a3d3750a1c870765009d195753c5500
Audrius <nezinomas@gmail.com>
2020-06-24 17:01:34 +0300

    more tests for summary view

--- project/bookkeeping/tests/test_views.py
+++ project/bookkeeping/tests/test_views.py
@@ -581,6 +581,28 @@ def test_view_summary_salary_avg(client_logged):
 
 
 @freeze_time('1999-01-01')
+def test_view_summary_salary_years(client_logged):
+    IncomeFactory(date=date(1998, 1, 1), price=12.0, income_type=IncomeTypeFactory(title='Atlyginimas'))
+    IncomeFactory(date=date(1999, 1, 1), price=10.0, income_type=IncomeTypeFactory(title='Atlyginimas'))
+
+    url = reverse('bookkeeping:summary')
+    response = client_logged.get(url)
+
+    assert response.context['salary_categories'] == [1998, 1999]
+
+
+@freeze_time('1999-01-01')
+def test_view_summary_balance_years(client_logged):
+    ExpenseFactory(date=date(1998, 1, 1))
+    ExpenseFactory(date=date(1999, 1, 1))
+
+    url = reverse('bookkeeping:summary')
+    response = client_logged.get(url)
+
+    assert response.context['balance_categories'] == [1998, 1999]
+
+
+@freeze_time('1999-01-01')
 def test_view_summary_incomes_avg(client_logged):
     IncomeFactory(
         date=date(1998, 1, 1),
