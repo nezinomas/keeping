db8799c7dc4a513e542f7fc9a45640d88bc73fd4
Audrius <nezinomas@gmail.com>
2020-07-03 09:14:52 +0300

    nsvrb

--- project/drinks/tests/test_views.py
+++ project/drinks/tests/test_views.py
@@ -253,7 +253,7 @@ def test_historical_data_ajax(client_logged):
 @pytest.fixture()
 def compare_form_data():
     return ([
-        {"name":"csrfmiddlewaretoken","value":"RIFWoIjFMOnqjK9mbzZdjeJYucGzet4hcimTmCRnsIw0MTV7eyjvdxFK6FriXrDy"},
+        {"name":"csrfmiddlewaretoken", "value":"RIFWoIjFMOnqjK9mbzZdjeJYucGzet4hcimTmCRnsIw0MTV7eyjvdxFK6FriXrDy"},
         {"name":"year1", "value":"1999"},
         {"name":"year2", "value":"2020"}
     ])
