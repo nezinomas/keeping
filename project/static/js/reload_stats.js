340b65bc4d953cc9e56dc10806b0a98c724b1934
Audrius <nezinomas@gmail.com>
2020-07-30 13:34:32 +0300

    if empty html block do nothing

--- project/static/js/reload_stats.js
+++ project/static/js/reload_stats.js
@@ -3,6 +3,8 @@ function reload_stats(data, blocks) {
     for(i = 0; i < blocks.length; i++) {
         var name = `#${blocks[i]}`;
         var block = $(data).filter(name).html();
-        $(name).html(block);
+        if(block) {
+            $(name).html(block);
+        }
     }
 }
