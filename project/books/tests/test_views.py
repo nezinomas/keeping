09787d6a449ef99fe46fd40084251310d1ee87f4
Audrius <nezinomas@gmail.com>
2020-08-08 16:11:59 +0300

    books stats

--- project/books/tests/test_views.py
+++ project/books/tests/test_views.py
@@ -1,21 +1,109 @@
 import json
+import re
+from datetime import date
 
 import pytest
 from django.urls import resolve, reverse
 from freezegun import freeze_time
 
 from ..factories import BookFactory
-from ..views import Lists, New, Update
+from ..views import Index, Lists, New, Update
 
 X_Req = {'HTTP_X_REQUESTED_WITH': 'XMLHttpRequest'}
 pytestmark = pytest.mark.django_db
 
-def test_view_lists_func():
+
+# ----------------------------------------------------------------------------
+#                                                             Books Index View
+# ----------------------------------------------------------------------------
+def test_view_index_func():
     view = resolve('/books/')
+
+    assert Index == view.func.view_class
+
+
+def test_books_index_200(client_logged):
+    url = reverse('books:books_index')
+    response = client_logged.get(url)
+
+    assert response.status_code == 200
+
+
+def test_books_index_context(client_logged):
+    url = reverse('books:books_index')
+    response = client_logged.get(url)
+
+    assert 'book_list' in response.context
+    assert 'chart_readed_books' in response.context
+
+
+def test_books_index_chart_year(client_logged):
+    url = reverse('books:books_index')
+    response = client_logged.get(url)
+
+    content = response.content.decode("utf-8")
+
+    assert 'id="chart_readed_books"><div id="chart_readed_books_container"></div>' in content
+
+
+@freeze_time('1999-07-18')
+def test_books_index_info_row(client_logged):
+    BookFactory()
+    BookFactory()
+    BookFactory(ended=date(1999, 2, 1))
+
+    url = reverse('books:books_index')
+    response = client_logged.get(url)
+
+    content = response.content.decode("utf-8")
+
+    readed = re.compile(r'Perskaitytos:.*?(\d+)<\/h6>')
+    assert re.findall(readed, content) == ['1']
+
+    reading = re.compile(r'Skaitomos:.*?(\d+)<\/h6>')
+    assert re.findall(reading, content) == ['2']
+
+
+@freeze_time('1999-07-18')
+def test_books_index_info_row_no_data(client_logged):
+    url = reverse('books:books_index')
+    response = client_logged.get(url)
+
+    content = response.content.decode("utf-8")
+
+    readed = re.compile(r'Perskaitytos:.*?(\d+)<\/h6>')
+    assert re.findall(readed, content) == ['0']
+
+    reading = re.compile(r'Skaitomos:.*?(\d+)<\/h6>')
+    assert re.findall(reading, content) == ['0']
+
+
+def test_books_index_add_button(client_logged):
+    url = reverse('books:books_index')
+    response = client_logged.get(url)
+
+    content = response.content.decode()
+
+    pattern = re.compile(r'<button type="button".+data-url="(.*?)".+ (\w+)<\/button>')
+    res = re.findall(pattern, content)
+
+    assert len(res[0]) == 2
+    assert res[0][0] == reverse('books:books_new')
+    assert res[0][1] == 'Knygą'
+
+
+# ----------------------------------------------------------------------------
+#                                                             Books Lists View
+# ----------------------------------------------------------------------------
+def test_view_lists_func():
+    view = resolve('/books/lists/')
 
     assert Lists == view.func.view_class
 
 
+# ----------------------------------------------------------------------------
+#                                                        Books New/Update View
+# ----------------------------------------------------------------------------
 def test_view_new_func():
     view = resolve('/books/new/')
 
--- project/books/views.py
+++ project/books/views.py
@@ -1,10 +1,22 @@
-from ..core.mixins.views import CreateAjaxMixin, ListMixin, UpdateAjaxMixin
-from . import models, forms
+from django.template.loader import render_to_string
+
+from ..core.mixins.views import (CreateAjaxMixin, IndexMixin, ListMixin,
+                                 UpdateAjaxMixin)
+from . import forms, models
+
+
+class Index(IndexMixin):
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        year = self.request.user.year
+
+        context['book_list'] = Lists.as_view()(self.request, as_string=True)
+
+        return context_to_reload(self.request, year, context)
 
 
 class Lists(ListMixin):
     model = models.Book
-    template_name = 'books/index.html'
 
 
 class New(CreateAjaxMixin):
@@ -15,3 +27,28 @@ class New(CreateAjaxMixin):
 class Update(UpdateAjaxMixin):
     model = models.Book
     form_class = forms.BookForm
+
+
+def context_to_reload(request, year, context=None):
+    context = context if context else {}
+
+    readed = models.Book.objects.readed()
+    reading = models.Book.objects.reading(year=year)
+
+    context['chart_readed_books'] = render_to_string(
+        'books/includes/chart_readed_books.html',
+        {
+            'categories': [x['year'] for x in readed],
+            'data': [x['cnt'] for x in readed],
+            'chart': 'chart_readed_books',
+            'chart_title': 'Perskaitytos knygos',
+            'chart_column_color': '70, 171, 157',
+        },
+        request
+    )
+    _readed = [x.get('cnt') for x in readed if x.get('year') == year]
+
+    context['readed'] = _readed[0] if _readed else 0
+    context['reading'] = reading[0]['cnt'] if reading else 0
+
+    return context
