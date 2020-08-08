2edc0493e612743f4a71bb2d6835cfbb88245ce4
Audrius <nezinomas@gmail.com>
2020-08-08 16:10:39 +0300

    new Index view

--- project/books/urls.py
+++ project/books/urls.py
@@ -7,6 +7,11 @@ app_name = 'books'
 urlpatterns = [
     path(
         'books/',
+        views.Index.as_view(),
+        name='books_index'
+    ),
+    path(
+        'books/lists/',
         views.Lists.as_view(),
         name='books_list'
     ),
