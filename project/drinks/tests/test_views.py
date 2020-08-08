f4ab8ab5fa88bc579bd7aeb64caef1d2262a9b2d
Audrius <nezinomas@gmail.com>
2020-08-08 23:07:15 +0300

    drinks views tests

--- project/drinks/tests/test_views.py
+++ project/drinks/tests/test_views.py
@@ -1,4 +1,5 @@
 import json
+import re
 from datetime import date
 
 import pytest
@@ -21,7 +22,7 @@ pytestmark = pytest.mark.django_db
 # ----------------------------------------------------------------------------
 #
 @freeze_time('2000-01-01')
-def test_view_drinks(client_logged):
+def test_new_200(client_logged):
     url = reverse('drinks:drinks_new')
 
     response = client_logged.get(url, {}, **X_Req)
@@ -33,7 +34,7 @@ def test_view_drinks(client_logged):
     assert '<input type="text" name="date" value="1999-01-01"' in actual['html_form']
 
 
-def test_view_drinks_new(client_logged):
+def test_new(client_logged):
     data = {'date': '1999-01-01', 'quantity': 999}
 
     url = reverse('drinks:drinks_new')
@@ -47,7 +48,7 @@ def test_view_drinks_new(client_logged):
     assert '999' in actual['html_list']
 
 
-def test_view_drinks_new_invalid_data(client_logged):
+def test_new_invalid_data(client_logged):
     data = {'date': -2, 'quantity': 'x'}
 
     url = reverse('drinks:drinks_new')
@@ -61,7 +62,7 @@ def test_view_drinks_new_invalid_data(client_logged):
 
 
 @patch('project.drinks.models.DrinkQuerySet.App_name', 'Counter Type')
-def test_view_drinks_update(client_logged):
+def test_update(client_logged):
     p = DrinkFactory()
 
     data = {'date': '1999-01-01', 'quantity': 999}
@@ -83,7 +84,7 @@ def test_view_drinks_update(client_logged):
 #                                                    DrinkTarget create/update
 # ----------------------------------------------------------------------------
 #
-def test_view_drinks_target(client_logged):
+def test_target(client_logged):
     url = reverse('drinks:drinks_target_new')
 
     response = client_logged.get(url, {}, **X_Req)
@@ -95,7 +96,7 @@ def test_view_drinks_target(client_logged):
     assert '<input type="text" name="year" value="1999"' in actual['html_form']
 
 
-def test_view_drinks_target_new(client_logged):
+def test_target_new(client_logged):
     data = {'year': 1999, 'quantity': 999}
 
     url = reverse('drinks:drinks_target_new')
@@ -109,7 +110,7 @@ def test_view_drinks_target_new(client_logged):
     assert '999' in actual['html_list']
 
 
-def test_view_drinks_target_new_invalid_data(client_logged):
+def test_target_new_invalid_data(client_logged):
     data = {'year': -2, 'quantity': 'x'}
 
     url = reverse('drinks:drinks_target_new')
@@ -122,7 +123,7 @@ def test_view_drinks_target_new_invalid_data(client_logged):
     assert not actual['form_is_valid']
 
 
-def test_view_drinks_target_update(get_user, client_logged):
+def test_target_update(get_user, client_logged):
     p = DrinkTargetFactory()
 
     data = {'year': 1999, 'quantity': 999}
@@ -139,73 +140,10 @@ def test_view_drinks_target_update(get_user, client_logged):
     assert '999' in actual['html_list']
 
 
-def test_view_index_func():
-    view = resolve('/drinks/')
-
-    assert views.Index == view.func.view_class
-
-
-def test_view_index_200(client_logged):
-    response = client_logged.get('/drinks/')
-
-    assert response.status_code == 200
-
-    assert 'drinks_list' in response.context
-    assert 'target_list' in response.context
-    assert 'chart_quantity' in response.context
-    assert 'chart_consumsion' in response.context
-    assert 'tbl_consumsion' in response.context
-    assert 'tbl_last_day' in response.context
-    assert 'tbl_alcohol' in response.context
-    assert 'tbl_std_av' in response.context
-
-
-@patch('project.drinks.models.DrinkQuerySet.App_name', 'Counter Type')
-def test_view_index_drinked_date(client_logged):
-    DrinkFactory(date=date(1999, 1, 2))
-    DrinkFactory(date=date(1998, 1, 2))
-
-    change_profile_year(client_logged, 1998)
-
-    response = client_logged.get('/drinks/')
-
-    assert '1998-01-02' in response.context["tbl_last_day"]
-
-
-def test_view_index_drinked_date_empty_db(client_logged):
-    response = client_logged.get('/drinks/')
-
-    assert 'Nėra duomenų' in response.context["tbl_last_day"]
-
-
-def test_view_index_target_empty_db(client_logged):
+def test_target_empty_db(client_logged):
     response = client_logged.get('/drinks/')
 
     assert 'Neįvestas tikslas' in response.context["target_list"]
-
-
-def test_view_index_drinks_list_empty_current_year(client_logged):
-    DrinkFactory(date=date(2020, 1, 2))
-
-    response = client_logged.get('/drinks/')
-
-    assert '<b>1999</b> metais įrašų nėra.' in response.context["drinks_list"]
-
-
-def test_view_index_tbl_consumsion_empty_current_year(client_logged):
-    DrinkFactory(date=date(2020, 1, 2))
-
-    response = client_logged.get('/drinks/')
-
-    assert 'Nėra duomenų' in response.context["tbl_consumsion"]
-
-
-def test_view_index_tbl_std_av_empty_current_year(client_logged):
-    DrinkFactory(date=date(2020, 1, 2))
-
-    response = client_logged.get('/drinks/')
-
-    assert 'Nėra duomenų' in response.context["tbl_std_av"]
 
 
 # ---------------------------------------------------------------------------------------
@@ -262,47 +200,47 @@ def _compare_form_data():
     ])
 
 
-def test_view_compare_func():
+def test_compare_func():
     view = resolve('/drinks/compare/')
 
     assert views.compare is view.func
 
 
-def test_view_compare_200(client_logged, _compare_form_data):
+def test_compare_200(client_logged, _compare_form_data):
     form_data = json.dumps(_compare_form_data)
     response = client_logged.post('/drinks/compare/', {'form_data': form_data})
 
     assert response.status_code == 200
 
 
-def test_view_compare_404(client_logged):
+def test_compare_404(client_logged):
     response = client_logged.post('/drinks/compare/')
 
     assert response.status_code == 404
 
 
-def test_view_compare_500(client_logged):
+def test_compare_500(client_logged):
     form_data = json.dumps([{'x': 'y'}])
     response = client_logged.post('/drinks/compare/', {'form_data': form_data})
 
     assert response.status_code == 500
 
 
-def test_view_compare_bad_json_data(client_logged):
+def test_compare_bad_json_data(client_logged):
     form_data = "{'x': 'y'}"
     response = client_logged.post('/drinks/compare/', {'form_data': form_data})
 
     assert response.status_code == 500
 
 
-def test_view_compare_302(client):
+def test_compare_302(client):
     url = reverse('drinks:compare')
     response = client.post(url)
 
     assert response.status_code == 302
 
 
-def test_view_compare_form_is_not_valid(client_logged, _compare_form_data):
+def test_compare_form_is_not_valid(client_logged, _compare_form_data):
     _compare_form_data[1]['value'] = None # year1 = None
     form_data = json.dumps(_compare_form_data)
 
@@ -314,7 +252,7 @@ def test_view_compare_form_is_not_valid(client_logged, _compare_form_data):
     assert not actual['form_is_valid']
 
 
-def test_view_compare_form_is_valid(client_logged, _compare_form_data):
+def test_compare_form_is_valid(client_logged, _compare_form_data):
     form_data = json.dumps(_compare_form_data)
 
     url = reverse('drinks:compare')
@@ -325,7 +263,7 @@ def test_view_compare_form_is_valid(client_logged, _compare_form_data):
     assert actual['form_is_valid']
 
 
-def test_view_compare_no_records_for_year(client_logged, _compare_form_data):
+def test_compare_no_records_for_year(client_logged, _compare_form_data):
     form_data = json.dumps(_compare_form_data)
 
     url = reverse('drinks:compare')
@@ -337,7 +275,7 @@ def test_view_compare_no_records_for_year(client_logged, _compare_form_data):
 
 
 @patch('project.drinks.models.DrinkQuerySet.App_name', 'Counter Type')
-def test_view_compare_chart_data(client_logged, _compare_form_data):
+def test_compare_chart_data(client_logged, _compare_form_data):
     DrinkFactory()
     DrinkFactory(date=date(2020, 1, 1), quantity=10)
 
@@ -360,13 +298,13 @@ def test_view_compare_chart_data(client_logged, _compare_form_data):
 # ---------------------------------------------------------------------------------------
 #                                                                            Realod Stats
 # ---------------------------------------------------------------------------------------
-def test_view_reload_stats_func():
+def test_reload_stats_func():
     view = resolve('/drinks/reload_stats/')
 
     assert views.reload_stats is view.func
 
 
-def test_view_reload_stats_render(get_user, rf):
+def test_reload_stats_render(get_user, rf):
     request = rf.get('/drinks/reload_stats/?ajax_trigger=1')
     request.user = UserFactory.build()
 
@@ -375,16 +313,259 @@ def test_view_reload_stats_render(get_user, rf):
     assert response.status_code == 200
 
 
-def test_view_reload_stats_render_ajax_trigger(client_logged):
+def test_reload_stats_render_ajax_trigger(client_logged):
     url = reverse('drinks:reload_stats')
     response = client_logged.get(url, {'ajax_trigger': 1})
 
     assert response.status_code == 200
 
 
-def test_view_reload_stats_render_ajax_trigger_not_set(client_logged):
+def test_reload_stats_render_ajax_trigger_not_set(client_logged):
     url = reverse('drinks:reload_stats')
     response = client_logged.get(url, follow=True)
 
     assert response.status_code == 200
     assert views.Index == response.resolver_match.func.view_class
+
+
+# ----------------------------------------------------------------------------
+#                                                            Drinks IndexView
+# ----------------------------------------------------------------------------
+def test_index_func():
+    view = resolve('/drinks/')
+
+    assert views.Index == view.func.view_class
+
+
+def test_index_200(client_logged):
+    url = reverse('drinks:drinks_index')
+    response = client_logged.get(url)
+
+    assert response.status_code == 200
+
+
+def test_index_add_button(client_logged):
+    url = reverse('drinks:drinks_index')
+    response = client_logged.get(url)
+
+    content = response.content.decode()
+
+    pattern = re.compile(r'<button type="button".+data-url="(.*?)".+ (\w+)<\/button>')
+    res = re.findall(pattern, content)
+
+    assert len(res[0]) == 2
+    assert res[0][0] == reverse('drinks:drinks_new')
+    assert res[0][1] == 'Gertynės'
+
+
+def test_index_links(client_logged):
+    url = reverse('drinks:drinks_index')
+    response = client_logged.get(url)
+
+    content = response.content.decode()
+
+    pattern = re.compile(r'<a href="(.*?)" class="btn btn-sm.+>(\w+)<\/a>')
+    res = re.findall(pattern, content)
+
+    assert len(res) == 3
+    assert res[0][0] == reverse('drinks:drinks_index')
+    assert res[0][1] == 'Grafikai'
+
+    assert res[1][0] == reverse('drinks:drinks_list')
+    assert res[1][1] == 'Duomenys'
+
+    assert res[2][0] == reverse('drinks:drinks_history')
+    assert res[2][1] == 'Istorija'
+
+
+def test_index_context(client_logged):
+    url = reverse('drinks:drinks_index')
+    response = client_logged.get(url)
+
+    assert 'tab' in response.context
+    assert 'chart_quantity' in response.context
+    assert 'chart_consumsion' in response.context
+    assert 'tbl_consumsion' in response.context
+    assert 'tbl_last_day' in response.context
+    assert 'tbl_alcohol' in response.context
+    assert 'tbl_std_av' in response.context
+    assert 'target_list' in response.context
+    assert 'all_years' in response.context
+    assert 'compare_form' in response.context
+
+
+def test_index_context_tab_value(client_logged):
+    url = reverse('drinks:drinks_index')
+    response = client_logged.get(url)
+
+    assert response.context['tab'] == 'index'
+
+
+def test_index_chart_consumsion(client_logged):
+    url = reverse('drinks:drinks_index')
+    response = client_logged.get(url)
+
+    content = response.content.decode("utf-8")
+
+    assert 'id="chart_consumsion"><div id="chart_consumsion_container"></div>' in content
+
+
+def test_index_chart_quantity(client_logged):
+    url = reverse('drinks:drinks_index')
+    response = client_logged.get(url)
+
+    content = response.content.decode("utf-8")
+
+    assert 'id="chart_quantity"><div id="chart_quantity_container"></div>' in content
+
+
+@patch('project.drinks.models.DrinkQuerySet.App_name', 'Counter Type')
+def test_index_drinked_date(client_logged):
+    DrinkFactory(date=date(1999, 1, 2))
+    DrinkFactory(date=date(1998, 1, 2))
+
+    change_profile_year(client_logged, 1998)
+
+    response = client_logged.get('/drinks/')
+
+    assert '1998-01-02' in response.context["tbl_last_day"]
+
+
+def test_index_drinked_date_empty_db(client_logged):
+    response = client_logged.get('/drinks/')
+
+    assert 'Nėra duomenų' in response.context["tbl_last_day"]
+
+
+def test_index_tbl_consumsion_empty_current_year(client_logged):
+    DrinkFactory(date=date(2020, 1, 2))
+
+    response = client_logged.get('/drinks/')
+
+    assert 'Nėra duomenų' in response.context["tbl_consumsion"]
+
+
+def test_index_tbl_std_av_empty_current_year(client_logged):
+    DrinkFactory(date=date(2020, 1, 2))
+
+    response = client_logged.get('/drinks/')
+
+    assert 'Nėra duomenų' in response.context["tbl_std_av"]
+
+
+# ----------------------------------------------------------------------------
+#                                                                 Drinks Lists
+# ----------------------------------------------------------------------------
+def test_list_func():
+    view = resolve('/drinks/lists/')
+
+    assert views.Lists == view.func.view_class
+
+
+def test_list_200(client_logged):
+    url = reverse('drinks:drinks_list')
+    response = client_logged.get(url)
+
+    assert response.status_code == 200
+
+
+def test_list_context(client_logged):
+    url = reverse('drinks:drinks_list')
+    response = client_logged.get(url)
+
+    assert 'data' in response.context
+    assert 'tab' in response.context
+
+
+def test_list_context_tab_value(client_logged):
+    url = reverse('drinks:drinks_list')
+    response = client_logged.get(url)
+
+    assert response.context['tab'] == 'data'
+
+
+def test_list_empty_current_year(client_logged):
+    DrinkFactory(date=date(2020, 1, 2))
+
+    url = reverse('drinks:drinks_list')
+    response = client_logged.get(url)
+
+    assert '<b>1999</b> metais įrašų nėra.' in response.context["data"]
+
+
+# ---------------------------------------------------------------------------------------
+#                                                                           Drinks Summary
+# ---------------------------------------------------------------------------------------
+def test_history_func():
+    view = resolve('/drinks/history/')
+
+    assert views.Summary == view.func.view_class
+
+
+def test_history_200(client_logged):
+    url = reverse('drinks:drinks_history')
+    response = client_logged.get(url)
+
+    assert response.status_code == 200
+
+
+def test_history_context_tab_value(client_logged):
+    url = reverse('drinks:drinks_history')
+    response = client_logged.get(url)
+
+    assert response.context['tab'] == 'history'
+
+
+def test_history_context(client_logged):
+    url = reverse('drinks:drinks_history')
+    response = client_logged.get(url)
+
+    assert 'drinks_categories' in response.context
+    assert 'drinks_data_ml' in response.context
+    assert 'drinks_data_alcohol' in response.context
+    assert 'drinks_cnt' in response.context
+
+
+def test_history_chart_consumsion(client_logged):
+    url = reverse('drinks:drinks_history')
+    response = client_logged.get(url)
+
+    content = response.content.decode("utf-8")
+
+    assert '<div id="chart_history_container"></div>' in content
+
+
+@freeze_time('1999-01-01')
+@patch('project.drinks.models.DrinkQuerySet.App_name', 'Counter Type')
+def test_history_drinks_years(client_logged):
+    DrinkFactory()
+    DrinkFactory(date=date(1998, 1, 1))
+
+    url = reverse('drinks:drinks_history')
+    response = client_logged.get(url)
+
+    assert response.context['drinks_categories'] == [1998, 1999]
+
+
+@freeze_time('1999-01-01')
+@patch('project.drinks.models.DrinkQuerySet.App_name', 'Counter Type')
+def test_history_drinks_data_ml(client_logged):
+    DrinkFactory(quantity=1)
+    DrinkFactory(date=date(1998, 1, 1), quantity=2)
+
+    url = reverse('drinks:drinks_history')
+    response = client_logged.get(url)
+
+    assert response.context['drinks_data_ml'] == pytest.approx([2.74, 1.37], rel=1e-2)
+
+
+@freeze_time('1999-01-01')
+@patch('project.drinks.models.DrinkQuerySet.App_name', 'Counter Type')
+def test_history_drinks_data_alcohol(client_logged):
+    DrinkFactory(quantity=1)
+    DrinkFactory(date=date(1998, 1, 1), quantity=2)
+
+    url = reverse('drinks:drinks_history')
+    response = client_logged.get(url)
+
+    assert response.context['drinks_data_alcohol'] == [0.05, 0.025]
