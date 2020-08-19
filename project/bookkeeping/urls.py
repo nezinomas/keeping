2135624c10478d0bf6381ac8cdaa381e9f64117a
Audrius <nezinomas@gmail.com>
2020-08-19 13:51:14 +0300

    App_name

--- project/bookkeeping/urls.py
+++ project/bookkeeping/urls.py
@@ -2,8 +2,10 @@ from django.urls import path, register_converter
 
 from ..core import converters
 from . import views
+from .apps import App_name
 
-app_name = 'bookkeeping'
+
+app_name = App_name
 
 register_converter(converters.DateConverter, 'date')
 
