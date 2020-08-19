5d8414ef3cffd7c1b0aeb4776e213f4f6ec8ad9a
Audrius <nezinomas@gmail.com>
2020-08-19 14:40:33 +0300

    renamed variables

--- project/books/lib/views_helper.py
+++ project/books/lib/views_helper.py
@@ -7,14 +7,14 @@ def context_to_reload(request, context=None):
     context = context if context else {}
     year = request.user.year
 
-    readed = models.Book.objects.readed()
-    reading = models.Book.objects.reading(year=year)
+    qs_readed = models.Book.objects.readed()
+    qs_reading = models.Book.objects.reading(year=year)
 
     context['chart_readed_books'] = render_to_string(
         'books/includes/chart_readed_books.html',
         {
-            'categories': [x['year'] for x in readed],
-            'data': [x['cnt'] for x in readed],
+            'categories': [x['year'] for x in qs_readed],
+            'data': [x['cnt'] for x in qs_readed],
             'chart': 'chart_readed_books',
             'chart_title': 'Perskaitytos knygos',
             'chart_column_color': '70, 171, 157',
@@ -22,12 +22,12 @@ def context_to_reload(request, context=None):
         request
     )
 
-    _readed = [x.get('cnt') for x in readed if x.get('year') == year]
+    readed = [x.get('cnt') for x in qs_readed if x.get('year') == year]
     context['info_row'] = render_to_string(
         'books/includes/info_row.html',
         {
-            'readed': _readed[0] if _readed else 0,
-            'reading': reading[0]['cnt'] if reading else 0,
+            'readed': readed[0] if readed else 0,
+            'reading': qs_reading[0]['cnt'] if qs_reading else 0,
         },
         request
     )
