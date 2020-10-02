f05f4dfffed297d44ddda2368ba7e09e375818f0
Audrius <nezinomas@gmail.com>
2020-10-02 14:14:05 +0300

    books search view

--- project/books/views.py
+++ project/books/views.py
@@ -1,3 +1,10 @@
+from django.http import JsonResponse
+from django.shortcuts import reverse
+from django.template.loader import render_to_string
+
+from ..core.forms import SearchForm
+from ..core.lib import search
+from ..core.mixins.ajax import AjaxCustomFormMixin
 from ..core.mixins.views import (CreateAjaxMixin, DispatchAjaxMixin,
                                  IndexMixin, ListMixin, UpdateAjaxMixin)
 from . import forms, models
@@ -9,7 +16,13 @@ class Index(IndexMixin):
         context = super().get_context_data(**kwargs)
         context.update({
             'book_list': Lists.as_view()(self.request, as_string=True),
-            **H.context_to_reload(self.request, context)
+            'search': render_to_string(
+                template_name='core/includes/search_form.html',
+                context={
+                    'form': SearchForm(),
+                    'url': reverse('books:books_search')},
+                request=self.request),
+            **H.context_to_reload(self.request, context),
         })
         return context
 
@@ -34,3 +47,26 @@ class ReloadStats(DispatchAjaxMixin, IndexMixin):
 
     def get(self, request, *args, **kwargs):
         return self.render_to_response(H.context_to_reload(request))
+
+
+class Search(AjaxCustomFormMixin):
+    template_name = 'core/includes/search_form.html'
+    form_class = SearchForm
+    form_data_dict = {}
+
+    def form_valid(self, form):
+        html = 'Nieko neradau'
+        _search = self.form_data_dict['search']
+
+        sql = search.search_books(_search)
+        if sql:
+            template = 'books/includes/books_list.html'
+            context = {'items': sql}
+            html = render_to_string(template, context, self.request)
+
+        data = {
+            'form_is_valid': True,
+            'html_form': self._render_form({'form': form, 'url': reverse('books:books_search')}),
+            'html': html,
+        }
+        return JsonResponse(data)
