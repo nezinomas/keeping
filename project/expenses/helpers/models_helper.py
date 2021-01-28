a0607d371de45a490e3b97ba787007b03689b991
Audrius <nezinomas@gmail.com>
2021-01-28 17:29:11 +0200

    add year and month to expenses file

--- project/expenses/helpers/models_helper.py
+++ project/expenses/helpers/models_helper.py
@@ -1,4 +1,9 @@
 import os
+from datetime import datetime
+
 
 def upload_attachment(instance, filename):
-    return os.path.join(instance.expense_type.slug, filename)
+    now = datetime.now()
+    f = f'{now.year}.{now.strftime("%m")}_{filename}'
+
+    return os.path.join(instance.expense_type.slug, f)
--- project/expenses/tests/test_models_helper.py
+++ project/expenses/tests/test_models_helper.py
@@ -1,13 +1,15 @@
 import os
 
+from freezegun import freeze_time
+
 from ..factories import ExpenseFactory
 from ..helpers import models_helper as T
 
-
+@freeze_time('1000-1-1')
 def test_upload_attachment():
     e = ExpenseFactory.build()
     f = 'x.jpg'
 
     actual = T.upload_attachment(e, f)
 
-    assert actual == os.path.join(e.expense_type.slug, f)
+    assert actual == os.path.join(e.expense_type.slug, '1000.01_x.jpg')
