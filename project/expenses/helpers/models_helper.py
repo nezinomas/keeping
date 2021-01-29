f64230e20e6c2d6e2cf2a6eae42e139161b20e00
Audrius <nezinomas@gmail.com>
2021-01-29 18:31:25 +0200

    if updating past expense get that past date for attachment file

--- project/expenses/helpers/models_helper.py
+++ project/expenses/helpers/models_helper.py
@@ -4,6 +4,10 @@ from datetime import datetime
 
 def upload_attachment(instance, filename):
     now = datetime.now()
+
+    if instance.pk:
+        now = instance.date
+
     f = f'{now.year}.{now.strftime("%m")}_{filename}'
 
     return os.path.join(instance.expense_type.slug, f)
