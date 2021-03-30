c0b5e7fb20dfe91ebf7dea1959c9c11c95ffbb5f
Audrius <nezinomas@gmail.com>
2021-03-30 12:39:25 +0300

    Incomes Delete

--- project/incomes/views.py
+++ project/incomes/views.py
@@ -5,8 +5,8 @@ from django.urls import reverse, reverse_lazy
 from ..core.forms import SearchForm
 from ..core.lib import search
 from ..core.mixins.ajax import AjaxCustomFormMixin
-from ..core.mixins.views import (CreateAjaxMixin, IndexMixin, ListMixin,
-                                 UpdateAjaxMixin)
+from ..core.mixins.views import (CreateAjaxMixin, DeleteAjaxMixin, IndexMixin,
+                                 ListMixin, UpdateAjaxMixin)
 from . import forms, models
 
 
@@ -51,6 +51,10 @@ class New(GetQuerySetMixin, CreateAjaxMixin):
 class Update(GetQuerySetMixin, UpdateAjaxMixin):
     model = models.Income
     form_class = forms.IncomeForm
+
+
+class Delete(DeleteAjaxMixin):
+    model = models.Income
 
 
 #----------------------------------------------------------------------------------------
