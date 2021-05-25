34d33d9cd16aec09115c7486dc16426614b5b7b7
Audrius <nezinomas@gmail.com>
2021-05-25 14:49:57 +0300

    test for average method

--- project/bookkeeping/tests/test_views_helpers.py
+++ project/bookkeeping/tests/test_views_helpers.py
@@ -157,3 +157,33 @@ def test_add_latest_check_key_date_not_found():
     T.add_latest_check_key(model, actual)
 
     assert actual == [{'title': 'x', 'latest_check': None}]
+
+
+@freeze_time('2000-2-5')
+def test_average_current_year():
+    qs = [{'year': 2000, 'sum': Decimal('12')}]
+
+    actual = T.average(qs)
+
+    assert actual == [6.0]
+
+
+@freeze_time('2001-2-5')
+def test_average_past_year():
+    qs = [{'year': 2000, 'sum': Decimal('12')}]
+
+    actual = T.average(qs)
+
+    assert actual == [1.0]
+
+
+@freeze_time('2000-2-5')
+def test_average():
+    qs = [
+        {'year': 1999, 'sum': Decimal('12')},
+        {'year': 2000, 'sum': Decimal('12')}
+    ]
+
+    actual = T.average(qs)
+
+    assert actual == [1.0, 6.0]
