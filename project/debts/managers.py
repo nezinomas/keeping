79e7896f74c6e74a3f144ca9133fb3c04bc7760e
Audrius <nezinomas@gmail.com>
2021-05-28 10:10:16 +0300

    refactoring, extracted queryset managers to seprate file

---
+++ project/debts/managers.py
@@ -0,0 +1,58 @@
+from decimal import Decimal
+from typing import Any, Dict, List
+
+from django.db import models
+from django.db.models import Case, Count, F, Sum, When
+
+from ..core.lib import utils
+
+
+class BorrowQuerySet(models.QuerySet):
+    def related(self):
+        user = utils.get_user()
+        return (
+            self
+            .select_related('user')
+            .filter(user=user)
+            .filter(closed=False)
+        )
+
+    def items(self):
+        return self.related()
+
+    def year(self, year):
+        return self.related().filter(date__year=year)
+
+    def summary(self, year: int) -> List[Dict[str, Any]]:
+        '''
+        return:
+            {
+                'id': account.id,
+                'title': account.title,
+                'borrow_past': Decimal(),
+                'borrow_now': Decimal()
+            }
+        '''
+        return (
+            self
+            .related()
+            .annotate(cnt=Count('name'))
+            .values('cnt')
+            .order_by('cnt')
+            .annotate(
+                borrow_past=Sum(
+                    Case(
+                        When(**{'date__year__lt': year}, then='price'),
+                        default=Decimal(0))),
+                borrow_now=Sum(
+                    Case(
+                        When(**{'date__year': year}, then='price'),
+                        default=Decimal(0)))
+            )
+            .values(
+                'borrow_past',
+                'borrow_now',
+                title=models.F('account__title'),
+                id=models.F('account__pk')
+            )
+        )
--- project/debts/models.py
+++ project/debts/models.py
@@ -1,65 +1,12 @@
 from decimal import Decimal
-from typing import Any, Dict, List
 
 from django.core.validators import MinValueValidator
 from django.db import models
-from django.db.models import Case, Count, F, Sum, When
 
 from ..accounts.models import Account
-from ..core.lib import utils
 from ..core.mixins.from_db import MixinFromDbAccountId
 from ..users.models import User
-
-
-class BorrowQuerySet(models.QuerySet):
-    def related(self):
-        user = utils.get_user()
-        return (
-            self
-            .select_related('user')
-            .filter(user=user)
-            .filter(closed=False)
-        )
-
-    def items(self):
-        return self.related()
-
-    def year(self, year):
-        return self.related().filter(date__year=year)
-
-    def summary(self, year: int) -> List[Dict[str, Any]]:
-        '''
-        return:
-            {
-                'id': account.id,
-                'title': account.title,
-                'borrow_past': Decimal(),
-                'borrow_now': Decimal()
-            }
-        '''
-        return (
-            self
-            .related()
-            .annotate(cnt=Count('name'))
-            .values('cnt')
-            .order_by('cnt')
-            .annotate(
-                borrow_past=Sum(
-                    Case(
-                        When(**{'date__year__lt': year}, then='price'),
-                        default=Decimal(0))),
-                borrow_now=Sum(
-                    Case(
-                        When(**{'date__year': year}, then='price'),
-                        default=Decimal(0)))
-            )
-            .values(
-                'borrow_past',
-                'borrow_now',
-                title=models.F('account__title'),
-                id=models.F('account__pk')
-            )
-        )
+from . import managers
 
 
 class Borrow(MixinFromDbAccountId):
@@ -90,7 +37,9 @@ class Borrow(MixinFromDbAccountId):
     )
 
     # objects = BookManager()
-    objects = BorrowQuerySet.as_manager()
+    objects = managers.BorrowQuerySet.as_manager()
 
     def __str__(self):
         return f'Pasiskolinta {round(self.price, 0)}'
+
+
