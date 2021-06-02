35a43922921b0c8b2411f6a264d024fcd6a8772c
Audrius <nezinomas@gmail.com>
2021-06-02 11:12:57 +0300

    imports

--- project/incomes/forms.py
+++ project/incomes/forms.py
@@ -6,7 +6,6 @@ from ..accounts.models import Account
 from ..core.helpers.helper_forms import set_field_properties
 from ..core.lib import utils
 from ..core.lib.date import set_year_for_form
-from ..core.mixins.form_mixin import FormForUserMixin
 from .models import Income, IncomeType
 
 
