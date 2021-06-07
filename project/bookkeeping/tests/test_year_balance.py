b6b3864a751a62d9d1ed8f81b3a35eca76c20b6d
Audrius <nezinomas@gmail.com>
2021-06-07 15:43:32 +0300

    rename residual -> money_flow fix errors in money_flow calculation remove Taupymas from expenses table and expenses chart

--- project/bookkeeping/tests/test_year_balance.py
+++ project/bookkeeping/tests/test_year_balance.py
@@ -1,7 +1,8 @@
 from datetime import date
 from decimal import Decimal
-from pandas import Timestamp as pdTime
+
 import pytest
+from pandas import Timestamp as pdTime
 
 from ..lib.year_balance import YearBalance
 
@@ -36,7 +37,7 @@ def _expenses():
 
 
 @pytest.fixture()
-def _residual():
+def _money_flow():
     return Decimal(1)
 
 
@@ -50,7 +51,7 @@ def _expect():
             'balance': 3.75,
             'savings': 1.0,
             'savings_close': 0.5,
-            'residual': 4.75
+            'money_flow': 4.25
         }, {
             'date': pdTime(date(1999, 2, 1)),
             'incomes': 1.25,
@@ -58,7 +59,7 @@ def _expect():
             'balance': 1.25,
             'savings': 0.0,
             'savings_close': 0.0,
-            'residual': 6.0
+            'money_flow': 5.5
         }, {
             'date': pdTime(date(1999, 3, 1)),
             'incomes': 0.0,
@@ -66,7 +67,7 @@ def _expect():
             'balance': 0.0,
             'savings': 0.0,
             'savings_close': 0.0,
-            'residual': 6.0
+            'money_flow': 5.5
         }, {
             'date': pdTime(date(1999, 4, 1)),
             'incomes': 0.0,
@@ -74,7 +75,7 @@ def _expect():
             'balance': 0.0,
             'savings': 0.0,
             'savings_close': 0.0,
-            'residual': 6.0
+            'money_flow': 5.5
         }, {
             'date': pdTime(date(1999, 5, 1)),
             'incomes': 0.0,
@@ -82,7 +83,7 @@ def _expect():
             'balance': 0.0,
             'savings': 0.0,
             'savings_close': 0.0,
-            'residual': 6.0
+            'money_flow': 5.5
         }, {
             'date': pdTime(date(1999, 6, 1)),
             'incomes': 0.0,
@@ -90,7 +91,7 @@ def _expect():
             'balance': 0.0,
             'savings': 0.0,
             'savings_close': 0.0,
-            'residual': 6.0
+            'money_flow': 5.5
         }, {
             'date': pdTime(date(1999, 7, 1)),
             'incomes': 0.0,
@@ -98,7 +99,7 @@ def _expect():
             'balance': 0.0,
             'savings': 0.0,
             'savings_close': 0.0,
-            'residual': 6.0
+            'money_flow': 5.5
         }, {
             'date': pdTime(date(1999, 8, 1)),
             'incomes': 0.0,
@@ -106,7 +107,7 @@ def _expect():
             'balance': 0.0,
             'savings': 0.0,
             'savings_close': 0.0,
-            'residual': 6.0
+            'money_flow': 5.5
         }, {
             'date': pdTime(date(1999, 9, 1)),
             'incomes': 0.0,
@@ -114,7 +115,7 @@ def _expect():
             'balance': 0.0,
             'savings': 0.0,
             'savings_close': 0.0,
-            'residual': 6.0
+            'money_flow': 5.5
         }, {
             'date': pdTime(date(1999, 10, 1)),
             'incomes': 0.0,
@@ -122,7 +123,7 @@ def _expect():
             'balance': 0.0,
             'savings': 0.0,
             'savings_close': 0.0,
-            'residual': 6.0
+            'money_flow': 5.5
         }, {
             'date': pdTime(date(1999, 11, 1)),
             'incomes': 0.0,
@@ -130,7 +131,7 @@ def _expect():
             'balance': 0.0,
             'savings': 0.0,
             'savings_close': 0.0,
-            'residual': 6.0
+            'money_flow': 5.5
         }, {
             'date': pdTime(date(1999, 12, 1)),
             'incomes': 0.0,
@@ -138,62 +139,65 @@ def _expect():
             'balance': 0.0,
             'savings': 0.0,
             'savings_close': 0.0,
-            'residual': 6.0
+            'money_flow': 5.5
         }
     ]
 
 
-def test_months_balance(_incomes, _expenses, _savings, _savings_close, _residual, _expect):
+def test_months_balance(_incomes, _expenses, _savings, _savings_close, _money_flow, _expect):
     actual = YearBalance(
         year=1999,
         incomes=_incomes,
         expenses=_expenses,
         savings=_savings,
         savings_close=_savings_close,
-        amount_start=_residual
+        amount_start=_money_flow
     ).balance
 
     assert actual == _expect
 
 
-def test_months_balance_no_savings(_incomes, _expenses, _savings_close, _residual, _expect):
+def test_months_balance_no_savings(_incomes, _expenses, _savings_close, _money_flow, _expect):
     actual = YearBalance(
         year=1999,
         incomes=_incomes,
         expenses=_expenses,
         savings_close=_savings_close,
-        amount_start=_residual
+        amount_start=_money_flow
     ).balance
 
     for x in _expect:
         x['savings'] = 0.0
+        x['money_flow'] += 1.0
 
     assert actual == _expect
 
 
-def test_months_balance_no_savings_close(_incomes, _expenses, _savings, _residual, _expect):
+def test_months_balance_no_savings_close(_incomes, _expenses, _savings, _money_flow, _expect):
     actual = YearBalance(
         year=1999,
         incomes=_incomes,
         expenses=_expenses,
         savings=_savings,
-        amount_start=_residual
+        amount_start=_money_flow
     ).balance
 
     for x in _expect:
         x['savings_close'] = 0.0
+        x['money_flow'] -= 1.0
+        x['money_flow'] += 0.5
 
     assert actual == _expect
 
 
-def test_months_balance_total_row(_incomes, _expenses, _savings, _savings_close, _residual, _expect):
+def test_months_balance_total_row(_incomes, _expenses, _savings, _savings_close, _money_flow, _expect):
     actual = YearBalance(
         year=1999,
         incomes=_incomes,
         expenses=_expenses,
         savings=_savings,
         savings_close=_savings_close,
-        amount_start=_residual
+        amount_start=_money_flow
     ).total_row
 
     expect = {
@@ -202,20 +206,20 @@ def test_months_balance_total_row(_incomes, _expenses, _savings, _savings_close,
         'balance': 5.0,
         'savings': 1.0,
         'savings_close': 0.5,
-        'residual': 70.75
+        'money_flow': 64.75
     }
 
     assert actual == expect
 
 
-def test_months_balance_average(_incomes, _expenses, _savings, _savings_close, _residual, _expect):
+def test_months_balance_average(_incomes, _expenses, _savings, _savings_close, _money_flow, _expect):
     actual = YearBalance(
         year=1999,
         incomes=_incomes,
         expenses=_expenses,
         savings=_savings,
         savings_close=_savings_close,
-        amount_start=_residual
+        amount_start=_money_flow
     ).average
 
     expect = {
@@ -224,7 +228,7 @@ def test_months_balance_average(_incomes, _expenses, _savings, _savings_close, _
         'balance': 2.5,
         'savings': 1.0,
         'savings_close': 0.5,
-        'residual': 5.90
+        'money_flow': 5.39
     }
 
     assert expect == pytest.approx(actual, rel=1e-2)
@@ -252,15 +256,17 @@ def test_amount_start_none():
     assert actual == 0.0
 
 
-def test_amount_end(_incomes, _expenses, _residual):
+def test_amount_end(_incomes, _expenses, _savings, _savings_close, _money_flow):
     actual = YearBalance(
         year=1999,
         incomes=_incomes,
         expenses=_expenses,
-        amount_start=_residual
+        savings=_savings,
+        savings_close=_savings_close,
+        amount_start=_money_flow
     ).amount_end
 
-    assert actual == 6.0
+    assert actual == 5.5
 
 
 def test_amount_end_none():
@@ -274,12 +280,12 @@ def test_amount_end_none():
     assert actual == 0.0
 
 
-def test_amount_balance(_incomes, _expenses, _residual):
+def test_amount_balance(_incomes, _expenses, _money_flow):
     actual = YearBalance(
         year=1999,
         incomes=_incomes,
         expenses=_expenses,
-        amount_start=_residual
+        amount_start=_money_flow
     ).amount_balance
 
     assert actual == 5.0
@@ -322,15 +328,17 @@ def test_balance_expense_data(_expenses, _expect):
     assert actual == expect
 
 
-def test_balance_money_flow(_incomes, _expenses, _residual, _expect):
+def test_balance_money_flow(_incomes, _expenses, _savings, _savings_close, _money_flow, _expect):
     actual = YearBalance(
         year=1999,
         incomes=_incomes,
         expenses=_expenses,
-        amount_start=_residual
+        savings=_savings,
+        savings_close=_savings_close,
+        amount_start=_money_flow
     ).money_flow
 
-    expect = [x['residual'] for x in _expect]
+    expect = [x['money_flow'] for x in _expect]
 
     assert actual == expect
 
