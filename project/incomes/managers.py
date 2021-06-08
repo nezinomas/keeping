6defa1c545434e3d26af876972ed35bf28247783
Audrius <nezinomas@gmail.com>
2021-06-08 11:02:17 +0300

    refactoring, income models QuerySet classed move to managers.py module

---
+++ project/incomes/managers.py
@@ -0,0 +1,130 @@
+from decimal import Decimal
+from typing import Any, Dict, List
+from django.db import models
+from django.db.models import Case, Count, F, Sum, When
+from django.db.models.functions import ExtractYear, TruncMonth, TruncYear
+
+from ..core.lib import utils
+from ..core.mixins.queryset_sum import SumMixin
+
+
+class IncomeTypeQuerySet(models.QuerySet):
+    def related(self):
+        user = utils.get_user()
+        return (
+            self
+            .select_related('user')
+            .filter(user=user)
+        )
+
+    def items(self):
+        return self.related()
+
+
+class IncomeQuerySet(SumMixin, models.QuerySet):
+    def related(self):
+        user = utils.get_user()
+        qs = (
+            self
+            .select_related('account', 'income_type')
+            .filter(income_type__user=user)
+        )
+        return qs
+
+    def year(self, year):
+        return self.related().filter(date__year=year)
+
+    def items(self):
+        return self.related().all()
+
+    def sum_by_month(self, year: int, month: int = None) -> List[Dict[str, Any]]:
+        '''
+        year:
+            filter data by year and return sums for every month
+        month:
+            filter data by year AND month, return sum for that month
+        return:
+            {'date': datetime.date(), 'sum': Decimal()}
+        '''
+
+        sum_annotation = 'sum'
+
+        return (
+            self
+            .related()
+            .month_sum(
+                year=year,
+                month=month,
+                sum_annotation=sum_annotation)
+            .values('date', sum_annotation)
+        )
+
+    def sum_by_year(self, income_type: List[str] = None):
+        qs = (
+            self
+            .related()
+            .annotate(c=Count('id'))
+            .values('c')
+            .annotate(date=TruncYear('date'))
+            .annotate(year=ExtractYear(F('date')))
+            .annotate(sum=Sum('price'))
+            .order_by('year')
+            .values('year', 'sum')
+        )
+
+        if income_type:
+            qs = qs.filter(income_type__title__in=income_type)
+
+        return qs
+
+    def summary(self, year: int) -> List[Dict[str, Any]]:
+        '''
+        return:
+            {
+                'id': account.id,
+                'title': account.title,
+                'i_past': Decimal(),
+                'i_now': Decimal()
+            }
+        '''
+        return (
+            self
+            .related()
+            .annotate(cnt=Count('income_type'))
+            .values('cnt')
+            .order_by('cnt')
+            .annotate(
+                i_past=Sum(
+                    Case(
+                        When(**{'date__year__lt': year}, then='price'),
+                        default=Decimal(0))),
+                i_now=Sum(
+                    Case(
+                        When(**{'date__year': year}, then='price'),
+                        default=Decimal(0)))
+            )
+            .values(
+                'i_past',
+                'i_now',
+                title=models.F('account__title'),
+                id=models.F('account__pk')
+            )
+        )
+
+    def sum_by_month_and_type(self, year):
+        return (
+            self
+            .related()
+            .filter(date__year=year)
+            .annotate(cnt=Count('income_type'))
+            .values('income_type')
+            .annotate(date=TruncMonth('date'))
+            .values('date')
+            .annotate(c=Count('id'))
+            .annotate(sum=Sum('price'))
+            .order_by('income_type__title', 'date')
+            .values(
+                'date',
+                'sum',
+                title=F('income_type__title'))
+        )
