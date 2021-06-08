99a7e0acc2c1f4cd6cbca2d0e4b891a13f48c8d5
Audrius <nezinomas@gmail.com>
2021-06-08 16:01:53 +0300

    mistype

--- project/bookkeeping/lib/year_balance.py
+++ project/bookkeeping/lib/year_balance.py
@@ -142,7 +142,7 @@ class YearBalance(BalanceBase):
             # create column and assign 0 for all cellls
             df.loc[:, name] = 0.0
             if arr:
-                # copy values from imput arrays to df
+                # copy values from input arrays to df
                 for d in arr:
                     df.at[to_datetime(d['date']), name] = float(d['sum'])
 
