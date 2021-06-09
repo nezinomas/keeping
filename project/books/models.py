99d423353c78fb31319d5be98a53ba5f76d486f9
Audrius <nezinomas@gmail.com>
2021-06-09 08:53:42 +0300

    refactoring, books QuerySet moved to managers.py module

--- project/books/models.py
+++ project/books/models.py
@@ -1,62 +1,9 @@
 from django.core.validators import (MaxValueValidator, MinLengthValidator,
                                     MinValueValidator)
 from django.db import models
-from django.db.models import Count, F, Q
-from django.db.models.functions import ExtractYear, TruncYear
 
-from ..core.lib import utils
-from ..core.mixins.queryset_sum import SumMixin
 from ..users.models import User
-
-
-class BooksQuerySet(SumMixin, models.QuerySet):
-    def related(self):
-        user = utils.get_user()
-        return (
-            self
-            .select_related('user')
-            .filter(user=user)
-        )
-
-    def year(self, year):
-        return (
-            self
-            .related()
-            .filter(Q(ended__year=year) | Q(ended__isnull=True))
-            .filter(started__year__lte=year)
-        )
-
-    def items(self):
-        return self.related()
-
-    def readed(self, year=None):
-        """
-        Returns <QuerySet [{'year': int, 'cnt': int}]>
-        """
-        return (
-            self
-            .related()
-            .exclude(ended__isnull=True)
-            .year_filter(year=year, field='ended')
-            .annotate(date=TruncYear('ended'))
-            .values('date')
-            .annotate(year=ExtractYear(F('date')))
-            .annotate(cnt=Count('id'))
-            .order_by('year')
-            .values('year', 'cnt')
-        )
-
-    def reading(self, year):
-        """
-        Returns {'reading: int}
-        """
-        return (
-            self
-            .related()
-            .filter(ended__isnull=True)
-            .filter(started__year__lte=year)
-            .aggregate(reading=Count('id'))
-        )
+from .managers import BooksQuerySet, BookTargetQuerySet
 
 
 class Book(models.Model):
@@ -91,26 +38,6 @@ class Book(models.Model):
 
     def __str__(self):
         return str(self.title)
-
-
-class BookTargetQuerySet(SumMixin, models.QuerySet):
-    def related(self):
-        user = utils.get_user()
-        return (
-            self
-            .select_related('user')
-            .filter(user=user)
-        )
-
-    def year(self, year):
-        return (
-            self
-            .related()
-            .filter(year=year)
-        )
-
-    def items(self):
-        return self.related()
 
 
 class BookTarget(models.Model):
