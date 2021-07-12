e462b0f2e1ac230e1c14f5fff52376f61596df45
Audrius <nezinomas@gmail.com>
2021-07-12 14:58:52 +0300

    NotUseForm and tests

---
+++ project/journals/forms.py
@@ -0,0 +1,44 @@
+import json
+
+from crispy_forms.helper import FormHelper
+from django import forms
+
+from ..core.helpers.helper_forms import set_field_properties
+from ..core.lib import utils
+from ..expenses.models import ExpenseType
+
+
+class NotUseForm(forms.Form):
+    choices = forms.ModelMultipleChoiceField(
+        queryset=None,
+        widget=forms.CheckboxSelectMultiple,
+        required=False
+    )
+    savings = forms.BooleanField(required=False)
+
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        self.fields['choices'].queryset = ExpenseType.objects.items()
+
+        user = utils.get_user()
+        checked_expenses = []
+        if user.journal.not_use_expenses:
+            checked_expenses = json.loads(user.journal.not_use_expenses)
+
+        self.initial['choices'] = checked_expenses
+        self.initial['savings'] = user.journal.not_use_savings
+
+        self.helper = FormHelper()
+        set_field_properties(self, self.helper)
+
+    def save(self):
+        journal = utils.get_user().journal
+
+        if self.cleaned_data["choices"]:
+            choices = list(self.cleaned_data["choices"].values_list("pk", flat=True))
+            journal.not_use_expenses = json.dumps(choices)
+
+        journal.not_use_savings = self.cleaned_data['savings']
+        journal.save()
---
+++ project/journals/tests/test_forms.py
@@ -0,0 +1,77 @@
+import json
+
+import pytest
+
+from ...expenses.factories import ExpenseTypeFactory
+from ...journals.factories import JournalFactory
+from ...journals.models import Journal
+from ..forms import NotUseForm
+
+pytestmark = pytest.mark.django_db
+
+
+def test_form_init():
+    NotUseForm()
+
+
+def test_form_fields():
+    ExpenseTypeFactory(title='X')
+    ExpenseTypeFactory(title='Y')
+    form = NotUseForm().as_p()
+
+    assert form.count('<input type="checkbox"') == 3
+
+
+def test_form_selected_expenses(get_user):
+    e1 = ExpenseTypeFactory(title='X')
+    e2 = ExpenseTypeFactory(title='Y')
+
+    get_user.journal.not_use_expenses = json.dumps([e1.pk, e2.pk])
+    get_user.journal.save()
+
+    form = NotUseForm().as_p()
+
+    assert form.count('checked') == 2
+
+
+def test_form_selected_savings(get_user):
+    get_user.journal.not_use_savings = True
+    get_user.journal.save()
+
+    form = NotUseForm().as_p()
+
+    assert form.count('checked') == 1
+
+
+def test_form_save_checked_all():
+    e1 = ExpenseTypeFactory(title='X')
+    e2 = ExpenseTypeFactory(title='Y')
+    form = NotUseForm(data={
+        'savings': True,
+        'choices': {e1.pk: True, e2: True}
+    })
+
+    assert form.is_valid()
+
+    form.save()
+
+    actual = Journal.objects.first()
+    assert actual.not_use_expenses == '[1, 2]'
+    assert actual.not_use_savings
+
+
+def test_form_save_checked_none():
+    ExpenseTypeFactory(title='X')
+    ExpenseTypeFactory(title='Y')
+    form = NotUseForm(data={
+        'savings': False,
+        'choices': {}
+    })
+
+    assert form.is_valid()
+
+    form.save()
+
+    actual = Journal.objects.first()
+    assert not actual.not_use_expenses
+    assert not actual.not_use_savings
