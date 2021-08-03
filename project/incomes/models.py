5af9e3f32c25716a11824b6f5016b47a43f181e9
Audrius <nezinomas@gmail.com>
2021-08-03 17:14:59 +0300

    RemovedInDjango40Warning: django.utils.translation.ugettext_lazy() is deprecated in favor of django.utils.translation.gettext_lazy()

--- project/incomes/models.py
+++ project/incomes/models.py
@@ -2,7 +2,7 @@ from decimal import Decimal
 
 from django.core.validators import MinValueValidator
 from django.db import models
-from django.utils.translation import ugettext_lazy as _
+from django.utils.translation import gettext_lazy as _
 
 from ..accounts.models import Account
 from ..core.mixins.from_db import MixinFromDbAccountId
