fc2bd0500e21522e4c49e4ce578823984544c4d7
Audrius <nezinomas@gmail.com>
2021-08-05 12:33:58 +0300

    Pensions managers

--- project/pensions/models.py
+++ project/pensions/models.py
@@ -9,20 +9,7 @@ from ..core.lib import utils
 from ..core.mixins.queryset_sum import SumMixin
 from ..core.models import TitleAbstract
 from ..journals.models import Journal
-
-
-class PensionTypeQuerySet(models.QuerySet):
-    def related(self):
-        journal = utils.get_user().journal
-        return (
-            self
-            .select_related('journal')
-            .filter(journal=journal)
-        )
-
-    def items(self, year: int = None):
-        return self.related()
-
+from . import managers
 
 class PensionType(TitleAbstract):
     journal = models.ForeignKey(
@@ -32,73 +19,11 @@ class PensionType(TitleAbstract):
     )
 
     # Managers
-    objects = PensionTypeQuerySet.as_manager()
+    objects = managers.PensionTypeQuerySet.as_manager()
 
     class Meta:
         unique_together = ['journal', 'title']
         ordering = ['title']
-
-
-
-class PensionQuerySet(SumMixin, models.QuerySet):
-    def related(self):
-        journal = utils.get_user().journal
-        qs = (
-            self
-            .select_related('pension_type')
-            .filter(pension_type__journal=journal)
-        )
-        return qs
-
-    def year(self, year):
-        return self.related().filter(date__year=year)
-
-    def items(self):
-        return self.related().all()
-
-    def summary(self, year: int) -> List[Dict[str, Any]]:
-        '''
-        return:
-            {
-                'id': account.id,
-                'title': account.title,
-                's_past': Decimal(),
-                's_now': Decimal()
-            }
-        '''
-        return (
-            self
-            .related()
-            .annotate(cnt=Count('pension_type'))
-            .values('cnt')
-            .order_by('cnt')
-            .annotate(
-                s_past=Sum(
-                    Case(
-                        When(**{'date__year__lt': year}, then='price'),
-                        default=Decimal(0))),
-                s_now=Sum(
-                    Case(
-                        When(**{'date__year': year}, then='price'),
-                        default=Decimal(0))),
-                s_fee_past=Sum(
-                    Case(
-                        When(**{'date__year__lt': year}, then='fee'),
-                        default=Decimal(0))),
-                s_fee_now=Sum(
-                    Case(
-                        When(**{'date__year': year}, then='fee'),
-                        default=Decimal(0)))
-            )
-            .values(
-                's_past',
-                's_now',
-                's_fee_now',
-                's_fee_past',
-                title=models.F('pension_type__title'),
-                id=models.F('pension_type__pk')
-            )
-        )
 
 
 class Pension(models.Model):
@@ -133,39 +58,7 @@ class Pension(models.Model):
         return f'{(self.date)}: {self.pension_type}'
 
     # managers
-    objects = PensionQuerySet.as_manager()
-
-
-class PensionBalanceQuerySet(models.QuerySet):
-    def related(self):
-        journal = utils.get_user().journal
-        qs = (
-            self
-            .select_related('pension_type')
-            .filter(pension_type__journal=journal)
-        )
-        return qs
-
-    def items(self):
-        return self.related()
-
-    def year(self, year: int):
-        qs = self.related().filter(year=year)
-
-        return qs.values(
-            'year',
-            'past_amount',
-            'past_fee',
-            'fees',
-            'invested',
-            'incomes',
-            'market_value',
-            'profit_incomes_proc',
-            'profit_incomes_sum',
-            'profit_invested_proc',
-            'profit_invested_sum',
-            title=F('pension_type__title')
-        )
+    objects = managers.PensionQuerySet.as_manager()
 
 
 class PensionBalance(models.Model):
@@ -189,7 +82,7 @@ class PensionBalance(models.Model):
     profit_invested_sum = models.FloatField(default=0.0)
 
     # Managers
-    objects = PensionBalanceQuerySet.as_manager()
+    objects = managers.PensionBalanceQuerySet.as_manager()
 
     def __str__(self):
         return f'{self.pension_type.title}'
