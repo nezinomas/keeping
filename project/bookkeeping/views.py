3effd7e22ed40d5f52f323f0457df76ab2ebdd5f
Audrius <nezinomas@gmail.com>
2021-10-28 15:55:08 +0300

    pensions II in summary

--- project/bookkeeping/views.py
+++ project/bookkeeping/views.py
@@ -219,6 +219,7 @@ class SummarySavings(IndexMixin):
         context['funds'] = chart_data(qs.filter(type='funds'))
         context['shares'] = chart_data(qs.filter(type='shares'))
         context['pensions'] = chart_data(qs.filter(type='pensions'))
+        context['pensions2'] = chart_data(PensionBalance.objects.sum_by_year())
         context['all'] = chart_data(SavingBalance.objects.sum_by_year())
 
         return context
