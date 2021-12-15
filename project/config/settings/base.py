3e089e4cfe97b4d6335354951cf8c3f7723991bf
Audrius <nezinomas@gmail.com>
2021-12-15 21:40:32 +0200

    rename file

--- project/config/settings/base.py
+++ project/config/settings/base.py
@@ -74,7 +74,7 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
         'OPTIONS': {
-            'read_default_file': os.path.join(PROJECT_ROOT, '_config_db.cnf'),
+            'read_default_file': os.path.join(PROJECT_ROOT, '.db'),
         },
     }
 }
