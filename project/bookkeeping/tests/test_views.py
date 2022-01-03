93d0523c44abdc433f2a3813c6265d78f88a0937
Audrius <nezinomas@gmail.com>
2022-01-03 15:37:57 +0200

    coverage 100%

--- project/bookkeeping/tests/test_views.py
+++ project/bookkeeping/tests/test_views.py
@@ -697,6 +697,81 @@ def test_view_detailed_rendered_expenses(client_logged, expenses):
     assert "Išlaidos / Expense Type" in content
 
 
+def test_view_detailed_no_expenses(client_logged):
+    url = reverse('bookkeeping:detailed')
+    response = client_logged.get(url)
+
+    assert response.status_code == 200
+
+    content = response.content.decode("utf-8")
+
+    assert "Išlaidos / " not in content
+
+
+def test_view_detailed_no_expenses_with_types(client_logged):
+    ExpenseTypeFactory()
+
+    url = reverse('bookkeeping:detailed')
+    response = client_logged.get(url)
+
+    assert response.status_code == 200
+
+    content = response.content.decode("utf-8")
+
+    assert "Išlaidos / Expense Type" not in content
+
+
+def test_view_detailed_with_incomes(client_logged):
+    IncomeFactory()
+
+    url = reverse('bookkeeping:detailed')
+    response = client_logged.get(url)
+
+    assert response.status_code == 200
+
+    content = response.content.decode("utf-8")
+
+    assert "<th>Pajamos</th>" in content
+    assert "Income Type" in content
+
+
+def test_view_detailed_no_incomes(client_logged):
+    url = reverse('bookkeeping:detailed')
+    response = client_logged.get(url)
+
+    assert response.status_code == 200
+
+    content = response.content.decode("utf-8")
+
+    assert "<th>Pajamos</th>" not in content
+    assert "Income Type</td>" not in content
+
+
+def test_view_detailed_no_savings(client_logged):
+    url = reverse('bookkeeping:detailed')
+    response = client_logged.get(url)
+
+    assert response.status_code == 200
+
+    content = response.content.decode("utf-8")
+
+    assert "<th>Taupymas</th>" not in content
+
+
+def test_view_detailed_with_savings(client_logged):
+    SavingFactory()
+
+    url = reverse('bookkeeping:detailed')
+    response = client_logged.get(url)
+
+    assert response.status_code == 200
+
+    content = response.content.decode("utf-8")
+
+    assert "<th>Taupymas</th>" in content
+    assert "Savings</td>" in content
+
+
 # ---------------------------------------------------------------------------------------
 #                                                                                 Summary
 # ---------------------------------------------------------------------------------------
