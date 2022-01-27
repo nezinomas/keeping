5300a3049d2e8dc8b89c316ada3f9b7379ecbe46
Audrius <nezinomas@gmail.com>
2022-01-27 11:07:21 +0200

    comment

--- project/core/mixins/from_db.py
+++ project/core/mixins/from_db.py
@@ -1,7 +1,10 @@
 from django.db import models
 
 
-
+# from_db method returns dictionary _old_values
+# in case account changed: _old_values={'account_id': [old_account_id, new_account_id]}
+# in case account same: _old_values={'account_id': [account_id]}
+# _old_values used in signals to find out which accounts to update
 class FromDbAccountIdMixin():
     class Meta:
         abstract = True
