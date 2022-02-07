e0eb64db048179b78858f60da24321b8d858a4be
Audrius <nezinomas@gmail.com>
2022-02-07 10:24:10 +0200

    refactoring extracted class

--- project/core/mixins/account_balance.py
+++ project/core/mixins/account_balance.py
@@ -51,7 +51,52 @@ def _getattr(obj, name, default=None):
         return default
 
 
-class AccountBalanceMixin():
+class CalcAccountBalanceTableFields():
+    def _calc_field(self, /, caller, field_value):
+        price = float(self.price)
+        original_price = float(self.original_price)
+
+        _switch = {
+            'update': field_value - original_price + price,
+            'new': field_value + price,
+            'delete': field_value - original_price
+        }
+        return _switch.get(caller, field_value)
+
+    def update_balance_tbl(self, caller, balance_tbl_field_name, pk):
+        _year = self.date.year
+        try:
+            _qs = (
+                apps.get_model('accounts.AccountBalance')
+                .objects
+                .get(Q(year=_year) & Q(account_id=pk))
+            )
+
+        except ObjectDoesNotExist as e:
+            print('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\nquery failed -> UPDATE ACCOUNT CLASS CALLED\n')
+            UpdatetBalanceTable(category_table='accounts.Account',
+                                balance_table='accounts.AccountBalance',
+                                balance_object=Balance,
+                                hooks=HOOKS)
+            raise e
+
+        print(
+            f'\n\nquery before save:\n{_qs.year=}\n{_qs.past=}\n{_qs.incomes=}\n{_qs.expenses=}\n{_qs.have=}')
+        _balance_tbl_field_value = getattr(_qs, balance_tbl_field_name)
+        _balance_tbl_field_value = self._calc_field(
+            caller, _balance_tbl_field_value)
+
+        setattr(_qs, balance_tbl_field_name, _balance_tbl_field_value)
+
+        _qs.balance = calc.calc_balance([_qs.past, _qs.incomes, _qs.expenses])
+        _qs.delta = calc.calc_delta([_qs.have, _qs.balance])
+
+        print(
+            f'\n\nquery after save:\n{_qs.year=}\n{_qs.past=}\n{_qs.incomes=}\n{_qs.expenses=}\n{_qs.have=}')
+        _qs.save()
+
+
+class AccountBalanceMixin(CalcAccountBalanceTableFields):
     original_price = 0.0
     old_values = {}
 
@@ -94,56 +139,14 @@ class AccountBalanceMixin():
                     caller = caller if caller else 'delete'
                     print(
                         f'\n\n>>>{caller=} {_hook=} {_balance_tbl_field_name=} {_account_name=} {_account.pk=} {_old_account_id=}')
-                    self._update_balance_tbl(caller, _balance_tbl_field_name, _account.pk)
+                    self.update_balance_tbl(caller, _balance_tbl_field_name, _account.pk)
                 except ObjectDoesNotExist:
                     return
             else:
                 try:
                     print(f'\n\n>>> caller: {_hook=} new {_account.pk=}')
-                    self._update_balance_tbl('new', _balance_tbl_field_name ,_account.pk)
+                    self.update_balance_tbl('new', _balance_tbl_field_name ,_account.pk)
                     print(f'\n\n>>> caller: {_hook=} delete {_old_account_id=}')
-                    self._update_balance_tbl('delete', _balance_tbl_field_name, _old_account_id)
+                    self.update_balance_tbl('delete', _balance_tbl_field_name, _old_account_id)
                 except ObjectDoesNotExist:
                     return
-
-    def _calc_field(self, /, caller, field_value):
-        price = float(self.price)
-        original_price = float(self.original_price)
-
-        _switch = {
-            'update': field_value - original_price + price,
-            'new': field_value + price,
-            'delete': field_value - original_price
-        }
-        return _switch.get(caller, field_value)
-
-    def _update_balance_tbl(self, caller, balance_tbl_field_name, pk):
-        _year = self.date.year
-        try:
-            _qs = (
-                apps.get_model('accounts.AccountBalance')
-                .objects
-                .get(Q(year=_year) & Q(account_id=pk))
-            )
-
-        except ObjectDoesNotExist as e:
-            print('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\nquery failed -> UPDATE ACCOUNT CLASS CALLED\n')
-            UpdatetBalanceTable(category_table='accounts.Account',
-                                balance_table='accounts.AccountBalance',
-                                balance_object=Balance,
-                                hooks=HOOKS)
-            raise e
-
-        print(
-            f'\n\nquery before save:\n{_qs.year=}\n{_qs.past=}\n{_qs.incomes=}\n{_qs.expenses=}\n{_qs.have=}')
-        _balance_tbl_field_value = getattr(_qs, balance_tbl_field_name)
-        _balance_tbl_field_value = self._calc_field(caller, _balance_tbl_field_value)
-
-        setattr(_qs, balance_tbl_field_name, _balance_tbl_field_value)
-
-        _qs.balance = calc.calc_balance([_qs.past, _qs.incomes, _qs.expenses])
-        _qs.delta = calc.calc_delta([_qs.have, _qs.balance])
-
-        print(
-            f'\n\nquery after save:\n{_qs.year=}\n{_qs.past=}\n{_qs.incomes=}\n{_qs.expenses=}\n{_qs.have=}')
-        _qs.save()
