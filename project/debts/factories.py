700da0e3d59db16241151a261964d3a1fb12ff14
Audrius <nezinomas@gmail.com>
2022-02-15 16:21:46 +0200

    debt factories

--- project/debts/factories.py
+++ project/debts/factories.py
@@ -8,26 +8,53 @@ from ..journals.factories import JournalFactory
 from . import models
 
 
-class DebtFactory(factory.django.DjangoModelFactory):
+class BorrowFactory(factory.django.DjangoModelFactory):
     class Meta:
         model = models.Debt
 
     date = dt(1999, 1, 1)
+    type = 'borrow'
     name = factory.Faker('first_name')
     price = Decimal('100')
     returned = Decimal('25')
     closed = False
-    remark = 'Debt Remark'
+    remark = 'Borrow Remark'
     account = factory.SubFactory(AccountFactory)
     journal = factory.SubFactory(JournalFactory)
 
 
-class DebtReturnFactory(factory.django.DjangoModelFactory):
+class BorrowReturnFactory(factory.django.DjangoModelFactory):
     class Meta:
         model = models.DebtReturn
 
     date = dt(1999, 1, 2)
     price = Decimal('5')
-    remark = 'Debt Return Remark'
+    remark = 'Borrow Return Remark'
     account = factory.SubFactory(AccountFactory)
-    debt = factory.SubFactory(DebtFactory)
+    borrow = factory.SubFactory(BorrowFactory)
+
+
+class LendFactory(factory.django.DjangoModelFactory):
+    class Meta:
+        model = models.Debt
+
+    date = dt(1999, 1, 1)
+    type = 'lend'
+    name = factory.Faker('first_name')
+    price = Decimal('100')
+    returned = Decimal('25')
+    closed = False
+    remark = 'Lend Remark'
+    account = factory.SubFactory(AccountFactory)
+    journal = factory.SubFactory(JournalFactory)
+
+
+class LendReturnFactory(factory.django.DjangoModelFactory):
+    class Meta:
+        model = models.DebtReturn
+
+    date = dt(1999, 1, 2)
+    price = Decimal('5')
+    remark = 'Lend Return Remark'
+    account = factory.SubFactory(AccountFactory)
+    lent = factory.SubFactory(LendFactory)
