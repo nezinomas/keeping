b508da53d5c712e958df249b46bfaea510e270b1
Audrius <nezinomas@gmail.com>
2022-03-10 10:49:27 +0200

    rename methods

--- project/drinks/lib/drinks_options.py
+++ project/drinks/lib/drinks_options.py
@@ -11,15 +11,15 @@ class DrinksOptions():
     @property
     def ratio(self) -> float:
         ratios = {
-            'beer': DrinksOptions.to_beer(1),
-            'wine': DrinksOptions.to_wine(1),
-            'vodka': DrinksOptions.to_vodka(1)
+            'beer': DrinksOptions.std_to_beer(1),
+            'wine': DrinksOptions.std_to_wine(1),
+            'vodka': DrinksOptions.std_to_vodka(1)
         }
 
         return ratios.get(self._drink_type, 1)
 
     @staticmethod
-    def to_beer(av: float) -> float:
+    def std_to_beer(av: float) -> float:
         # one 500ml bottle ~ 2.5 std av
         return av * 0.4
 
@@ -29,7 +29,7 @@ class DrinksOptions():
         return av / 0.4
 
     @staticmethod
-    def to_wine(av: float) -> float:
+    def std_to_wine(av: float) -> float:
         # one 750ml bottle ~ 8 std av
         return av * 0.125
 
@@ -39,7 +39,7 @@ class DrinksOptions():
         return av / 0.125
 
     @staticmethod
-    def to_vodka(av: float) -> float:
+    def std_to_vodka(av: float) -> float:
         # one 1000ml bottle ~ 40 std av
         return av * 0.025
 
