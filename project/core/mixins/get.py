6ac9e9cffa5333fa92977e7495dc56bd68b0cdaf
Audrius <nezinomas@gmail.com>
2022-04-07 13:59:51 +0300

    delete get_context_data in GetQuerysetMixin

--- project/core/mixins/get.py
+++ project/core/mixins/get.py
@@ -2,8 +2,7 @@ from django.core.exceptions import ObjectDoesNotExist
 
 
 class GetQuerysetMixin():
-    context_object_name = 'items'
-    object_list = 'objects'
+    object_list = 'items'
 
     def get_queryset(self):
         year = self.request.user.year
@@ -29,12 +28,3 @@ class GetQuerysetMixin():
                 pass
 
         return obj
-
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-
-        no_items = kwargs.get('no_items')
-        if not no_items:
-            context[self.context_object_name] = self.get_queryset()
-
-        return context
