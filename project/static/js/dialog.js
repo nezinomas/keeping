dd8277226bc4c1a6522d9c07899da09a9fe44bd7
Audrius <nezinomas@gmail.com>
2022-04-11 14:56:23 +0300

    changes in dialog.js

--- project/static/js/dialog.js
+++ project/static/js/dialog.js
@@ -1,3 +1,33 @@
+$(document).keydown(function (event) {
+    if (event.keyCode == 27) {
+        $('#modal-form').modal("hide");
+    }
+});
+
+// eh?
+// $(document).on('submit', '.js_form', function (e) {
+//     e.preventDefault();
+// });
+
+// replace dot in year field
+$(document).ready(function () {
+    var fields = ['year', 'closed', 'valid_for'];
+    var len = fields.length;
+    for (i = 0; i < len; i++) {
+        var field = fields[i];
+        var year = $(`#id_${field}:text`).attr('value')
+        if (year) {
+            year = year.replace(".", "");
+            year = year.replace(",", "");
+
+            $(`#id_${field}`).attr('value', year);
+            $(`#id_${field}:text`).val(year);
+        }
+    }
+});
+
+
+// HTMX related functions
 const modal = new bootstrap.Modal(document.getElementById("modal"))
 
 htmx.on("htmx:afterSwap", (e) => {
@@ -7,11 +37,20 @@ htmx.on("htmx:afterSwap", (e) => {
     }
 })
 
-function save() {
+function save_new() {
     htmx.on("htmx:beforeSwap", (e) => {
         // Empty response targeting #dialog => hide the modal
         if (e.detail.target.id == "dialog" && !e.detail.xhr.response) {
-                $('#modal form')[0].reset();
+            $('#modal form')[0].reset();
+            e.detail.shouldSwap = false;
+        }
+    })
+}
+
+function update() {
+    htmx.on("htmx:beforeSwap", (e) => {
+        // Empty response targeting #dialog => hide the modal
+        if (e.detail.target.id == "dialog" && !e.detail.xhr.response) {
             e.detail.shouldSwap = false;
         }
     })
@@ -28,31 +67,3 @@ function save_and_close() {
         }
     })
 }
-
-$(document).keydown(function (event) {
-    if (event.keyCode == 27) {
-        $('#modal-form').modal("hide");
-    }
-});
-
-// eh?
-$(document).on('submit', '.js_form', function (e) {
-    e.preventDefault();
-});
-
-// replace dot in year field
-$(document).ready(function () {
-    var fields = ['year', 'closed', 'valid_for'];
-    var len = fields.length;
-    for(i = 0; i < len; i++) {
-        var field = fields[i];
-        var year = $(`#id_${field}:text`).attr('value')
-        if (year) {
-            year = year.replace(".", "");
-            year = year.replace(",", "");
-
-            $(`#id_${field}`).attr('value', year);
-            $(`#id_${field}:text`).val(year);
-        }
-    }
-});
