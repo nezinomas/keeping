e72a061334f1eed277bf33268a3f8984d61e3af1
Audrius <nezinomas@gmail.com>
2022-04-27 16:16:10 +0300

    urls reorganize

--- project/counts/urls.py
+++ project/counts/urls.py
@@ -8,36 +8,46 @@ app_name = App_name
 urlpatterns = [
     path(
         'counts/',
-        views.Dispatch.as_view(),
-        name='dispatch'
+        views.Redirect.as_view(),
+        name='redirect'
     ),
     path(
-        'counts/index/<slug:slug>/',
+        'counts/none/',
+        views.Empty.as_view(),
+        name='empty'
+    ),
+    path(
+        'counts/<slug:slug>/',
+        views.Main.as_view(),
+        name='main'
+    ),
+    path(
+        'counts/<slug:slug>/index/',
         views.Index.as_view(),
         name='index'
     ),
     path(
-        'counts/lists/<slug:slug>/',
+        'counts/<slug:slug>/list/',
         views.Lists.as_view(),
         name='list'
     ),
     path(
-        'counts/history/<slug:slug>/',
+        'counts/<slug:slug>/history/',
         views.History.as_view(),
         name='history'
     ),
     path(
-        'counts/new/<slug:slug>/',
+        'counts/<slug:slug>/new/',
         views.New.as_view(),
         name='new'
     ),
     path(
-        'counts/update/<slug:slug>/<int:pk>/',
+        'counts/<slug:slug>/update/<int:pk>/',
         views.Update.as_view(),
         name='update'
     ),
     path(
-        'counts/delete/<slug:slug>/<int:pk>/',
+        'counts/<slug:slug>/delete/<int:pk>/',
         views.Delete.as_view(),
         name='delete'
     ),
@@ -47,12 +57,12 @@ urlpatterns = [
         name='type_new'
     ),
     path(
-        'counts/type/update/<slug:slug>/',
+        'counts/type/<slug:slug>/update/',
         views.TypeUpdate.as_view(),
         name='type_update'
     ),
     path(
-        'counts/type/delete/<slug:slug>/',
+        'counts/type/<slug:slug>/delete/',
         views.TypeDelete.as_view(),
         name='type_delete'
     ),
