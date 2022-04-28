0c44169b71a558c7e34624eb4d211bc765c3d8e0
Audrius <nezinomas@gmail.com>
2022-04-28 13:24:03 +0300

    counts models managers updates

--- project/counts/managers.py
+++ project/counts/managers.py
@@ -8,80 +8,86 @@ from ..core.mixins.queryset_sum import SumMixin
 from ..users.models import User
 
 
-class CountQuerySet(models.QuerySet):
-    counter_type = None
-
-    def related(self, user: User = None, counter_type=None):
-        if not user:
-            user = utils.get_user()
-
-        # set counter_type then editing counter title
-        self.counter_type = counter_type if counter_type else self.counter_type
-
-        if not self.counter_type:
-            self.counter_type = utils.get_request_kwargs('slug')
-
+class CountQuerySet(SumMixin, models.QuerySet):
+    def related(self):
         return (
             self
             .select_related('user')
-            .filter(user=user)
-            .filter(counter_type__iexact=self.counter_type)
+            .filter(user=utils.get_user())
             .order_by('-date')
         )
 
-    def year(self, year, user: User = None):
-        return (
-            self
-            .related(user)
-            .filter(date__year=year)
-        )
+    def year(self, year, count_type=None):
+        qs = self.related()
 
-    def items(self, user: User = None):
-        return self.related(user)
+        if count_type:
+            qs = qs.filter(count_type__slug=count_type)
 
-    def sum_by_year(self, year: int = None) -> List[Dict[date, float]]:
+        qs = qs.filter(date__year=year)
+
+        return qs
+
+    def items(self, count_type=None):
+        qs = self.related()
+        if count_type:
+            qs = qs.filter(count_type__slug=count_type)
+
+        return qs
+
+    def sum_by_year(self, year=None, count_type=None):
         # Returns
         # QuerySet [{'date': datetime.date, 'qty': float}]
+        qs = self.related()
 
-        return (
-            self
-            .related()
+        if count_type:
+            qs = qs.filter(count_type__slug=count_type)
+
+        qs = qs\
             .year_sum(
                 year=year,
                 sum_annotation='qty',
-                sum_column='quantity')
+                sum_column='quantity')\
             .order_by('date')
-        )
 
-    def sum_by_month(self, year: int, month: int = None) -> List[Dict[date, float]]:
+        return qs
+
+    def sum_by_month(self, year, count_type=None, month=None) -> List[Dict[date, float]]:
         # Returns
         # QuerySet [{'date': datetime.date, 'qty': float}]
+        qs = self.related()
 
-        return (
-            self
-            .related()
+        if count_type:
+            qs = qs.filter(count_type__slug=count_type)
+
+        qs = qs\
+            .related()\
             .month_sum(
                 year=year,
                 month=month,
                 sum_annotation='qty',
-                sum_column='quantity')
+                sum_column='quantity')\
             .order_by('date')
-        )
 
-    def sum_by_day(self, year: int, month: int = None) -> List[Dict[date, float]]:
+        return qs
+
+    def sum_by_day(self, year, count_type=None, month=None) -> List[Dict[date, float]]:
         # Returns
         # QuerySet [{'date': datetime.date, 'qty': float}]
+        qs = self.related()
 
-        return (
-            self
-            .related()
+        if count_type:
+            qs = qs.filter(count_type__slug=count_type)
+
+        qs = qs\
+            .related()\
             .day_sum(
                 year=year,
                 month=month,
                 sum_annotation='qty',
-                sum_column='quantity')
+                sum_column='quantity')\
             .order_by('date')
-        )
+
+        return qs
 
 
 class CountTypeQuerySet(models.QuerySet):
