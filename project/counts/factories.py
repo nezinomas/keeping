10d9893f34d7be836dd8b843c6c8c331c6eeea59
Audrius <nezinomas@gmail.com>
2022-04-28 13:23:26 +0300

    factories update

--- project/counts/factories.py
+++ project/counts/factories.py
@@ -6,19 +6,20 @@ from ..users.factories import UserFactory
 from .models import Count, CountType
 
 
+class CountTypeFactory(factory.django.DjangoModelFactory):
+    class Meta:
+        model = CountType
+        django_get_or_create = ('title','user',)
+
+    user = factory.SubFactory(UserFactory)
+    title = 'Count Type'
+
+
 class CountFactory(factory.django.DjangoModelFactory):
     class Meta:
         model = Count
 
+    user = factory.SubFactory(UserFactory)
     date = date(1999, 1, 1)
+    count_type = factory.SubFactory(CountTypeFactory)
     quantity = 1
-    count_type = factory.SubFactory(CountType)
-    user = factory.SubFactory(UserFactory)
-
-
-class CountTypeFactory(factory.django.DjangoModelFactory):
-    class Meta:
-        model = CountType
-
-    user = factory.SubFactory(UserFactory)
-    title = 'Count Type'
