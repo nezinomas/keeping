5657b93e7ce199401322336bb246caec2d87abbd
Audrius <nezinomas@gmail.com>
2022-05-02 16:22:41 +0300

    transactions forms refactoring, extracted methods

--- project/transactions/forms.py
+++ project/transactions/forms.py
@@ -25,6 +25,15 @@ class TransactionForm(YearBetweenMixin, forms.ModelForm):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
 
+        self._initial_fields_values()
+        self._overwrite_default_queries()
+        self._set_htmx_attributes()
+        self._translate_fields()
+
+        self.helper = FormHelper()
+        set_field_properties(self, self.helper)
+
+    def _initial_fields_values(self):
         self.fields['date'].widget = DatePickerInput(
             options={
                 "format": "YYYY-MM-DD",
@@ -36,25 +45,41 @@ class TransactionForm(YearBetweenMixin, forms.ModelForm):
         self.fields['price'].label = _('Amount')
         self.fields['date'].initial = set_year_for_form()
 
-        # overwrite ForeignKey expense_type queryset
-        self.fields['from_account'].queryset = Account.objects.items()
-        self.fields['to_account'].queryset = Account.objects.items()
+    def _overwrite_default_queries(self):
+        from_account = self.fields['from_account']
+        to_account = self.fields['to_account']
 
-        # field labels
-        self.fields['date'].label = _('Date')
-        self.fields['from_account'].label = _('From account')
-        self.fields['to_account'].label = _('To account')
+        from_account.queryset = Account.objects.items()
+        to_account.queryset = Account.objects.items()
 
-        self.helper = FormHelper()
-        set_field_properties(self, self.helper)
+        if not self.instance.pk:
+            from_account_pk = self.data.get('from_account')
+        else:
+            from_account_pk = self.instance.from_account.pk
 
-        # htmx attributes for chained dropdown
+        try:
+            from_account_pk = int(from_account_pk)
+        except (ValueError, TypeError):
+            from_account_pk = None
+
+        if from_account_pk:
+            to_account.queryset = \
+                Account.objects \
+                .items() \
+                .exclude(pk=from_account_pk)
+
+    def _set_htmx_attributes(self):
         url = reverse('accounts:load')
 
         field = self.fields['from_account']
         field.widget.attrs['hx-get'] = url
         field.widget.attrs['hx-target'] = '#id_to_account'
         field.widget.attrs['hx-trigger'] = 'change'
+
+    def _translate_fields(self):
+        self.fields['date'].label = _('Date')
+        self.fields['from_account'].label = _('From account')
+        self.fields['to_account'].label = _('To account')
 
 
 class SavingCloseForm(YearBetweenMixin, forms.ModelForm):
@@ -69,26 +94,29 @@ class SavingCloseForm(YearBetweenMixin, forms.ModelForm):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
 
+        self._initial_fields_values()
+        self._overwrite_default_queries()
+        self._translate_fields()
+        self._set_css_classes()
+
+    def _initial_fields_values(self):
         self.fields['date'].widget = DatePickerInput(
             options={
                 "format": "YYYY-MM-DD",
                 "locale": utils.get_user().journal.lang,
             })
 
-        # form input settings
         self.fields['price'].widget.attrs = {'step': '0.01'}
         self.fields['fee'].widget.attrs = {'step': '0.01'}
 
-        # form initial values
         self.fields['date'].initial = set_year_for_form()
 
-        # overwrite ForeignKey expense_type queryset
+    def _overwrite_default_queries(self):
         self.fields['from_account'].queryset = SavingType.objects.items()
         self.fields['to_account'].queryset = Account.objects.items()
 
-        # form fields labels
+    def _translate_fields(self):
         self.fields['price'].label = _('Amount')
-        # 'Suma kuri lieka atskaičius mokesčius'
         self.fields['price'].help_text = _('Amount left after fee')
         self.fields['fee'].label = _('Fees')
         self.fields['date'].label = _('Date')
@@ -96,6 +124,7 @@ class SavingCloseForm(YearBetweenMixin, forms.ModelForm):
         self.fields['to_account'].label = _('To account')
         self.fields['close'].label = mark_safe(f"{_('Close')} <b>{_('From account')}</b>")
 
+    def _set_css_classes(self):
         self.helper = FormHelper()
         set_field_properties(self, self.helper)
 
@@ -124,6 +153,13 @@ class SavingChangeForm(YearBetweenMixin, forms.ModelForm):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
 
+        self._initial_fields_values()
+        self._overwrite_default_queries()
+        self._set_htmx_attributes()
+        self._translate_fields()
+        self._set_css_classes()
+
+    def _initial_fields_values(self):
         self.fields['date'].widget = DatePickerInput(
             options={
                 "format": "YYYY-MM-DD",
@@ -137,11 +173,38 @@ class SavingChangeForm(YearBetweenMixin, forms.ModelForm):
         # initial values
         self.fields['date'].initial = set_year_for_form()
 
-        # overwrite ForeignKey expense_type queryset
-        self.fields['from_account'].queryset = SavingType.objects.items()
-        self.fields['to_account'].queryset = SavingType.objects.items()
+    def _overwrite_default_queries(self):
+        from_account = self.fields['from_account']
+        to_account = self.fields['to_account']
 
-        # fields labels
+        from_account.queryset = SavingType.objects.items()
+        to_account.queryset = SavingType.objects.items()
+
+        if not self.instance.pk:
+            from_account_pk = self.data.get('from_account')
+        else:
+            from_account_pk = self.instance.from_account.pk
+
+        try:
+            from_account_pk = int(from_account_pk)
+        except (ValueError, TypeError):
+            from_account_pk = None
+
+        if from_account_pk:
+            to_account.queryset = \
+                SavingType.objects \
+                .items() \
+                .exclude(pk=from_account_pk)
+
+    def _set_htmx_attributes(self):
+        url = reverse('transactions:load_saving_type')
+
+        field = self.fields['from_account']
+        field.widget.attrs['hx-get'] = url
+        field.widget.attrs['hx-target'] = '#id_to_account'
+        field.widget.attrs['hx-trigger'] = 'change'
+
+    def _translate_fields(self):
         self.fields['price'].label = _('Amount')
         self.fields['price'].help_text = _('Amount left after fee')
         self.fields['fee'].label = _('Fees')
@@ -150,18 +213,11 @@ class SavingChangeForm(YearBetweenMixin, forms.ModelForm):
         self.fields['to_account'].label = _('To account')
         self.fields['close'].label = mark_safe(f"{_('Close')} <b>{_('From account')}</b>")
 
+    def _set_css_classes(self):
         self.helper = FormHelper()
         set_field_properties(self, self.helper)
 
         self.fields['close'].widget.attrs['class'] = " form-check-input"
-
-        # htmx attributes for chained dropdown
-        url = reverse('transactions:load_saving_type')
-
-        field = self.fields['from_account']
-        field.widget.attrs['hx-get'] = url
-        field.widget.attrs['hx-target'] = '#id_to_account'
-        field.widget.attrs['hx-trigger'] = 'change'
 
     def save(self):
         close = self.cleaned_data.get('close')
