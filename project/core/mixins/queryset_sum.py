934e0c20abc6e6788af03b10ce9aa3c8fcb70381
Audrius <nezinomas@gmail.com>
2022-05-03 13:55:13 +0300

    nsvrb

--- project/core/mixins/queryset_sum.py
+++ project/core/mixins/queryset_sum.py
@@ -19,59 +19,48 @@ class SumMixin():
         return self
     month_filter.queryset_only = True
 
-    def year_sum(self,
-                 year=None,
-                 sum_annotation='sum',
-                 groupby='id',
+    def year_sum(self, year=None,
+                 sum_annotation='sum', groupby='id',
                  sum_column='price'):
-        return (
-            self
-            .year_filter(year)
-            .annotate(cnt=Count(groupby))
-            .values(groupby)
-            .annotate(date=TruncYear('date'))
-            .values('date')
-            .annotate(c=Count('id'))
-            .annotate(**{sum_annotation: Sum(sum_column)})
-            .order_by('date')
-            .annotate(year=ExtractYear(F('date')))
+        return \
+            self \
+            .year_filter(year) \
+            .annotate(cnt=Count(groupby)) \
+            .values(groupby) \
+            .annotate(date=TruncYear('date')) \
+            .values('date') \
+            .annotate(c=Count('id')) \
+            .annotate(**{sum_annotation: Sum(sum_column)}) \
+            .order_by('date') \
+            .annotate(year=ExtractYear(F('date'))) \
             .values('year', sum_annotation)
-        )
 
-    def month_sum(self,
-                  year,
-                  month=None,
-                  sum_annotation='sum',
-                  sum_column='price',
+    def month_sum(self, year, month=None,
+                  sum_annotation='sum', sum_column='price',
                   groupby='id'):
-        return (
-            self
-            .year_filter(year)
-            .month_filter(month)
-            .annotate(cnt=Count(groupby))
-            .values(groupby)
-            .annotate(date=TruncMonth('date'))
-            .values('date')
-            .annotate(c=Count('id'))
-            .annotate(**{sum_annotation: Sum(sum_column)})
-            .order_by('date')
-            .values('date', sum_annotation)
-        )
+        return \
+            self \
+            .year_filter(year) \
+            .month_filter(month) \
+            .annotate(cnt=Count(groupby)) \
+            .values(groupby) \
+            .annotate(date=TruncMonth('date')) \
+            .values('date') \
+            .annotate(c=Count('id')) \
+            .annotate(**{sum_annotation: Sum(sum_column)}) \
+            .order_by('date') \
+            .values('date', sum_annotation) \
 
-    def day_sum(self,
-                year,
-                month=None,
-                sum_annotation='sum',
-                sum_column='price',
+    def day_sum(self, year, month=None,
+                sum_annotation='sum', sum_column='price',
                 groupby='id'):
-        return (
-            self
-            .year_filter(year)
-            .month_filter(month)
-            .annotate(c=Count(groupby))
-            .values('c')
-            .annotate(date=TruncDay('date'))
-            .annotate(**{sum_annotation: Sum(sum_column)})
-            .order_by('date')
-            .values('date', sum_annotation)
-        )
+        return \
+            self \
+            .year_filter(year) \
+            .month_filter(month) \
+            .annotate(c=Count(groupby)) \
+            .values('c') \
+            .annotate(date=TruncDay('date')) \
+            .annotate(**{sum_annotation: Sum(sum_column)}) \
+            .order_by('date') \
+            .values('date', sum_annotation) \
