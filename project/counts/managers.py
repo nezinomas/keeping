b46726a31e9c73d0219fcb4bfd9db3804abc529f
Audrius <nezinomas@gmail.com>
2022-05-04 14:32:03 +0300

    multiline

--- project/counts/managers.py
+++ project/counts/managers.py
@@ -9,12 +9,11 @@ from ..core.mixins.queryset_sum import SumMixin
 
 class CountQuerySet(SumMixin, models.QuerySet):
     def related(self):
-        return (
-            self
-            .select_related('user')
-            .filter(user=utils.get_user())
+        return \
+            self \
+            .select_related('user') \
+            .filter(user=utils.get_user()) \
             .order_by('-date')
-        )
 
     def year(self, year, count_type=None):
         qs = self.related()
@@ -22,9 +21,7 @@ class CountQuerySet(SumMixin, models.QuerySet):
         if count_type:
             qs = qs.filter(count_type__slug=count_type)
 
-        qs = qs.filter(date__year=year)
-
-        return qs
+        return qs.filter(date__year=year)
 
     def items(self, count_type=None):
         qs = self.related()
@@ -41,14 +38,14 @@ class CountQuerySet(SumMixin, models.QuerySet):
         if count_type:
             qs = qs.filter(count_type__slug=count_type)
 
-        qs = qs \
+        return \
+            qs \
             .year_sum(
                 year=year,
                 sum_annotation='qty',
                 sum_column='quantity') \
             .order_by('date')
 
-        return qs
 
     def sum_by_day(self, year, count_type=None, month=None) -> List[Dict[date, float]]:
         # Returns
@@ -58,15 +55,14 @@ class CountQuerySet(SumMixin, models.QuerySet):
         if count_type:
             qs = qs.filter(count_type__slug=count_type)
 
-        qs = qs \
+        return \
+            qs \
             .day_sum(
                 year=year,
                 month=month,
                 sum_annotation='qty',
                 sum_column='quantity') \
             .order_by('date')
-
-        return qs
 
 
 class CountTypeQuerySet(models.QuerySet):
