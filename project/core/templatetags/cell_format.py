45d498ff7aac84d47398b0598cc4efe82e354c8b
Audrius <nezinomas@gmail.com>
2022-05-04 21:50:55 +0300

    refactorings

--- project/core/templatetags/cell_format.py
+++ project/core/templatetags/cell_format.py
@@ -1,77 +1,68 @@
 from typing import Any
-from decimal import Decimal
 from django import template
 
 register = template.Library()
 
 
 def _to_float(_str: Any) -> float:
-    if type(_str) == float or type(_str) == int or type(_str) == Decimal:
+    if isinstance(_str, str):
+        _str = _str.replace('.', str())
+        _str = _str.replace(',', '.')
+
+    try:
         return float(_str)
-    else:
-        return float(_str.replace('.', '').replace(',', '.'))
+    except ValueError:
+        return _str
 
 
 @register.filter
 def negative(value):
-    _value = ''
-
     try:
         value = float(value)
-    except:
-        return _value
+    except ValueError:
+        return str()
 
     if value < 0:
-        _value = 'table-danger'
+        return 'table-danger'
 
-    return _value
+    return str()
 
 
 @register.filter
 def positive(value):
-    _value = ''
-
     try:
         value = float(value)
-    except:
-        return _value
+    except ValueError:
+        return str()
 
     if value >= 0:
-        _value = 'table-success'
+        return 'table-success'
 
-    return _value
+    return str()
 
 
 @register.filter
 def positive_negative(value):
-    _value = ''
-
     try:
         value = _to_float(value)
-    except:
-        return _value
+    except ValueError:
+        return str()
 
     if value >= 0:
-        _value = 'table-success'
-    else:
-        _value = 'table-danger'
+        return 'table-success'
 
-    return _value
+    return 'table-danger'
 
 
 @register.filter
 def compare(value: str, args: str) -> str:
-    _value = ''
-
     try:
-        value = _to_float(value)
-        compare = _to_float(args)
-    except Exception as ex:
-        return _value
+        _value = _to_float(value)
+        _compare = _to_float(args)
+    except (TypeError, ValueError):
+        return str()
 
-    if value >= compare:
-        _value = 'table-success'
-    else:
-        _value = 'table-danger'
+    if _value >= _compare:
+        return 'table-success'
 
-    return _value
+    return 'table-danger'
