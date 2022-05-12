nezinomas/keeping

commit 4846a1c52263da2e73299cdae50143b07cf39d84

nsvrb

project/transactions/managers.py
@@ -9,18 +9,16 @@ from ..core.mixins.queryset_sum import SumMixin
 class BaseMixin(models.QuerySet):
     def related(self):
         journal = utils.get_user().journal
-        return (
-            self
-            .select_related('from_account', 'to_account')
+        return \
+            self \
+            .select_related('from_account', 'to_account') \
             .filter(from_account__journal=journal, to_account__journal=journal)
-        )
 
     def year(self, year):
-        return (
-            self
-            .related()
+        return \
+            self \
+            .related() \
             .filter(date__year=year)
-        )
 
     def items(self):
         return self.related()
@@ -30,22 +28,18 @@ class BaseMixin(models.QuerySet):
         method used only in post_save signal
         method sum prices by year
         '''
-        return (
-            self
-            .related()
-            .annotate(year=ExtractYear(F('date')))
-            .values('year', 'to_account__title')
-            .annotate(incomes=Sum('price'))
-            .values('year', 'incomes', id=F('to_account__pk'))
+        return \
+            self \
+            .related() \
+            .annotate(year=ExtractYear(F('date'))) \
+            .values('year', 'to_account__title') \
+            .annotate(incomes=Sum('price')) \
+            .values('year', 'incomes', id=F('to_account__pk')) \
             .order_by('year', 'id')
-        )
 
     def annotate_fee(self, fee):
         if fee:
-            return (
-                self
-                .annotate(fee=Sum('fee'))
-            )
+            return self.annotate(fee=F('fee'))
         return self
     annotate_fee.queryset_only = True
 
@@ -59,16 +53,15 @@ class BaseMixin(models.QuerySet):
         if fee:
             values.append('fee')
 
-        return (
-            self
-            .related()
-            .annotate(year=ExtractYear(F('date')))
-            .values('year', 'from_account__title')
-            .annotate(expenses=Sum('price'))
-            .annotate_fee(fee=fee)
-            .values(*values, id=F('from_account__pk'))
+        return \
+            self \
+            .related() \
+            .annotate(year=ExtractYear(F('date'))) \
+            .values('year', 'from_account__title') \
+            .annotate(expenses=Sum('price')) \
+            .annotate_fee(fee=fee) \
+            .values(*values, id=F('from_account__pk')) \
             .order_by('year', 'id')
-        )
     base_expenses.queryset_only = True
 
 

commit 978d4bb97797ab2fee1a9a15b4871130ac547ece

fixe error introduced in 4846a1c5 in line 42

project/transactions/managers.py
@@ -40,7 +40,7 @@ class BaseMixin(models.QuerySet):
 
     def annotate_fee(self, fee):
         if fee:
-            return self.annotate(fee=F('fee'))
+            return self.annotate(fee=Sum('fee'))
         return self
     annotate_fee.queryset_only = True
 

project/debts/tests/test_models_debt_return.py
@@ -383,51 +383,6 @@ def test_borrow_return_post_delete_with_updt():
     assert actual.balance == 99.0
 
 
-@patch('project.core.lib.utils.get_request_kwargs', return_value='lend')
-def test_lend_return_sum_all_months(mck):
-    LendReturnFactory(date=dt(1999, 1, 1), price=1)
-    LendReturnFactory(date=dt(1999, 1, 2), price=2)
-    LendReturnFactory(date=dt(1999, 2, 1), price=4)
-    LendReturnFactory(date=dt(1999, 2, 2), price=1)
-
-    expect = [
-        {'date': dt(1999, 1, 1), 'sum': Decimal('3')},
-        {'date': dt(1999, 2, 1), 'sum': Decimal('5')},
-    ]
-
-    actual = list(DebtReturn.objects.sum_by_month(1999))
-
-    assert expect == actual
-
-
-@patch('project.core.lib.utils.get_request_kwargs', return_value='lend')
-def test_lend_return_sum_all_months_ordering(mck):
-    LendReturnFactory(date=dt(1999, 1, 1), price=1)
-    LendReturnFactory(date=dt(1999, 1, 2), price=2)
-    LendReturnFactory(date=dt(1999, 2, 1), price=4)
-    LendReturnFactory(date=dt(1999, 2, 2), price=1)
-
-    actual = list(DebtReturn.objects.sum_by_month(1999))
-
-    assert actual[0]['date'] == dt(1999, 1, 1)
-    assert actual[1]['date'] == dt(1999, 2, 1)
-
-
-@patch('project.core.lib.utils.get_request_kwargs', return_value='lend')
-def test_lend_return_sum_one_month(mck):
-    LendReturnFactory(date=dt(1999, 1, 1), price=1)
-    LendReturnFactory(date=dt(1999, 1, 2), price=2)
-
-    expect = [
-        {'date': dt(1999, 1, 1), 'sum': Decimal('3')}
-    ]
-
-    actual = list(DebtReturn.objects.sum_by_month(1999, 1))
-
-    assert len(expect) == 1
-    assert expect == actual
-
-
 def test_lend_return_autoclose():
     LendReturnFactory(price=100)
 