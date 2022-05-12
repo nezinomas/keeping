e67d472d888e8be562a82e387c8031a4960c0bd9
Audrius <nezinomas@gmail.com>
2022-05-12 16:37:02 +0300

    conflicts on merge

--- project/static/js/dialog.js
+++ project/static/js/dialog.js
@@ -1,9 +1,20 @@
+// focus on [autofocus] attribute
+$(document).on('shown.bs.modal', '#modal', function () {
+    $(this).find('[autofocus]').focus();
+});
+
+
 htmx.on("htmx:afterSwap", (e) => {
     // Response targeting #dialog => show the modal
     if (e.detail.target.id == "dialog") {
         $("#modal").modal("show").draggable({ handle: ".modal-header" });
     }
 })
+
+
+$(document).on('shown.bs.modal', '.modal', function () {
+    $(this).find('[autofocus]').focus();
+});
 
 
 htmx.on("htmx:beforeSwap", (e) => {
@@ -16,13 +27,59 @@ htmx.on("htmx:beforeSwap", (e) => {
         $('.is-invalid').removeClass('is-invalid');
 
         if(subbmiter == '_new') {
-            $('#modal form')[0].reset();
+            var price = $('#id_price');
+            if (price) {
+                price.val('');
+            }
+
+            var price_sum_field = $('.sum-prices-field');
+            if (price_sum_field) {
+                price_sum_field.val('0.0');
+            }
+
+            var qty = $('#id_quantity');
+            if (qty) {
+                qty.val('1');
+            }
+
+            var title = $('#id_title');
+            if (title) {
+                title.val('');
+            }
+
+            var remark = $('#id_remark');
+            if (remark) {
+                remark.val('');
+            }
+
+            var exception = $('#id_exception');
+            if (exception) {
+                exception.prop('checked', false);
+            }
         }
 
         if(subbmiter == '_close') {
             $('#modal').modal('hide');
             $('#modal form')[0].reset();
         }
+
         e.detail.shouldSwap = false;
     }
+
 })
+
+
+$(document).on('hidden.bs.modal', '#modal', function () {
+    var form = $('.form');
+    var trigger_name = form.attr("data-hx-trigger-form");
+    var data_inserted = form.attr('data-hx-inserted');
+
+    alert(`Trigger: ${trigger_name} type: ${typeof (trigger_name)} | insert: ${data_inserted} type: ${typeof (data_inserted)}`);
+
+    if (trigger_name === 'None' || trigger_name == undefined) return;
+
+    if (trigger_name && data_inserted) {
+        alert('Triggering event');
+        htmx.trigger("body", trigger_name, {});
+    }
+});
