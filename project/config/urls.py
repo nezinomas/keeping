6c24723ae1d528cc640981b4b259e09d84712837
Audrius <nezinomas@gmail.com>
2022-05-19 17:16:59 +0300

    refactoring  urls

--- project/config/urls.py
+++ project/config/urls.py
@@ -12,7 +12,7 @@ urlpatterns = [
     path('', include('project.debts.urls')),
     path('', include('project.drinks.urls')),
     path('', include('project.expenses.urls')),
-    path('', include('project.incomes.urls')),
+    path('incomes/', include('project.incomes.urls')),
     path('counts/', include('project.counts.urls')),
     path('', include('project.savings.urls')),
     path('', include('project.plans.urls')),
--- project/incomes/urls.py
+++ project/incomes/urls.py
@@ -6,47 +6,47 @@ app_name = 'incomes'
 
 urlpatterns = [
     path(
-        'incomes/',
+        '',
         views.Index.as_view(),
         name='index'
     ),
     path(
-        'incomes/lists/',
+        'lists/',
         views.Lists.as_view(),
         name='list'
     ),
     path(
-        'incomes/new/',
+        'new/',
         views.New.as_view(),
         name='new'
     ),
     path(
-        'incomes/update/<int:pk>/',
+        'update/<int:pk>/',
         views.Update.as_view(),
         name='update'
     ),
     path(
-        'incomes/delete/<int:pk>/',
+        'delete/<int:pk>/',
         views.Delete.as_view(),
         name='delete'
     ),
     path(
-        'incomes/type/',
+        'type/',
         views.TypeLists.as_view(),
         name='type_list'
     ),
     path(
-        'incomes/type/new/',
+        'type/new/',
         views.TypeNew.as_view(),
         name='type_new'
     ),
     path(
-        'incomes/type/update/<int:pk>/',
+        'type/update/<int:pk>/',
         views.TypeUpdate.as_view(),
         name='type_update'
     ),
     path(
-        'incomes/search/',
+        'search/',
         views.Search.as_view(),
         name='search'
     ),
