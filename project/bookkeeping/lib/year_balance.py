93790b8243c5ad91de403ccea09fb234cba51bb4
Audrius <nezinomas@gmail.com>
2022-06-18 15:25:21 +0300

    nsvrb

--- project/bookkeeping/lib/year_balance.py
+++ project/bookkeeping/lib/year_balance.py
@@ -142,7 +142,6 @@ class YearBalance(BalanceBase):
 
             # copy values from input arrays to df
             for row in data_arr:
-
                 df.at[to_datetime(row['date']), col_name] = float(row['sum'])
 
         return df
