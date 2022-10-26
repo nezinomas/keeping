40958e782c29ce83307cc2e1a348ddf4e3b80929
nezinomas <nezinomas@gmai.com>
2022-10-26 12:34:56 +0300

    rename variables

--- project/drinks/services/index.py
+++ project/drinks/services/index.py
@@ -60,33 +60,34 @@ class IndexService:
                  target: float = 0.0,
                  latest_past_date: date = None,
                  latest_current_date: date = None):
-        self._options = DrinksOptions()
 
-        self.drink_stats = drink_stats
-        self._year = drink_stats.year
-        self.target = target
-        self.per_day_of_year = drink_stats.per_day_of_year
-        self.quantity_of_year = drink_stats.qty_of_year
-        self.latest_past_date = latest_past_date
-        self.latest_current_date = latest_current_date
+        self._target = target
+        self._latest_past_date = latest_past_date
+        self._latest_current_date = latest_current_date
+
+        self._drink_stats = drink_stats
+        self._per_day_of_year = drink_stats.per_day_of_year
+        self._quantity_of_year = drink_stats.qty_of_year
+
+        self._options = DrinksOptions()
 
     def chart_quantity(self) -> List[Dict]:
         return {
             'categories' : list(month_names().values()),
-            'data' : self.drink_stats.qty_of_month,
+            'data' : self._drink_stats.qty_of_month,
             'text' : {'quantity': _('Quantity')}
         }
 
     def chart_consumption(self) -> str:
         return {
             'categories' : list(month_names().values()),
-            'data': self.drink_stats.per_day_of_month,
-            'target': self.target,
-            'avg': self.per_day_of_year,
+            'data': self._drink_stats.per_day_of_month,
+            'target': self._target,
+            'avg': self._per_day_of_year,
             'avg_label_y':
-                self._avg_label_position(self.per_day_of_year, self.target),
+                self._avg_label_position(self._per_day_of_year, self._target),
             'target_label_y':
-                self._target_label_position(self.per_day_of_year, self.target),
+                self._target_label_position(self._per_day_of_year, self._target),
             'text': {
                 'limit': _('Limit'),
                 'alcohol': _('Alcohol consumption per day, ml'),
@@ -96,7 +97,7 @@ class IndexService:
     def tbl_dry_days(self) -> Dict:
         _dict = {}
 
-        if latest := self.latest_current_date or self.latest_past_date:
+        if latest := self._latest_current_date or self._latest_past_date:
             delta = (datetime.now().date() - latest).days
             _dict = {'date': latest, 'delta': delta}
 
@@ -104,13 +105,13 @@ class IndexService:
 
     def tbl_consumption(self) -> str:
         return {
-            'qty': self.quantity_of_year,
-            'avg': self.per_day_of_year,
-            'target': self.target,
+            'qty': self._quantity_of_year,
+            'avg': self._per_day_of_year,
+            'target': self._target,
         }
 
     def tbl_alcohol(self) -> str:
-        stdav = self.quantity_of_year / self._options.ratio
+        stdav = self._quantity_of_year / self._options.ratio
 
         return {
             'liters': self._options.stdav_to_alcohol(stdav)
@@ -118,7 +119,7 @@ class IndexService:
 
     def tbl_std_av(self) -> str:
         return {
-            'items': self._std_av(self._year, self.quantity_of_year)
+            'items': self._std_av(self._drink_stats.year, self._quantity_of_year)
         }
 
     def _avg_label_position(self, avg: float, target: float) -> int:
