bc6fb0cc1b2828c1b5045b48da5a1acdf9fa44e7
nezinomas <nezinomas@gmai.com>
2022-10-28 09:27:08 +0300

    new test

--- project/bookkeeping/tests/test_services_index_data.py
+++ project/bookkeeping/tests/test_services_index_data.py
@@ -1,4 +1,3 @@
-from mock import patch
 import pytest
 from ..services.index import IndexServiceData as I
 
@@ -20,10 +19,19 @@ def data_dummy(monkeypatch):
 
 
 @pytest.fixture()
-def data(monkeypatch):
-    monkeypatch.setattr(
-        'project.bookkeeping.services.index.Debt.objects.sum_by_month',
-        lambda x, debt_type: [{'date': 'xxxx', 'sum_debt': 2, 'sum_return': 1}])
+def data(mocker):
+    debts = [
+        [{'date': 'xxxx', 'sum_debt': 2, 'sum_return': 1}],  # borrow
+        [{'date': 'xxxx', 'sum_debt': 7, 'sum_return': 5}],  # lend
+    ]
+
+    module = 'project.bookkeeping.services.index'
+    mocker.patch(f'{module}.Debt.objects.sum_by_month', side_effect=debts)
+    mocker.patch(f'{module}.Income.objects.sum_by_month', side_effect=['Income sum_by_month data'])
+    mocker.patch(f'{module}.Expense.objects.sum_by_month', side_effect=['Expense sum_by_month data'])
+    mocker.patch(f'{module}.Saving.objects.sum_by_month', side_effect=['Saving sum_by_month data'])
+    mocker.patch(f'{module}.SavingClose.objects.sum_by_month', side_effect=['SavingClose sum_by_month data'])
+
 
 
 def test_init(amount_start, data_dummy):
@@ -42,3 +50,18 @@ def test_get_debt_data():
 
     assert debt == [{'date': 'xxxx-yy-zz', 'sum': 999}]
     assert debt_return == [{'date': 'xxxx-yy-zz', 'sum': 111}]
+
+
+def test_data(amount_start, data):
+    I.collect_data(1999)
+
+    assert I.data['incomes'] == 'Income sum_by_month data'
+    assert I.data['expenses'] == 'Expense sum_by_month data'
+    assert I.data['savings'] == 'Saving sum_by_month data'
+    assert I.data['savings_close'] == 'SavingClose sum_by_month data'
+
+    assert I.data['borrow'] == [{'date': 'xxxx', 'sum': 2}]
+    assert I.data['borrow_return'] == [{'date': 'xxxx', 'sum': 1}]
+
+    assert I.data['lend'] == [{'date': 'xxxx', 'sum': 7}]
+    assert I.data['lend_return'] == [{'date': 'xxxx', 'sum': 5}]
