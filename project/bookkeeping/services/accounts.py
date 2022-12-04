7331ff992f0ec7cf94f24a666bcf6cd0e9c682e6
Audrius <nezinomas@gmail.com>
2022-12-04 21:48:36 +0200

    start

--- project/bookkeeping/services/accounts.py
+++ project/bookkeeping/services/accounts.py
@@ -4,7 +4,7 @@ from ...accounts.models import AccountBalance
 from ...core.lib import utils
 from ..models import AccountWorth
 from . import common
-
+import pandas as pd
 
 @dataclass
 class AccountServiceData:
@@ -36,3 +36,48 @@ class AccountService:
 
         return \
             utils.sum_all(self.data) if self.data else total_row
+
+class AccountsServiceNew:
+    _df = pd.DataFrame()
+    _df_have = pd.DataFrame()
+
+    def __init__(self, year, incomes, expenses, have):
+        self._year = year
+        self._have = have
+        self._df = self._make_df(incomes, expenses)
+        self._df_have = self._make_df_have(have)
+
+    def _make_df(self, incomes, expenses):
+        exp = pd.DataFrame(expenses).set_index(['id', 'year']).sort_index(level=['id', 'year'])
+        exp['expenses'] = exp['expenses'].astype(float)
+
+        inc = pd.DataFrame(incomes).set_index(['id', 'year']).sort_index(level=['id', 'year'])
+        inc['incomes'] = inc['incomes'].astype(float)
+
+        df = inc.add(exp, fill_value=0.0).fillna(0.0)
+        df['balance'] = df['incomes'] - df['expenses']
+
+        return df
+
+    def _make_df_have(self, have):
+        df = pd.DataFrame(have)
+        df = df.loc[df['year'] == self._year, ['id', 'have']].set_index('id')
+        df['have'] = df['have'].astype(float)
+        return df
+
+    def table(self):
+        df = self._df.copy().reset_index()
+        print('start\n', df)
+        past = df.loc[df['year'] < self._year].groupby(['id']).sum()
+        print('past\n', past)
+        now = df.loc[df['year'] == self._year, ['id', 'incomes', 'expenses']].set_index('id')
+
+        now['past'] = past['balance']
+        now['balance'] = now['past'] + now['incomes'] - now['expenses']
+
+        # add have
+        print(f'-----have\n{self._df_have}\n')
+        now['have'] = self._df_have['have']
+        now['delta'] = now['have'] - now['balance']
+        print(now)
+        return now.reset_index().to_dict('records')
---
+++ project/bookkeeping/tests/test_services_account.py
@@ -0,0 +1,43 @@
+from decimal import Decimal
+
+import pytest
+
+from ..services.accounts import AccountsServiceNew
+
+
+@pytest.fixture(name="incomes")
+def fixture_incomes():
+    return [
+        {'year': 1999, 'incomes': Decimal('100'), 'id': 'X'},
+        {'year': 2000, 'incomes': Decimal('200'), 'id': 'X'},
+        {'year': 1999, 'incomes': Decimal('110'), 'id': 'Y'},
+        {'year': 2000, 'incomes': Decimal('220'), 'id': 'Y'},
+    ]
+
+@pytest.fixture(name="expenses")
+def fixture_expenses():
+    return [
+        {'year': 1999, 'expenses': Decimal('50'), 'id': 'X'},
+        {'year': 2000, 'expenses': Decimal('100'), 'id': 'X'},
+        {'year': 2000, 'expenses': Decimal('110'), 'id': 'Y'},
+    ]
+
+
+@pytest.fixture(name="have")
+def fixture_have():
+    return [
+        {'year': 1999, 'have': Decimal('1'), 'id': 'X'},
+        {'year': 1999, 'have': Decimal('2'), 'id': 'Y'},
+        {'year': 2000, 'have': Decimal('10'), 'id': 'X'},
+        {'year': 2000, 'have': Decimal('20'), 'id': 'Y'},
+    ]
+
+def test_table(incomes, expenses, have):
+    actual = AccountsServiceNew(2000, incomes, expenses, have).table()
+
+    expect = [
+        {'id': 'X', 'past': 50.0, 'incomes': 200.0, 'expenses': 100.0, 'balance': 150.0, 'have': 10.0, 'delta': -140.0},
+        {'id': 'Y', 'past': 110.0, 'incomes': 220.0, 'expenses': 110.0, 'balance': 220.0, 'have': 20.0, 'delta': -200.0},
+    ]
+
+    assert actual == expect
