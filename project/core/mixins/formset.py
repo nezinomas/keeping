9c23d56d422ac856b3612b8dce15be9b508bd1e0
nezinomas <nezinomas@gmai.com>
2022-12-20 15:54:07 +0200

    small refactorings

--- project/core/mixins/formset.py
+++ project/core/mixins/formset.py
@@ -1,7 +1,6 @@
 
 from decimal import Decimal
 
-from django.forms import ValidationError
 from django.forms.formsets import BaseFormSet
 from django.forms.models import modelformset_factory
 from django.utils.translation import gettext as _
@@ -12,6 +11,7 @@ from ...core.mixins.views import httpHtmxResponse
 class BaseTypeFormSet(BaseFormSet):
     def clean(self):
         if any(self.errors):
+            # if forms have errors, don't run formset clean
             return
 
         dublicates = {}
@@ -56,48 +56,37 @@ class FormsetMixin():
     def get_type_model(self):
         return self.type_model or self.model
 
-    def get_formset(self, post=None):
+    def get_formset(self, post=None, **kwargs):
         form = self.get_form_class()
-
-        __formset = (
+        formset = \
             modelformset_factory(
                 model=self.model,
                 form=form,
                 formset=BaseTypeFormSet,
                 extra=0,
             )
-        )
-
         return \
-            __formset(post) if post else __formset(initial=self.formset_initial())
-
-    def get_shared_form(self, post=None):
-        return self.shared_form_class(post) if self.shared_form_class else None
+            formset(post) if post else formset(initial=self.formset_initial())
 
     def post(self, request, *args, **kwargs):
         formset = self.get_formset(request.POST or None)
-        shared_form = self.get_shared_form(request.POST or None)
 
-        if formset.is_valid() and (shared_form and shared_form.is_valid()):
-            date = shared_form.cleaned_data.get('date')
-
+        if formset.is_valid():
             for form in formset:
                 price = form.cleaned_data.get('price')
 
                 if not isinstance(price, Decimal):
                     continue
 
-                # if from has price (price can be 0) save that form
-                form.instance.date = date
                 form.save()
 
             return httpHtmxResponse(self.get_hx_trigger_django())
 
         return super().form_invalid(formset)
 
+
     def get_context_data(self, **kwargs):
         context = {
             'formset': self.get_formset(self.request.POST or None),
-            'shared_form': self.get_shared_form(self.request.POST or None)
         }
         return super().get_context_data(**kwargs) | context
