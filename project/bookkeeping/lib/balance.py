e5334d24bbc086cfc372010f46de4d1574059302
nezinomas <nezinomas@gmai.com>
2022-12-21 16:24:08 +0200

    new balance, start

---
+++ project/bookkeeping/lib/balance.py
@@ -0,0 +1,29 @@
+from datetime import date
+
+import janitor
+import pandas as pd
+from pandas import DataFrame as DF
+
+
+def create_data(data: list[dict], types: list, sum_column: str = 'sum', option: str = 'months') -> DF:
+    year = data[0]['date'].year
+    df = DF(data).remove_columns(['exception_sum']).to_datetime('date')
+    df[sum_column] = df[sum_column].apply(pd.to_numeric, downcast='float')
+    # groupby month or day and sum
+    grp = df.date.dt.month if option == 'months' else df.date.dt.day
+    df = df.groupby(['title', grp])['sum'].sum()
+    # modifie df: unstack, transpose, row to col names
+    df = df.unstack().reset_index().T.reset_index()
+    df.columns = df.iloc[0] # first row values -> to columns names
+    df = df.drop(0)  # remove first row
+    df = df.rename(columns={'title': 'date'})  # rename column
+    # convert column data
+    df['date'] = pd.to_datetime(df['date'].apply(lambda x: date(year, x, 1)))
+    # create missing rows
+    new_dates = {'date': lambda d: pd.date_range(d.min(), d.max(), freq='MS')}
+    df = df.complete(new_dates, fill_value=0.0).set_index('date')
+    # create missing columns
+    df[[*set(types) - set(df.columns)]] = 0.0
+    df = df[sorted(df.columns)]
+    print(f'\nF >>>>>>\n{df}\n')
+    return df
---
+++ project/bookkeeping/tests/test_lib_balance.py
@@ -0,0 +1,62 @@
+from datetime import date
+from decimal import Decimal
+
+import pandas as pd
+import pytest
+from ..lib import balance
+
+
+@pytest.fixture(name='data')
+def fixture_data():
+    return ([
+        {
+            'date': date(1999, 1, 1),
+            'title': 'T1',
+            'sum': Decimal('1'),
+            'exception_sum': Decimal('0.5')
+        }, {
+            'date': date(1999, 1, 1),
+            'title': 'T2',
+            'sum': Decimal('2'),
+            'exception_sum': Decimal('0')
+        }, {
+            'date': date(1999, 1, 31),
+            'title': 'T1',
+            'sum': Decimal('3'),
+            'exception_sum': Decimal('0')
+        }, {
+            'date': date(1999, 12, 1),
+            'title': 'T1',
+            'sum': Decimal('4'),
+            'exception_sum': Decimal('0')
+        }, {
+            'date': date(1999, 12, 1),
+            'title': 'T2',
+            'sum': Decimal('5'),
+            'exception_sum': Decimal('0')
+        },
+    ])
+
+
+@pytest.fixture(name='types')
+def fixture_types():
+    return ['T1', 'T2', 'T0']
+
+
+def test_months_data(data, types):
+    actual = balance.create_data(data, types)
+
+    assert isinstance(actual, pd.DataFrame)
+
+    assert actual.loc['1999-01-01', 'T0'] == 0.0
+    assert actual.loc['1999-01-01', 'T1'] == 4.0
+    assert actual.loc['1999-01-01', 'T2'] == 2.0
+
+    assert actual.loc['1999-02-01', 'T0'] == 0.0
+    assert actual.loc['1999-02-01', 'T1'] == 0.0
+    assert actual.loc['1999-02-01', 'T2'] == 0.0
+
+    assert actual.loc['1999-12-01', 'T0'] == 0.0
+    assert actual.loc['1999-12-01', 'T1'] == 4.0
+    assert actual.loc['1999-12-01', 'T2'] == 5.0
+    assert 0
