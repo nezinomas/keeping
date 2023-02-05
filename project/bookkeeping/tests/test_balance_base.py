b5b2f62e9ae0898f5a429916fd6ac41d104cc070
Audrius <nezinomas@gmail.com>
2023-02-05 17:55:01 +0200

    formated with black

--- project/bookkeeping/tests/test_balance_base.py
+++ project/bookkeeping/tests/test_balance_base.py
@@ -9,21 +9,21 @@ from ..lib.balance_base import BalanceBase
 @pytest.fixture(name="df")
 def fixture_df():
     arr = [
-        {'date': date(1999, 1, 1), 'x': 1.1, 'y': 2.1},
-        {'date': date(1999, 1, 2), 'x': 1.1, 'y': 2.1},
-        {'date': date(1999, 1, 3), 'x': 0.0, 'y': 2.1},
-        {'date': date(1999, 1, 4), 'x': 1.1, 'y': 0.0},
+        {"date": date(1999, 1, 1), "x": 1.1, "y": 2.1},
+        {"date": date(1999, 1, 2), "x": 1.1, "y": 2.1},
+        {"date": date(1999, 1, 3), "x": 0.0, "y": 2.1},
+        {"date": date(1999, 1, 4), "x": 1.1, "y": 0.0},
     ]
     return pl.DataFrame(arr)
 
 
 @pytest.mark.parametrize(
-    'df, expected',
+    "df, expected",
     [
         (None, []),
         (pl.DataFrame(), []),
-        (pl.DataFrame({'t': [1.1], 'date': ['x']}), [{'t': 1.1, 'date': 'x'}]),
-    ]
+        (pl.DataFrame({"t": [1.1], "date": ["x"]}), [{"t": 1.1, "date": "x"}]),
+    ],
 )
 def test_balance(df, expected):
     actual = BalanceBase(df).balance
@@ -32,12 +32,12 @@ def test_balance(df, expected):
 
 
 @pytest.mark.parametrize(
-    'data, expected',
+    "data, expected",
     [
         (None, []),
         (pl.DataFrame(), []),
-        (pl.DataFrame({'t': [1.1], 'date': ['x']}), [{'t': 1.1, 'date': 'x'}]),
-    ]
+        (pl.DataFrame({"t": [1.1], "date": ["x"]}), [{"t": 1.1, "date": "x"}]),
+    ],
 )
 def test_balance_then_before_was_called_total_row(data, expected):
     obj = BalanceBase(data)
@@ -48,15 +48,15 @@ def test_balance_then_before_was_called_total_row(data, expected):
 
 
 @pytest.mark.parametrize(
-    'data, expected',
+    "data, expected",
     [
         (None, {}),
-        (pytest.lazy_fixture('df'), {'x': 1.1, 'y': 2.1}),
-        (pl.DataFrame({'x': [1.1, 2.2]}), {'x': 1.65}),
-        (pl.DataFrame({'x': [0.0, 1.1, 0, 2.2]}), {'x': 1.65}),
-        (pl.DataFrame({'x': pl.Series([0.0, None], dtype=pl.Float32)}), {'x': 0.0}),
-        (pl.DataFrame({'t1': [1.1, 2.2], 't2': [0, 0]}), {'t1': 1.65, 't2': 0.0}),
-    ]
+        (pytest.lazy_fixture("df"), {"x": 1.1, "y": 2.1}),
+        (pl.DataFrame({"x": [1.1, 2.2]}), {"x": 1.65}),
+        (pl.DataFrame({"x": [0.0, 1.1, 0, 2.2]}), {"x": 1.65}),
+        (pl.DataFrame({"x": pl.Series([0.0, None], dtype=pl.Float32)}), {"x": 0.0}),
+        (pl.DataFrame({"t1": [1.1, 2.2], "t2": [0, 0]}), {"t1": 1.65, "t2": 0.0}),
+    ],
 )
 def test_average(data, expected):
     actual = BalanceBase(data).average
@@ -65,12 +65,12 @@ def test_average(data, expected):
 
 
 @pytest.mark.parametrize(
-    'data, expected',
+    "data, expected",
     [
-        (pytest.lazy_fixture('df'), {'x': 3.3, 'y': 6.3}),
+        (pytest.lazy_fixture("df"), {"x": 3.3, "y": 6.3}),
         (pl.DataFrame(), {}),
         (None, {}),
-    ]
+    ],
 )
 def test_total_row(data, expected):
     actual = BalanceBase(data).total_row
@@ -82,10 +82,10 @@ def test_total_column(df):
     actual = BalanceBase(df).total_column
 
     expect = [
-        {'date': date(1999, 1, 1), 'total': 3.2},
-        {'date': date(1999, 1, 2), 'total': 3.2},
-        {'date': date(1999, 1, 3), 'total': 2.1},
-        {'date': date(1999, 1, 4), 'total': 1.1},
+        {"date": date(1999, 1, 1), "total": 3.2},
+        {"date": date(1999, 1, 2), "total": 3.2},
+        {"date": date(1999, 1, 3), "total": 2.1},
+        {"date": date(1999, 1, 4), "total": 1.1},
     ]
 
     assert actual == expect
@@ -100,4 +100,4 @@ def test_total(df):
 def test_types(df):
     actual = BalanceBase(df).types
 
-    assert actual == ['x', 'y']
+    assert actual == ["x", "y"]
