cd59e3d6b5a3b53b546314e734bb263ddeaed36d
Audrius <nezinomas@gmail.com>
2023-02-05 12:58:50 +0200

    formated with black

--- project/bookkeeping/lib/balance_base.py
+++ project/bookkeeping/lib/balance_base.py
@@ -2,15 +2,15 @@ import polars as pl
 from polars import DataFrame as DF
 
 
-class BalanceBase():
+class BalanceBase:
     def __init__(self, data: DF = DF()):
         self._data = data
 
     @property
     def balance(self) -> list[dict]:
-        '''
+        """
         Return [{'date': datetime.datetime, 'title': float}]
-        '''
+        """
         if not isinstance(self._data, DF):
             return []
 
@@ -18,24 +18,24 @@ class BalanceBase():
 
     @property
     def types(self) -> list:
-        return sorted(self._data.select(pl.exclude('date')).columns)
+        return sorted(self._data.select(pl.exclude("date")).columns)
 
     @property
     def total(self) -> float:
-        '''
+        """
         Return total sum of all columns
-        '''
+        """
         if not isinstance(self._data, DF) or self._data.is_empty():
             return 0.0
 
         return self._data.select(pl.sum(pl.exclude("date")).sum())[0, 0]
 
-    def make_total_column(self, df = DF()) -> DF:
-        '''
+    def make_total_column(self, df=DF()) -> DF:
+        """
         calculate total column for balance DataFrame
 
         return filtered DataFrame with date and total column
-        '''
+        """
 
         if not isinstance(self._data, DF):
             return DF()
@@ -43,7 +43,9 @@ class BalanceBase():
         if self._data.is_empty():
             return DF()
 
-        return self._data.select([pl.col('date'), pl.sum(pl.exclude('date')).alias('total')])
+        return self._data.select(
+            [pl.col("date"), pl.sum(pl.exclude("date")).alias("total")]
+        )
 
     @property
     def total_column(self) -> dict[str, float]:
@@ -57,13 +59,13 @@ class BalanceBase():
         if self._data.is_empty():
             return {}
 
-        df = self._data.select(pl.exclude('date')).sum().head(1)
+        df = self._data.select(pl.exclude("date")).sum().head(1)
 
         return df.to_dicts()[0]
 
     @property
     def average(self) -> dict[str, float]:
-        """ Calculate mean of every column, null values ignored
+        """Calculate mean of every column, null values ignored
 
         Returns:
             dict[str, float]
@@ -74,7 +76,7 @@ class BalanceBase():
         if self._data.is_empty():
             return {}
 
-        cols = self._data.select(pl.exclude('date')).columns
+        cols = self._data.select(pl.exclude("date")).columns
 
         def col_sum(col_name) -> pl.Expr:
             return pl.col(col_name).sum()
@@ -83,14 +85,15 @@ class BalanceBase():
             return pl.col(col_name).filter(pl.col(col_name) != 0).count()
 
         df = (
-            self._data
-            .select(cols)
+            self._data.select(cols)
             .fill_null(0)
-            .with_columns([
-                pl.when(col_sum(col_name) != 0)
-                .then(col_sum(col_name) / count_not_nulls(col_name))
-                .otherwise(pl.lit(0))
-                for col_name in cols
-            ])
+            .with_columns(
+                [
+                    pl.when(col_sum(col_name) != 0)
+                    .then(col_sum(col_name) / count_not_nulls(col_name))
+                    .otherwise(pl.lit(0))
+                    for col_name in cols
+                ]
+            )
         )
         return df.to_dicts()[0]
