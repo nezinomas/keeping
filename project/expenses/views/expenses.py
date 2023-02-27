42b06cced352b453bde7bddbce4ee67767082f0a
Audrius <nezinomas@gmail.com>
2023-02-27 19:59:17 +0200

    formated with black

--- project/expenses/views/expenses.py
+++ project/expenses/views/expenses.py
@@ -5,17 +5,22 @@ from django.urls import reverse_lazy
 from django.utils.translation import gettext as _
 
 from ...core.lib.convert_price import ConvertToCents
-from ...core.mixins.views import (CreateViewMixin, DeleteViewMixin,
-                                  ListViewMixin, SearchViewMixin,
-                                  TemplateViewMixin, UpdateViewMixin,
-                                  rendered_content)
+from ...core.mixins.views import (
+    CreateViewMixin,
+    DeleteViewMixin,
+    ListViewMixin,
+    SearchViewMixin,
+    TemplateViewMixin,
+    UpdateViewMixin,
+    rendered_content,
+)
 from .. import forms, models
 from . import expenses_type
 
 
-class GetMonthMixin():
+class GetMonthMixin:
     def get_month(self):
-        month = self.request.GET.get('month')
+        month = self.request.GET.get("month")
         now = datetime.now().month
 
         try:
@@ -27,12 +32,12 @@ class GetMonthMixin():
 
 
 class Index(GetMonthMixin, TemplateViewMixin):
-    template_name = 'expenses/index.html'
+    template_name = "expenses/index.html"
 
     def get_context_data(self, **kwargs):
         context = {
-            'types': rendered_content(self.request, expenses_type.Lists),
-            'expenses': rendered_content(self.request, Lists),
+            "types": rendered_content(self.request, expenses_type.Lists),
+            "expenses": rendered_content(self.request, Lists),
         }
         return super().get_context_data(**kwargs) | context
 
@@ -47,56 +52,58 @@ class Lists(GetMonthMixin, ListViewMixin):
         if month in range(1, 13):
             qs = qs.filter(date__month=month)
 
-        qs = qs.order_by('-date', 'expense_type', F('expense_name').asc())
+        qs = qs.order_by("-date", "expense_type", F("expense_name").asc())
 
         return qs
 
     def get_context_data(self, **kwargs):
         month = self.get_month()
 
-        notice = _('There are no records for month <b>%(month)s</b>.') % {'month': month}
+        notice = _("There are no records for month <b>%(month)s</b>.") % {
+            "month": month
+        }
         if month == 13 or not month:
-            notice = \
-                _('No records in <b>%(year)s</b>.') \
-                % {'year': self.request.user.year}
+            notice = _("No records in <b>%(year)s</b>.") % {
+                "year": self.request.user.year
+            }
 
-        context = {'notice': notice}
+        context = {"notice": notice}
         return super().get_context_data(**kwargs) | context
 
 
 class New(CreateViewMixin):
     model = models.Expense
     form_class = forms.ExpenseForm
-    success_url = reverse_lazy('expenses:list')
-    hx_trigger_form = 'reload'
+    success_url = reverse_lazy("expenses:list")
+    hx_trigger_form = "reload"
 
 
 class Update(ConvertToCents, UpdateViewMixin):
     model = models.Expense
     form_class = forms.ExpenseForm
-    success_url = reverse_lazy('expenses:list')
-    hx_trigger_django = 'reload'
+    success_url = reverse_lazy("expenses:list")
+    hx_trigger_django = "reload"
 
 
 class Delete(DeleteViewMixin):
     model = models.Expense
-    success_url = reverse_lazy('expenses:list')
-    hx_trigger_django = 'reload'
+    success_url = reverse_lazy("expenses:list")
+    hx_trigger_django = "reload"
 
 
 class Search(SearchViewMixin):
-    template_name = 'expenses/expense_list.html'
+    template_name = "expenses/expense_list.html"
     per_page = 50
 
-    search_method = 'search_expenses'
+    search_method = "search_expenses"
 
 
 class LoadExpenseName(ListViewMixin):
-    template_name = 'core/dropdown.html'
+    template_name = "core/dropdown.html"
     object_list = []
 
     def get(self, request, *args, **kwargs):
-        expense_type_pk = request.GET.get('expense_type')
+        expense_type_pk = request.GET.get("expense_type")
 
         try:
             expense_type_pk = int(expense_type_pk)
@@ -104,11 +111,9 @@ class LoadExpenseName(ListViewMixin):
             expense_type_pk = None
 
         if expense_type_pk:
-            self.object_list = \
-                models.ExpenseName \
-                .objects \
-                .related() \
-                .filter(parent=expense_type_pk) \
-                .year(request.user.year) \
-
-        return self.render_to_response({'object_list': self.object_list})
+            self.object_list = (
+                models.ExpenseName.objects.related()
+                .filter(parent=expense_type_pk)
+                .year(request.user.year)
+            )
+        return self.render_to_response({"object_list": self.object_list})
