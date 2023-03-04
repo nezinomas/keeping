7ae2f7c0bd4e40ca9de4916c4cf6004f79c2b738
Audrius <nezinomas@gmail.com>
2023-03-04 17:07:30 +0200

    formated with black

--- project/incomes/tests/test_integration.py
+++ project/incomes/tests/test_integration.py
@@ -7,30 +7,31 @@ from selenium.webdriver.common.keys import Keys
 
 from ..factories import IncomeFactory
 from ...core.tests.utils import Browser
+
 pytestmark = pytest.mark.django_db
 
 
 @pytest.mark.webtest
 class Incomes(Browser):
-    @time_machine.travel('1999-1-1')
+    @time_machine.travel("1999-1-1")
     def test_search(self):
-        IncomeFactory(remark='xxxx')
-        IncomeFactory(remark='yyyy')
-        IncomeFactory(remark='zzzz')
+        IncomeFactory(remark="xxxx")
+        IncomeFactory(remark="yyyy")
+        IncomeFactory(remark="zzzz")
 
-        self.browser.get(f'{self.live_server_url}/incomes/')
+        self.browser.get(f"{self.live_server_url}/incomes/")
 
         rows = self.browser.find_elements(by=By.XPATH, value="//table/tbody/tr")
         assert len(rows) == 3
 
-        search = self.browser.find_element(by=By.ID, value='id_search')
-        search.send_keys('xxxx')
+        search = self.browser.find_element(by=By.ID, value="id_search")
+        search.send_keys("xxxx")
         search.send_keys(Keys.RETURN)
 
         sleep(0.1)
 
         rows = self.browser.find_elements(by=By.XPATH, value="//table/tbody/tr")
-        assert len(rows) == 1 # head row + find row
+        assert len(rows) == 1  # head row + find row
 
         cells = self.browser.find_elements(by=By.XPATH, value="//table/tbody/tr[1]/td")
-        assert cells[3].text == 'xxxx'
+        assert cells[3].text == "xxxx"
