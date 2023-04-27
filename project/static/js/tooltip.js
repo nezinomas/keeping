369d0fe6037cfdda9dc85c5bb7bbfad0c3ea0f59
nezinomas <nezinomas@gmai.com>
2023-04-27 16:09:28 +0300

    load tooltips in template

--- project/static/js/tooltip.js
+++ project/static/js/tooltip.js
@@ -4,22 +4,8 @@ function setUpToolTip() {
     const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl));
 }
 
-/*
-    wait 3000ms for ajax calls to finish
-    only then load setup tooltip script
-*/
-$.holdReady(true);
-setTimeout(function () {
-    $.holdReady(false);
-}, 3000);
-
-$(document).ready(function () {
-    setUpToolTip();
-});
-
 
 /* initializate the tooltips after model hide */
 $(document).on('hidden.bs.modal', '#modal', function () {
-    alert('o cia')
     setUpToolTip();
 })
