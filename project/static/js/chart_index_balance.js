f5b32fa6c8e59f5fbb6b258e9bb2fff8d3db3aab
nezinomas <nezinomas@gmai.com>
2023-04-28 13:03:55 +0300

    const instead var

--- project/static/js/chart_index_balance.js
+++ project/static/js/chart_index_balance.js
@@ -1,7 +1,5 @@
 $(function () {
-    var chartData = JSON.parse(
-        document.getElementById('chart-balance-data').textContent
-    );
+    const chartData = JSON.parse(document.getElementById('chart-balance-data').textContent);
     // convert data
     for(i = 0; i < 12; i++) {
         chartData.incomes[i] /= 100
