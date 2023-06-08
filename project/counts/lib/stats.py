493383bc71fcdae6773cdf4e0b59521d862f13f2
nezinomas <nezinomas@gmai.com>
2023-06-08 16:39:21 +0300

    number_of_records calculated as sum of qty, not dataframe rows quantity

--- project/counts/lib/stats.py
+++ project/counts/lib/stats.py
@@ -33,7 +33,7 @@ class Stats:
 
     @property
     def number_of_records(self):
-        return self._df.shape[0]
+        return self._df.select(pl.col("qty").sum())[0,0]
 
     def weekdays_stats(self) -> list[dict[int, float]]:
         """Returns [{'weekday': int, 'count': float}]"""
