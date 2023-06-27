32080357acdff98d9b6f1748da4868dad426e5f5
nezinomas <nezinomas@gmai.com>
2023-06-27 12:50:38 +0300

    BASE_DIR in wsgi

--- project/config/wsgi.py
+++ project/config/wsgi.py
@@ -6,7 +6,7 @@ from django.core.wsgi import get_wsgi_application
 
 
 # Set the project base directory
-BASE_DIR = Path().cwd()
+BASE_DIR = Path(__file__).absolute().parent.parent.parent
 
 # Take environment variables from .conf file
 with open(BASE_DIR / ".conf", "rb") as f:
