0c22aebe50881d4dc14f7a7af1fecc92ad10d12a
nezinomas <nezinomas@gmai.com>
2023-08-22 17:32:41 +0300

    formated with black

--- project/bookkeeping/services/summary_savings.py
+++ project/bookkeeping/services/summary_savings.py
@@ -17,14 +17,12 @@ class SummarySavingsServiceData:
     pensions3: list = field(init=False, default_factory=list)
 
     def __post_init__(self):
-        self.funds = \
-            list(SavingBalance.objects.sum_by_type().filter(type='funds'))
-        self.shares = \
-            list(SavingBalance.objects.sum_by_type().filter(type='shares'))
-        self.pensions2 = \
-            list(PensionBalance.objects.sum_by_year())
-        self.pensions3 = \
-            list(SavingBalance.objects.sum_by_type().filter(type='pensions'))
+        self.funds = list(SavingBalance.objects.sum_by_type().filter(type="funds"))
+        self.shares = list(SavingBalance.objects.sum_by_type().filter(type="shares"))
+        self.pensions2 = list(PensionBalance.objects.sum_by_year())
+        self.pensions3 = list(
+            SavingBalance.objects.sum_by_type().filter(type="pensions")
+        )
 
 
 class SummarySavingsService:
@@ -36,18 +34,19 @@ class SummarySavingsService:
 
     @property
     def records(self):
-        return \
-            0 \
-            + len(self.funds) \
-            + len(self.shares) \
-            + len(self.pensions2) \
-            + len(self.pensions3) \
+        return (
+            0
+            + len(self.funds)
+            + len(self.shares)
+            + len(self.pensions2)
+            + len(self.pensions3)
+        )
 
     def make_chart_data(self, *attr_names: str) -> list[dict]:
-        '''
+        """
         attr_names
         available: funds, shares, pensions2, pensions3
-        '''
+        """
         data = []
 
         for attr in attr_names:
@@ -59,23 +58,23 @@ class SummarySavingsService:
     @staticmethod
     def chart_data(*args):
         items = {
-            'text_total': _('Total'),
-            'text_profit': _('Profit'),
-            'text_invested': _('Invested'),
-            'categories': [],
-            'invested': [],
-            'profit': [],
-            'total': [],
-            'max': 0,
+            "text_total": _("Total"),
+            "text_profit": _("Profit"),
+            "text_invested": _("Invested"),
+            "categories": [],
+            "invested": [],
+            "profit": [],
+            "total": [],
+            "max": 0,
         }
 
         # from tuple[list[dict]] make list[dict]
         data = itertools.chain.from_iterable(args)
 
         for row in data:
-            _year = row.get('year')
-            _invested = row.get('invested')
-            _profit = row.get('profit')
+            _year = row.get("year")
+            _invested = row.get("invested")
+            _profit = row.get("profit")
             _total_sum = _invested + _profit
 
             if _year > datetime.now().year:
@@ -84,19 +83,19 @@ class SummarySavingsService:
             if not _invested and not _profit:
                 continue
 
-            if _year not in items['categories']:
-                items['categories'].append(_year)
-                items['invested'].append(_invested)
-                items['profit'].append(_profit)
-                items['total'].append(_total_sum)
+            if _year not in items["categories"]:
+                items["categories"].append(_year)
+                items["invested"].append(_invested)
+                items["profit"].append(_profit)
+                items["total"].append(_total_sum)
             else:
-                ix = items['categories'].index(_year)  # category index
-                items['invested'][ix] += _invested
-                items['profit'][ix] += _profit
-                items['total'][ix] += _total_sum
+                ix = items["categories"].index(_year)  # category index
+                items["invested"][ix] += _invested
+                items["profit"][ix] += _profit
+                items["total"][ix] += _total_sum
 
         # max value
         with contextlib.suppress(ValueError):
-            items['max'] = (max(items.get('profit')) + max(items.get('invested')))
+            items["max"] = max(items.get("profit")) + max(items.get("invested"))
 
         return items
