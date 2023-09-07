47a682f80c84d90a048fe29cae76c9aa0aab77a8
nezinomas <nezinomas@gmai.com>
2023-09-07 13:19:36 +0300

    change test_sub with hypothesis

--- project/core/tests/test_math.py
+++ project/core/tests/test_math.py
@@ -1,5 +1,14 @@
 import pytest
+from hypothesis import given, assume
+from hypothesis import strategies as st
+
 from ..templatetags import math
+
+
+@st.composite
+def numbers_strategy(draw):
+    return draw(st.none() | st.integers() | st.floats(allow_nan=False, allow_infinity=False))
+
 
 @pytest.mark.parametrize(
     'value, expect',
@@ -15,20 +24,14 @@ def test_price(value, expect):
     assert actual == expect
 
 
-@pytest.mark.parametrize(
-    'a, b, expect',
-    [
-        (1, -1, 2),
-        (-1, 1, -2),
-        (1, 1, 0),
-        (None, None, 0),
-        (0, None, 0),
-        (None, 0, 0),
-    ]
-)
-def test_sub(a, b, expect):
-    actual = math.sub(a, b)
-    assert actual == expect
+@given(numbers_strategy(), numbers_strategy())
+def test_sub(x, y):
+    if x is None or y is None:
+        assert math.sub(x, y) == 0
+
+    assume(x and y)
+    assert math.sub(x, y) == x - y
+
 
 
 @pytest.mark.parametrize(
