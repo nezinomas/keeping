e5e73fe3713df3c98b68ed9449100ad2757694be
nezinomas <nezinomas@gmai.com>
2023-09-11 16:46:35 +0300

    tests generated by Sourcery

--- project/bookkeeping/tests/test_views_no_incomes.py
+++ project/bookkeeping/tests/test_views_no_incomes.py
@@ -127,3 +127,69 @@ def test_no_incomes_summary(_data):
     assert actual[2]["label"] == "cut"
     assert round(actual[2]["money_fund"], 2) == 12
     assert round(actual[2]["money_fund_pension"], 2) == 14
+
+
+@pytest.fixture
+def no_incomes_data_class():
+    # Create a sample NoIncomesData object for testing
+    return SimpleNamespace(
+        year=1999,
+        months=12,
+        account_sum=1000,
+        fund_sum=500,
+        pension_sum=200,
+        expenses=[
+            {"title": "Expense 1", "sum": 100},
+            {"title": "Expense 2", "sum": 200},
+            {"title": "Expense 3", "sum": 300},
+        ],
+        savings={"sum": 500},
+        unnecessary=["Expense 2"]
+    )
+
+
+def test_summary_property(no_incomes_data_class):
+    # Arrange
+    no_incomes = NoIncomes(no_incomes_data_class)
+
+    # Act
+    summary = no_incomes.summary
+
+    # Assert
+    assert len(summary) == 3
+    assert summary[0]["label"] == "money"
+    assert summary[0]["money_fund"] == 1500
+    assert summary[0]["money_fund_pension"] == 1700
+
+    assert summary[1]["label"] == "no_cut"
+    assert summary[1]["money_fund"] == 1500 / no_incomes.avg_expenses
+    assert summary[1]["money_fund_pension"] == 1700 / no_incomes.avg_expenses
+
+    assert summary[2]["label"] == "cut"
+    assert summary[2]["money_fund"] == 1500 / (no_incomes.avg_expenses - no_incomes.cut_sum)
+    assert summary[2]["money_fund_pension"] == 1700 / (no_incomes.avg_expenses - no_incomes.cut_sum)
+
+
+def test_calc_method(no_incomes_data_class):
+    # Arrange
+    no_incomes = NoIncomes(no_incomes_data_class)
+
+    # Act
+    no_incomes._calc()
+
+    # Assert
+    assert no_incomes.avg_expenses == (100 + 200 + 300 + 500) / 12
+    assert no_incomes.cut_sum == (200 + 500) / 12
+
+
+def test_div_method(no_incomes_data_class):
+    # Arrange
+    no_incomes = NoIncomes(no_incomes_data_class)
+
+    # Act
+    result1 = no_incomes._div(10, 2)
+    result2 = no_incomes._div(10, 0)
+
+    # Assert
+    assert result1 == 5
+    assert result2 == 0
