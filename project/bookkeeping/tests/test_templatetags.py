5469f5e786a519a7d03b027848132823cac87e9b
nezinomas <nezinomas@gmai.com>
2023-09-12 09:44:56 +0300

    update tests

--- project/bookkeeping/tests/test_templatetags.py
+++ project/bookkeeping/tests/test_templatetags.py
@@ -4,24 +4,26 @@ from django.template import Context, Template
 
 @pytest.fixture(name="info_table")
 def fixture_info_table():
-    return Template("{% load tables %}" "{% info_table arr %}")
+    return Template("{% load slippers %}" "{% info_table data=arr %}")
 
 
-@pytest.mark.parametrize("arr", [({"arr": {}}), ({}), ({"arr": {"xxx": "yyy"}})])
+@pytest.mark.parametrize(
+    "arr",
+    [
+        ({"arr": {}}),
+        ({}),
+        ({"arr": {"xxx": "yyy"}}),
+        ({"arr": {"data": "yyy"}}),
+        ({"arr": {"title": "yyy"}}),
+        ({"arr": {"title": "", "data": ""}}),
+    ]
+)
 def test_info_table_no_data(info_table, arr):
     ctx = Context(arr)
 
     actual = info_table.render(ctx)
 
     assert actual == "\n\n\n\n\n"
-
-
-def test_info_table_with_data(info_table):
-    ctx = Context({"arr": {"data": [1]}})
-
-    actual = info_table.render(ctx)
-
-    assert "0,01" in actual
 
 
 def test_info_table_with_data_and_title(info_table):
