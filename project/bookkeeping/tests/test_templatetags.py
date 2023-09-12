1f62d7d86b28f7fc39ad613076e2b478ecf8eb2e
nezinomas <nezinomas@gmai.com>
2023-09-12 10:09:26 +0300

    additional test for highlight

--- project/bookkeeping/tests/test_templatetags.py
+++ project/bookkeeping/tests/test_templatetags.py
@@ -35,6 +35,21 @@ def test_info_table_with_data_and_title(info_table):
     assert "xxx" in actual
 
 
+def test_info_table_highlight(info_table):
+    ctx = Context({
+        "arr": {
+            "data": [1, -2],
+            "title": ["x", "y"],
+            "highlight": [True, True]
+        }
+    })
+
+    actual = info_table.render(ctx)
+
+    assert '<td class="table-success">0,01</td>' in actual
+    assert '<td class="table-danger">-0,02</td>' in actual
+
+
 def test_info_table_for_calculate_debt_remains():
     template = Template("{% load slippers %}" "{% info_table data=arr calculate_debt_remains='true' %}")
     ctx = Context({"arr": {"data": [5, 3], "title": ["x", 'y']}})
