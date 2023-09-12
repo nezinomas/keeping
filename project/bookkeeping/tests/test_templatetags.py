8066ebfe4dfab09f04ed084f60562c91921184ec
nezinomas <nezinomas@gmai.com>
2023-09-12 10:05:55 +0300

    show remains to be return in info_table template

--- project/bookkeeping/tests/test_templatetags.py
+++ project/bookkeeping/tests/test_templatetags.py
@@ -33,3 +33,12 @@ def test_info_table_with_data_and_title(info_table):
 
     assert "0,01" in actual
     assert "xxx" in actual
+
+
+def test_info_table_for_calculate_debt_remains():
+    template = Template("{% load slippers %}" "{% info_table data=arr calculate_debt_remains='true' %}")
+    ctx = Context({"arr": {"data": [5, 3], "title": ["x", 'y']}})
+
+    actual = template.render(ctx)
+
+    assert "Liko grąžinti:  0,02" in actual
