da7677c1e4560cafd7553ac2c2886c2809ca6ab5
nezinomas <nezinomas@gmai.com>
2023-11-06 13:55:49 +0200

    get_data method

--- project/bookkeeping/services/forecast.py
+++ project/bookkeeping/services/forecast.py
@@ -1,4 +1,4 @@
-from dataclasses import dataclass, field
+from dataclasses import dataclass
 from ...expenses.models import Expense
 from ...incomes.models import Income
 from ...savings.models import Saving
@@ -10,8 +10,18 @@ from ...core.lib.date import monthnames
 class ForecastServiceData:
     year: int
 
+    def data(self):
+        return {
+            "incomes": self.get_data(Income.objects.sum_by_month(self.year)),
+            "expenses": self.get_data(Expense.objects.sum_by_month(self.year)),
+            "savings": self.get_data(Saving.objects.sum_by_month(self.year)),
+            "planned_incomes": self.get_planned_data(
+                IncomePlan.objects.year(self.year).values(*monthnames())
+            ),
+        }
+
     def get_data(self, data):
-        arr = [0]*12
+        arr = [0] * 12
 
         for row in data:
             month = row["date"].month
@@ -20,8 +30,8 @@ class ForecastServiceData:
         return arr
 
     def get_planned_data(self, data):
-        arr = [0]*12
-        month_map = {month: i+1 for i, month in enumerate(monthnames())}
+        arr = [0] * 12
+        month_map = {month: i + 1 for i, month in enumerate(monthnames())}
 
         for row in data:
             for k, v in row.items():
