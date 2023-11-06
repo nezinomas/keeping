c4d74938b87bc72f1645d7d810b9793deb2ce538
nezinomas <nezinomas@gmai.com>
2023-11-06 16:41:16 +0200

    forecast for end or year

--- project/bookkeeping/services/forecast.py
+++ project/bookkeeping/services/forecast.py
@@ -4,6 +4,7 @@ from ...incomes.models import Income
 from ...savings.models import Saving
 from ...plans.models import IncomePlan
 from ...core.lib.date import monthnames
+import polars as pl
 
 
 @dataclass
@@ -44,4 +45,51 @@ class ForecastServiceData:
 
 
 class ForecastService:
-    pass
+    def __init__(self, month, data):
+        self._month = month
+        self._data = self._create_df(data)
+
+    def _create_df(self, data):
+        return pl.DataFrame(data | {"month": list(range(1, 13))})
+
+    def balance(self):
+        df = (
+            self._data
+            .filter(pl.col("month") < self._month)
+            .sum()
+            .with_columns(
+                (pl.col("incomes") - pl.col("expenses") - pl.col("savings")).alias("balance")
+            )
+        )
+        return df.select(pl.col("balance")).to_series().to_list()[0]
+
+    def planned_incomes(self):
+        df = (
+            self._data
+            .filter(pl.col("month") >= self._month)
+            .select(pl.col("planned_incomes"))
+            .sum()
+        )
+        return df[0, 0]
+
+    def averages(self):
+        min_ = 0 if self._month < 3 else self._month - 3
+        max_ = self._month - 1
+        df = (
+            self._data
+            .filter((pl.col("month") >= min_) & ((pl.col("month") <= max_)))
+            .mean()
+        )
+        return {"expenses": df["expenses"][0], "savings": df["savings"][0]}
+
+    def forecast(self):
+        month_left = 12 - self._month + 1
+        averages = self.averages()
+
+        return (
+            0.0
+            + self.balance()
+            + self.planned_incomes()
+            - averages["expenses"] * month_left
+            - averages["savings"] * month_left
+        )
