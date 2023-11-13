aeb87211287e5487f34ccf4b28428aca2f4c0ea7
nezinomas <nezinomas@gmai.com>
2023-11-13 12:03:39 +0200

    imports

--- project/bookkeeping/services/__init__.py
+++ project/bookkeeping/services/__init__.py
@@ -1,16 +1,16 @@
-from .accounts import AccountService, AccountServiceData
-from .chart_summary import ChartSummaryService, ChartSummaryServiceData
-from .chart_summary_expenses import (ChartSummaryExpensesService,
-                                     ChartSummaryExpensesServiceData)
-from .detailed import DetailedService, DetailerServiceData
-from .expand_day import ExpandDayService
-from .expenses import load_service as load_expense_service
-from .index import load_service as load_income_service
-from .month import load_service as load_month_service
-from .pensions import PensionServiceData, PensionsService
-from .savings import SavingServiceData, SavingsService
-from .summary_savings_and_incomes import (ServiceSummarySavingsAndIncomes,
-                                          ServiceSummarySavingsAndIncomesData)
-from .wealth import WealthService, WealthServiceData
-from .forecast import load_service as load_forecast_service
-from ..lib.no_incomes import load_service as load_no_income_service
+from . import (
+    accounts,
+    chart_summary,
+    chart_summary_expenses,
+    detailed,
+    expand_day,
+    expenses,
+    forecast,
+    index,
+    month,
+    pensions,
+    savings,
+    summary_savings,
+    summary_savings_and_incomes,
+    wealth
+)
