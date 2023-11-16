a46c5787cee3708294470e8c2ce31503460a8f67
nezinomas <nezinomas@gmai.com>
2023-11-16 16:53:48 +0200

    fixed sum error

--- project/bookkeeping/lib/make_dataframe.py
+++ project/bookkeeping/lib/make_dataframe.py
@@ -78,9 +78,12 @@ class MakeDataFrame:
         }
 
     def create_data(self, sum_col_name: str = "sum") -> DF:
+        data = self._modify_data()
+
         return (
-            pl.DataFrame(self._data)
-            .pipe(self._insert_missing_rows)
+            pl.DataFrame(data)
+            .pipe(self._group_and_sum)
+            .sort([pl.col.title, pl.col.date])
             .select(["date", "title", sum_col_name])
             .sort(["title", "date"])
             .with_columns(pl.col(sum_col_name).cast(pl.Int32))
@@ -97,40 +100,17 @@ class MakeDataFrame:
             .sort("date")
         )
 
-    def _insert_missing_rows(self, df) -> pl.Expr:
-        df_empty = self._empty_df()
-
-        if df.is_empty():
-            return df_empty.with_columns(
-                title=pl.lit("__tmp_to_drop__"),
-                sum=pl.lit(0),
-                exception_sum=pl.lit(0),
-            )
-
+    def _group_and_sum(self, df):
+        cols = df.columns
         return (
-            df_empty.join(df, on="date", how="outer")
-            .fill_null(0)
-            .select(pl.all().forward_fill())
+            df
+            .group_by([pl.col.date, pl.col.title])
+            .agg([pl.col(i).sum() for i in cols if i not in ["date", "title"]])
         )
-
-    def _empty_df(self) -> DF:
-        if self.month:
-            days = calendar.monthrange(self.year, self.month)[1]
-            start = date(self.year, self.month, 1)
-            end = date(self.year, self.month, days)
-            every = "1d"
-        else:
-            start = date(self.year, 1, 1)
-            end = date(self.year, 12, 31)
-            every = "1mo"
-
-        rng = pl.date_range(start, end, every, eager=True)
-        return pl.DataFrame({"date": rng})
 
     def _drop_columns(self, df: DF) -> pl.Expr:
         col_to_drop = [
             "__tmp_to_drop__",
-            "null",
         ]
         return df.drop([name for name in col_to_drop if name in df.columns])
 
@@ -141,6 +121,7 @@ class MakeDataFrame:
 
         cols_diff = set(self._columns) - set(df.columns)
         cols = [pl.lit(0).alias(col_name) for col_name in cols_diff]
+
         return df.select([pl.all(), *cols])
 
     def _sort_columns(self, df: DF) -> pl.Expr:
--- project/bookkeeping/tests/test_lib_make_dataframe.py
+++ project/bookkeeping/tests/test_lib_make_dataframe.py
@@ -65,33 +65,14 @@ def test_month_expenses(month_data, columns):
     assert actual.columns == ["date", "T0", "T1", "T2"]
 
     # 1999-01-01 first row
-    assert actual[0, "T0"] == 0.0
-    assert actual[0, "T1"] == 4.0
-    assert actual[0, "T2"] == 2.0
+    assert actual[0, "T0"] == 0
+    assert actual[0, "T1"] == 7
+    assert actual[0, "T2"] == 2
 
     # 1999-12-01 last row
-    assert actual[11, "T0"] == 0.0
-    assert actual[11, "T1"] == 4.0
-    assert actual[11, "T2"] == 5.0
-
-
-def test_month_expenses(month_data, columns):
-    actual = make_dataframe.MakeDataFrame(
-        year=1999, data=month_data, columns=columns
-    ).data
-
-    assert actual.shape == (12, 4)
-    assert actual.columns == ["date", "T0", "T1", "T2"]
-
-    # 1999-01-01 first row
-    assert actual[0, "T0"] == 0.0
-    assert actual[0, "T1"] == 4.0
-    assert actual[0, "T2"] == 2.0
-
-    # 1999-12-01 last row
-    assert actual[11, "T0"] == 0.0
-    assert actual[11, "T1"] == 4.0
-    assert actual[11, "T2"] == 5.0
+    assert actual[11, "T0"] == 0
+    assert actual[11, "T1"] == 4
+    assert actual[11, "T2"] == 5
 
 
 def test_month_expenses_partial_data():
@@ -99,8 +80,7 @@ def test_month_expenses_partial_data():
         {"date": date(1999, 2, 1), "title": "X", "sum": 5},
     ]
 
-    actual = make_dataframe.MakeDataFrame(
-        year=1999, data=data).data
+    actual = make_dataframe.MakeDataFrame(year=1999, data=data).data
 
     assert actual.shape == (12, 2)
     assert actual.columns == ["date", "X"]
@@ -110,9 +90,9 @@ def test_month_no_data_expenses(columns):
     actual = make_dataframe.MakeDataFrame(year=1999, data=[], columns=columns).data
 
     for i in range(12):
-        assert actual[i, "T0"] == 0.0
-        assert actual[i, "T1"] == 0.0
-        assert actual[i, "T2"] == 0.0
+        assert actual[i, "T0"] == 0
+        assert actual[i, "T1"] == 0
+        assert actual[i, "T2"] == 0
 
 
 def test_month_dtype(month_data, columns):
@@ -187,14 +167,14 @@ def test_day_expenses(day_data, columns):
     assert actual.columns == ["date", "T0", "T1", "T2"]
 
     # first row 1999-01-01
-    assert actual[0, "T0"] == 0.0
-    assert actual[0, "T1"] == 5.0
-    assert actual[0, "T2"] == 2.0
+    assert actual[0, "T0"] == 0
+    assert actual[0, "T1"] == 5
+    assert actual[0, "T2"] == 2
 
     # last row 1999-01-30
-    assert actual[29, "T0"] == 0.0
-    assert actual[29, "T1"] == 3.0
-    assert actual[29, "T2"] == 0.0
+    assert actual[29, "T0"] == 0
+    assert actual[29, "T1"] == 3
+    assert actual[29, "T2"] == 0
 
 
 @pytest.mark.parametrize("data", [([]), (None)])
