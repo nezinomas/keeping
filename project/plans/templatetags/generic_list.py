11efabecc5dc66a49bed9adbb453f17dd210614a
nezinomas <nezinomas@gmai.com>
2023-12-22 14:56:37 +0200

    show expense type in necessary plan list

--- project/plans/templatetags/generic_list.py
+++ project/plans/templatetags/generic_list.py
@@ -1,16 +1,16 @@
 from django import template
 
-from ...core.lib.translation import month_names
 
 register = template.Library()
 
 
 @register.inclusion_tag("plans/includes/generic_list.html")
-def generic_list(year, items, type=None, update=None, delete=None):
+def generic_list(year, items, type=None, expense_type=None, update=None, delete=None):
     return {
         "year": year,
         "items": items,
         "type": type,
+        "expense_type": "expense_type" if expense_type else None,
         "update": update,
         "delete": delete,
     }
