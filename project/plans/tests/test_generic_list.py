ae07258031516f5c0c6ab4a3b155d906ba9b13ee
Audrius <nezinomas@gmail.com>
2023-12-23 18:13:45 +0200

    formated with black

--- project/plans/tests/test_generic_list.py
+++ project/plans/tests/test_generic_list.py
@@ -3,32 +3,32 @@ from django.template import Context, Template
 
 
 def _remove_line_end(rendered):
-    return str(rendered).replace('\n', '')
+    return str(rendered).replace("\n", "")
 
 
 @pytest.fixture()
 def _template():
     template_to_render = Template(
-        '{% load generic_list %}'
+        "{% load generic_list %}"
         '{% generic_list year=2019 items=items type="type" update=url_update %}'
     )
     return template_to_render
 
 
 def test_has_type(_template):
-    items = [type('O', (object,), dict(january=11, type='TypeRowName'))]
-    context = Context({'items': items})
+    items = [type("O", (object,), dict(january=11, type="TypeRowName"))]
+    context = Context({"items": items})
 
-    actual = _template.render(context).replace('\n', '').replace("  ", "")
+    actual = _template.render(context).replace("\n", "").replace("  ", "")
 
     assert '<td class="text-start">TypeRowName</td>' in actual
 
 
 def test_no_type(_template):
-    items = [type('O', (object,), dict(january=11))]
-    context = Context({'items': items})
+    items = [type("O", (object,), dict(january=11))]
+    context = Context({"items": items})
 
-    actual = _template.render(context).replace('\n', '').replace("  ", "")
+    actual = _template.render(context).replace("\n", "").replace("  ", "")
 
     assert '<td class="text-start">type</td>' in actual
 
@@ -36,30 +36,26 @@ def test_no_type(_template):
 def test_necessary_expense_1(_template):
     items = [
         type(
-            'O',
+            "O",
             (object,),
-            dict(january=11, type='TypeRowName', expense_type=dict(necessary=True))
+            dict(january=11, type="TypeRowName", expense_type=dict(necessary=True)),
         )
     ]
-    context = Context({'items': items})
+    context = Context({"items": items})
 
-    actual = _template.render(context).replace('\n', '').replace("  ", "")
-    expect = '<td class="text-start">TypeRowName <i class="bi bi-star plans-star"></i></td>'
+    actual = _template.render(context).replace("\n", "").replace("  ", "")
+    expect = (
+        '<td class="text-start">TypeRowName <i class="bi bi-star plans-star"></i></td>'
+    )
 
     assert expect in _remove_line_end(actual)
 
 
 def test_necessary_expense_3(_template):
-    items = [
-        type(
-            'O',
-            (object,),
-            dict(january=11, saving_type=dict(title='xxx'))
-        )
-    ]
-    context = Context({'items': items})
+    items = [type("O", (object,), dict(january=11, saving_type=dict(title="xxx")))]
+    context = Context({"items": items})
 
-    actual = _template.render(context).replace('\n', '').replace("  ", "")
+    actual = _template.render(context).replace("\n", "").replace("  ", "")
     expect = '<td class="text-start">type <i class="bi bi-star plans-star"></i></td>'
 
     assert expect in _remove_line_end(actual)
@@ -67,19 +63,19 @@ def test_necessary_expense_3(_template):
 
 def test_no_object_1(_template):
     items = []
-    context = Context({'items': items})
+    context = Context({"items": items})
 
     actual = _template.render(context)
-    expect = '<b>2019</b> metais įrašų nėra.'
+    expect = "<b>2019</b> metais įrašų nėra."
 
     assert expect in _remove_line_end(actual)
 
 
 def test_no_object_2(_template):
     items = None
-    context = Context({'items': items})
+    context = Context({"items": items})
 
     actual = _template.render(context)
-    expect = '<b>2019</b> metais įrašų nėra.'
+    expect = "<b>2019</b> metais įrašų nėra."
 
     assert expect in _remove_line_end(actual)
