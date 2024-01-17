e55e5390ac2d2e460062c7a5f0d4193f1767d871
nezinomas <nezinomas@gmai.com>
2024-01-17 13:57:37 +0200

    modify my-theme js file

--- project/static/js/highcharts_themes/my-theme.js
+++ project/static/js/highcharts_themes/my-theme.js
@@ -1,42 +1,74 @@
 Highcharts.theme = {
+    chart: {
+        backgroundColor: null,
+        style:{
+            fontFamily: "Dosis, sans-serif"
+        },
+    },
     title: {
-        verticalAlign: 'top',
+        verticalAlign: "top",
+        style: {
+            fontSize: "16px",
+            fontWeight: "bold",
+            textTransform: "uppercase"
+        }
+    },
+    tooltip:{
+        borderWidth:0,
+        backgroundColor: "rgba(219,219,216,0.8)",
+        shadow: !1
     },
     xAxis: {
-        lineColor: '#000',
+        lineColor: "#000",
         lineWidth: 2,
+        gridLineWidth: 1,
         labels: {
             style: {
-                fontSize: '10px',
+                fontSize: "10px",
             },
         }
     },
     yAxis: {
+        title:{
+            style:{
+                textTransform: "uppercase"
+            }
+        },
         labels: {
             style: {
-                fontSize: '10px',
+                fontSize: "10px",
             }
         },
         minorTicks: false,
     },
     legend: {
         enabled: false,
-        layout: 'horizontal',
-        align: 'right',
-        verticalAlign: 'top',
+        layout: "horizontal",
+        align: "right",
+        verticalAlign: "top",
         floating: true,
         borderWidth: 0,
+        backgroundColor: "#F0F0EA",
+        itemStyle:{
+            fontWeight: "bold",
+            fontSize: "13px",
+        },
     },
     credits: {
-        enabled: false
+        enabled: false,
     },
     tooltip: {
-        backgroundColor: '#FFF',
+        backgroundColor: "#FFF",
         borderWidth: 1,
         shadow: true,
         useHTML: true,
         style: {
-            fontSize: '12px',
+            fontSize: "12px",
+        },
+    },
+    plotOptions:{
+        candlestick: {
+            lineColor: "#404048"
         },
     },
 };
@@ -47,7 +79,7 @@ Highcharts.setOptions(Highcharts.theme);
 // Set decimal point format
 Highcharts.setOptions({
     lang: {
-        thousandsSep: '.',
-        decimalPoint: ',',
+        thousandsSep: ".",
+        decimalPoint: ",",
     }
 });
