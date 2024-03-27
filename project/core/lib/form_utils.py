9d9aed33908a9e87151af893d544f7a089ac479b
nezinomas <nezinomas@gmai.com>
2024-03-27 15:54:23 +0200

    comment code

--- project/core/lib/form_utils.py
+++ project/core/lib/form_utils.py
@@ -1,19 +1,24 @@
-from django.forms import BooleanField
+from django.forms import BooleanField, ModelMultipleChoiceField
 
 
 def add_css_class(instance):
-    default_input_css_class = []
+    pass
+    # default_input_css_class = []
 
-    for field_name in instance.fields:
-        if isinstance(instance.fields[field_name], BooleanField):
-            continue
+    # for field_name in instance.fields:
+    #     print(f'--------------------------->\nname {field_name} type {type(field_name)} - {type(instance.fields[field_name])}\n')
+    #     if isinstance(instance.fields[field_name], (BooleanField, ModelMultipleChoiceField)):
+    #         instance.fields[field_name].widget.attrs["class"] = " ".join(
+    #         ['']
+    #     )
+    #     else :
 
-        if css_class := instance.fields[field_name].widget.attrs.get("class"):
-            default_input_css_class.append(css_class)
+    #     # if css_class := instance.fields[field_name].widget.attrs.get("class"):
+    #     #     default_input_css_class.append(css_class)
 
-        instance.fields[field_name].widget.attrs["class"] = " ".join(
-            default_input_css_class
-        )
+    #         instance.fields[field_name].widget.attrs["class"] = " ".join(
+    #             ['form-control-my']
+    #         )
 
 
 def clean_year_picker_input(field_name, data, cleaned_data, errors):
