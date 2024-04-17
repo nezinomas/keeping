1b7a6c23cbad359d1513be3488a2d26a63ea6c58
nezinomas <nezinomas@gmai.com>
2024-04-17 15:03:55 +0300

    modal image view

--- project/core/urls.py
+++ project/core/urls.py
@@ -7,6 +7,7 @@ app_name = App_name
 
 urlpatterns = [
     path("year/<int:year>/", views.set_year, name="set_year"),
+    path("modal/image/", views.ModalImage.as_view(), name="modal_image"),
     path(
         "set/balances/", views.RegenerateBalances.as_view(), name="regenerate_balances"
     ),
