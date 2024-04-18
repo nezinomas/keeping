98c572f441cc875dcc85c0b1a8f059c396f9b9c4
nezinomas <nezinomas@gmai.com>
2024-04-18 17:34:00 +0300

    update integration tests

--- project/counts/tests/test_integration.py
+++ project/counts/tests/test_integration.py
@@ -2,6 +2,7 @@ from time import sleep
 
 import pytest
 from selenium.webdriver.common.by import By
+from selenium.webdriver.common.action_chains import ActionChains
 
 from ...core.tests.test_integration_browser import Browser
 from ..factories import CountTypeFactory
@@ -30,19 +31,27 @@ class Incomes(Browser):
 
         self.browser.get(f"{self.live_server_url}/counts/")
 
-        self.browser.find_element(By.CSS_SELECTOR, ".btn.dropdown-toggle.dropdown-toggle-split.nav-link.active").click()
-        self.browser.find_element(By.XPATH, "//button[text()='Add count type']").click()
-        sleep(0.5)
+        # hover over dropdown menu
+        menu = self.browser.find_element(By.XPATH, '//a[contains(@href,"/counts/")]/following-sibling::*[1]')
+        ActionChains(self.browser).move_to_element(menu).perform()
+        sleep(0.25)
 
+        # click on add count type
+        self.browser.find_element(By.XPATH, "//button[text()='Add count type']").click()
+        sleep(0.25)
+
+        # fill form
         self.browser.find_element(By.ID, "id_title").send_keys("-XXX-")
         self.browser.find_element(By.ID, "_close").click()
-        sleep(0.5)
+        sleep(0.25)
 
+        # fink delete button and click it
         self.browser.find_element(By.CSS_SELECTOR, ".button-danger").click()
-        sleep(0.5)
+        sleep(0.25)
+
         # delete button in delete form
         self.browser.find_element(By.ID, "_close").click()
-        sleep(0.5)
+        sleep(0.25)
 
         page = self.browser.page_source
         assert '-AAA-' in page
