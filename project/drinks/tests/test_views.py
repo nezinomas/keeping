e1e9e0c94c4d8a2e6500306e62b28948b5bee3e1
nezinomas <nezinomas@gmai.com>
2024-05-13 12:30:40 +0300

    Duplicate ID button-new

--- project/drinks/tests/test_views.py
+++ project/drinks/tests/test_views.py
@@ -34,7 +34,7 @@ def test_index_add_button(client_logged):
     content = response.content.decode()
 
     pattern = re.compile(
-        r'<button .*? id="button-new" .*? hx-get="(.*?)" hx-target="#mainModal">(\w+)<\/button>', re.MULTILINE)
+        r'<button type="button" class="button-outline-success" hx-get="(.*?)" hx-target="#mainModal">(\w+)<\/button>', re.MULTILINE)
     res = re.findall(pattern, content)
 
     assert len(res[0]) == 2
