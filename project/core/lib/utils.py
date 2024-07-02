0c279c5d8dd4c918c1222ff6f5fb4597320badab
nezinomas <nezinomas@gmai.com>
2024-07-02 16:22:06 +0300

    delete unused methods

--- project/core/lib/utils.py
+++ project/core/lib/utils.py
@@ -15,24 +15,6 @@ def get_request_kwargs(name):
     return crequest.resolver_match.kwargs.get(name) if crequest else None
 
 
-def sum_all(arr, keys=None):
-    if isinstance(arr, QuerySet):
-        arr = [*arr.values()]
-
-    result = Counter()
-    for item in arr:
-        for k, v in item.items():
-            if isinstance(v, (int, float)) and (keys is None or k in keys):
-                result[k] += v
-    return dict(result)
-
-
-def sum_col(arr: List[Dict], key: Any) -> float:
-    rtn = sum_all(arr)
-
-    return rtn.get(key, 0)
-
-
 def total_row(data, fields) -> dict:
     row = {field: sum(getattr(d, field, 0) for d in data) for field in fields}
 
