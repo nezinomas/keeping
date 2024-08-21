2c616f273c01345a23d09b4d784a50aeced77af9
nezinomas <nezinomas@gmai.com>
2024-08-21 14:18:50 +0300

    ExpenseTypeSelect widget

--- project/expenses/forms.py
+++ project/expenses/forms.py
@@ -14,6 +14,21 @@ from ..core.lib.date import set_year_for_form
 from .models import Expense, ExpenseName, ExpenseType
 
 
+class ExpenseTypeSelect(forms.Select):
+    def create_option(self, name, value, label, selected, index, subindex=None, attrs=None):
+        option = super().create_option(name, value, label, selected, index, subindex, attrs)
+
+        if value:
+            url = reverse("expenses:load_expense_name", kwargs={"expense_type": value.instance.pk})
+        else:
+            url = reverse("expenses:load_expense_name")
+
+        option['attrs']['hx-get'] = url
+        option['attrs']['hx-target'] = "#id_expense_name"
+        option['attrs']['hx-trigger'] = "click"
+        return option
+
+
 class ExpenseForm(ConvertToPrice, forms.ModelForm):
     price = forms.FloatField(min_value=0.01)
     total_sum = forms.CharField(required=False)
