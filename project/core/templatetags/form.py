f33e3edc43a279de72b177388cdd92240687137b
nezinomas <nezinomas@gmai.com>
2024-11-20 15:01:20 +0200

    generic delete form as django-cotton component

--- project/core/templatetags/form.py
+++ project/core/templatetags/form.py
@@ -13,13 +13,3 @@ def generic_form(context, title, css_class=None):
         "url": context.get("url", ""),
         "hx_trigger_form": context.get("hx_trigger_form", ""),
     }
-
-
-@register.inclusion_tag("core/includes/generic_delete_form.html", takes_context=True)
-def generic_delete_form(context, title):
-    return {
-        "title": title,
-        "url": context.get("url"),
-        "object": context.get("object"),
-        "hx_trigger_form": context.get("hx_trigger_form", ""),
-    }
