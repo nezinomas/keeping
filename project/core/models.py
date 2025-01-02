2237b3c53396e6d2b5b9cc89cab39b3d7c01fcde
nezinomas <nezinomas@gmai.com>
2025-01-02 15:29:59 +0200

    imports

--- project/core/models.py
+++ project/core/models.py
@@ -1,4 +1,4 @@
-from django.core.validators import MinLengthValidator, MinValueValidator
+from django.core.validators import MinLengthValidator
 from django.db import models
 from django.utils.text import slugify
 
