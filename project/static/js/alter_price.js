6e78cbe1ef87eacc00eb5bd1ca2377c7f44d1d8f
nezinomas <nezinomas@gmai.com>
2025-07-14 14:13:52 +0300

    refactored alter price code

--- project/static/js/alter_price.js
+++ project/static/js/alter_price.js
@@ -1,32 +1,16 @@
-$(document).ready(function () {
+document.addEventListener("DOMContentLoaded", function () {
     const fields = ["id_price", "id_fee"];
 
     fields.forEach(field => {
         let element = document.getElementById(field);
         if (element) {
             element.type = "text";
+
+            element.addEventListener("keyup", function (event) {
+                event.preventDefault();
+                let fieldValue = String(element.value).replaceAll(",", ".");
+                element.value = fieldValue;
+            });
         }
     });
 });
-
-
-let price_input = document.getElementById("id_price");
-price_input.addEventListener("keyup", function (event) {
-    event.preventDefault();
-
-    let price = String(price_input.value).replaceAll(",", ".");
-
-    price_input.value = price;
-});
-
-
-let fee_input = document.getElementById("id_fee");
-if(fee_input) {
-    fee_input.addEventListener("keyup", function (event) {
-        event.preventDefault();
-
-        let fee = String(fee_input.value).replaceAll(",", ".");
-
-        fee_input.value = fee;
-    });
-}
