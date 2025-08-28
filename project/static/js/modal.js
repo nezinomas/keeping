3b605226f1ac510543eabe8b6278c1d4db91f10b
nezinomas <nezinomas@gmail.com>
2025-08-28 14:16:32 +0300

    refactore modal.js

--- project/static/js/modal.js
+++ project/static/js/modal.js
@@ -1,124 +1,136 @@
 // Constants for modal and container IDs
-const MODALS = ['mainModal', 'imgModal'];
 const CONTAINERS = ['mainModalContainer', 'imgModalContainer'];
 const FORM_FIELDS = ['price', 'fee', 'quantity', 'title', 'remark', 'attachment'];
+const MODALS = {
+    mainModal: null,
+    imgModal: null
+};
 
 
-// prevent default form submission
-document.addEventListener('submit', (event) => {
-    if (event.target.matches('.modal-form')) {
-        event.preventDefault();
+// Modal class to encapsulate modal behavior
+class Modal {
+    constructor(id) {
+        this.id = id;
+        this.container = document.querySelector(`#${id}Container`);
+        this.form = this.container?.querySelector('#modal-form');
+
     }
-});
 
+    show() {
+        this.container && (this.container.style.display = 'block');
+    }
 
-// close modal on Close Buton
-document.addEventListener('click', (event) => {
-    if (event.target.matches('.modal-close')) {
-        const modalId = event.target.dataset.dismiss;
-        if (modalId) {
-            modalHide(modalId);
+    hide() {
+        if (!this.container) {
+            return;
+        }
+
+        this.container.style.display = 'none';
+
+        if(this.form) {
+            this.#hxTrigger();
+            this.form.remove();
         }
     }
-});
 
+    resetForm() {
+        if (!this.form) {
+            return;
+        }
 
-// close modal on ESC
-document.addEventListener('keydown', (event) => {
-    if (event.keyCode === 27) {
-        document.querySelectorAll('.modal-close').forEach((button) => {
-            const modalId = button.dataset.dismiss;
-            if (modalId) {
-                modalHide(modalId);
+        FORM_FIELDS.forEach((field) => {
+            const input = this.form.querySelector(`#id_${field}`);
+            if (input) {
+                input.value = '';
             }
         });
     }
-});
 
+    #hxTrigger() {
+        const triggerName = this.form?.dataset.hxTriggerForm;
+        const dataInserted = this.form?.dataset.hxInserted;
 
-// close modal on outside (e.g. containerModal) click
-document.addEventListener('click', (event) => {
-    const targetId = event.target.id;
-    if (CONTAINERS.includes(targetId)) {
-        const modalId = targetId.replace('Container', '');
-        modalHide(modalId);
-    }
-});
-
-
-// show modal on click button with hx-target="#mainModal"
-htmx.on('htmx:afterSwap', (event) => {
-    const targetId = event.detail.target?.id;
-    if (!MODALS.includes(targetId)) return;
-
-    const modalContainer = document.querySelector(`#${targetId}Container`);
-    if (modalContainer) {
-        modalContainer.style.display = 'block';
-    }
-
-    if (targetId === 'imgModal') {
-        const url = event.detail.requestConfig?.triggeringEvent?.originalTarget?.dataset?.url;
-        if (url) {
-            const modalBodyInput = document.querySelector(`#${targetId} .modal-body`);
-            if (modalBodyInput) {
-                modalBodyInput.innerHTML = `<img src="${url}" />`;
-            }
+        if (triggerName && triggerName !== 'None' && dataInserted) {
+            htmx.trigger('body', triggerName, {});
         }
     }
-});
+}
 
 
-// reload modal form with error messages or reset fields and close modal form
-htmx.on('htmx:beforeSwap', (event) => {
-    const targetId = event.detail.target?.id;
-    if (targetId === 'mainModal' && !event.detail.xhr.response) {
-        const submitterId = event.detail.requestConfig?.triggeringEvent?.submitter?.id;
+function initializeModals() {
+    // prevent default form submission
+    document.addEventListener('submit', (event) => {
+        if (event.target.matches('.modal-form')) {
+            event.preventDefault();
+        }
+    });
 
-        // Remove error messages
-        document.querySelectorAll('.invalid-feedback').forEach((el) => el.remove());
-        document.querySelectorAll('.is-invalid').forEach((el) => el.classList.remove('is-invalid'));
 
-        if (submitterId === '_new') {
-            // Reset fields
-            FORM_FIELDS.forEach((field) => {
-                const input = document.querySelector(`#id_${field}`);
-                if (input) {
-                    input.value = '';
-                }
+    // close modal on Close Buton
+    document.addEventListener('click', (event) => {
+        if (event.target.matches('.modal-close')) {
+            const modalId = event.target.dataset.dismiss;
+            MODALS[modalId]?.hide();
+        }
+    });
+
+
+    // close modal on ESC
+    document.addEventListener('keydown', (event) => {
+        if (event.key === 'Escape') {
+            document.querySelectorAll('.modal-close').forEach((button) => {
+                const modalId = button.dataset.dismiss;
+                MODALS[modalId]?.hide();
             });
         }
+    });
 
-        if (submitterId === '_close') {
-            modalHide(targetId);
-            const form = document.querySelector('#modal-form .modal-form');
-            if (form) {
-                form.reset();
+
+    // close modal on outside (e.g. containerModal) click
+    document.addEventListener('click', (event) => {
+        const targetId = event.target.id;
+        if (CONTAINERS.includes(targetId)) {
+            const modalId = targetId.replace('Container', '');
+            MODALS[modalId]?.hide();
+        }
+    });
+
+
+    // show modal on click button with hx-target="#mainModal"
+    htmx.on('htmx:afterSwap', (e) => {
+        const targetId = e.detail.target?.id;
+        const modal = new Modal(targetId)
+        MODALS[targetId] = modal;
+        modal.show();
+
+        if (targetId === 'imgModal') {
+            const url = e.detail.requestConfig?.triggeringEvent?.originalTarget?.dataset?.url;
+            if (url) {
+                const modalBodyInput = document.querySelector(`#${targetId} .modal-body`);
+                modalBodyInput && (modalBodyInput.innerHTML = `<img src="${url}" />`);
             }
         }
-
-        event.detail.shouldSwap = false;
-    }
-});
+    });
 
 
+    // reload modal form with error messages or reset fields and close modal form
+    htmx.on('htmx:beforeSwap', (e) => {
+        const targetId = e.detail.target?.id;
+        if (targetId !== 'mainModal' || e.detail.xhr.response) {
+        return;
+        }
 
-// trigger htmx on form submit if form has data-hx-trigger-form or data-hx-inserted attribute
-function hxTrigger() {
-    const form = document.querySelector('.modal-form');
-    const triggerName = form?.dataset.hxTriggerForm;
-    const dataInserted = form?.dataset.hxInserted;
+        const submitterId = e.detail.requestConfig?.triggeringEvent?.submitter?.id;
 
-    if (triggerName && triggerName !== 'None' && dataInserted) {
-        htmx.trigger('body', triggerName, {});
-    }
-}
+        MODALS.mainModal.resetForm();
 
+        if (submitterId === '_close') {
+            MODALS.mainModal.hide();
+        }
 
-// Hide modal
-function modalHide(target) {
-    const modalContainer = document.querySelector(`#${target}Container`);
-    if (modalContainer) {
-        modalContainer.style.display = 'none';
-        hxTrigger();
-    }
-}
+        e.detail.shouldSwap = false;
+    });
+};
+
+// Initialize modals when the DOM is ready
+document.addEventListener('DOMContentLoaded', initializeModals);
