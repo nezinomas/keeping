6b69071a04f3cd7492c0c2b2681f3d0612ce5611
nezinomas <nezinomas@gmail.com>
2025-09-25 15:14:58 +0300

    charts colors

--- project/static/highcharts/themes/theme.js
+++ project/static/highcharts/themes/theme.js
@@ -1,16 +1,16 @@
 Highcharts.theme = {
     colors: [
-        "#7cb5ec",
-        "#f7a35c",
-        "#90ee7e",
-        "#7798BF",
-        "#aaeeee",
-        "#ff0066",
-        "#eeaaee",
-        "#55BF3B",
-        "#DF5353",
-        "#7798BF",
-        "#aaeeee"
+        "var(--chart-color-0)",
+        "var(--chart-color-1)",
+        "var(--chart-color-2)",
+        "var(--chart-color-3)",
+        "var(--chart-color-4)",
+        "var(--chart-color-5)",
+        "var(--chart-color-6)",
+        "var(--chart-color-7)",
+        "var(--chart-color-8)",
+        "var(--chart-color-9)",
+        "var(--chart-color-10)",
     ],
     chart: {
         backgroundColor: null,
