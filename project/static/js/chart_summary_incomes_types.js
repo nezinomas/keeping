57d4612ec729b4f59ce33da36076b1d5cdab9134
nezinomas <nezinomas@gmail.com>
2025-10-03 15:58:25 +0300

    charts colors

--- project/static/js/chart_summary_incomes_types.js
+++ project/static/js/chart_summary_incomes_types.js
@@ -1,3 +1,18 @@
+Highcharts.setOptions({
+    colors: [
+        "var(--chart-color-0)",
+        "var(--chart-color-1)",
+        "var(--chart-color-2)",
+        "var(--chart-color-3)",
+        "var(--chart-color-4)",
+        "var(--chart-color-5)",
+        "var(--chart-color-6)",
+        "var(--chart-color-7)",
+        "var(--chart-color-8)",
+        "var(--chart-color-9)",
+    ]
+});
+
 document.addEventListener('DOMContentLoaded', () => {
     const chartData = JSON.parse(document.getElementById("chart-incomes-types-data").textContent);
 
