13d27b314dc6dc306d52d72cdc291302385684ae
nezinomas <nezinomas@gmail.com>
2025-11-12 16:26:22 +0200

    update savings model services

--- project/savings/services/model_services.py
+++ project/savings/services/model_services.py
@@ -23,19 +23,17 @@ class SavingTypeModelService:
             raise ValueError("Authenticated user required")
 
         self.user = user
-        self.model = cast(SavingTypeQuerySet, SavingType.objects)
+        self.objects = cast(SavingTypeQuerySet, SavingType.objects).related(self.user)
 
     def none(self):
         return SavingType.objects.none()
 
     def items(self, year=None):
         _year = year or self.user.year
-        return self.model.related(self.user).filter(
-            Q(closed__isnull=True) | Q(closed__gte=_year)
-        )
+        return self.objects.filter(Q(closed__isnull=True) | Q(closed__gte=_year))
 
     def all(self):
-        return self.model.related(self.user)
+        return self.objects
 
 
 class SavingModelService:
@@ -50,28 +48,23 @@ class SavingModelService:
             raise ValueError("Authenticated user required")
 
         self.user = user
-        self.model = cast(SavingQuerySet, Saving.objects)
+        self.objects = cast(SavingQuerySet, Saving.objects).related(self.user)
 
     def year(self, year):
-        return self.model.related(self.user).filter(date__year=year)
+        return self.objects.filter(date__year=year)
 
     def items(self):
-        return self.model.related(self.user)
+        return self.objects
 
     def sum_by_year(self):
-        return self.model.related(self.user).year_sum()
+        return self.objects.year_sum()
 
     def sum_by_month(self, year: int, month: Optional[int] = None):
-        return (
-            self.model.related(self.user)
-            .month_sum(year, month)
-            .annotate(title=Value("savings"))
-        )
+        return self.objects.month_sum(year, month).annotate(title=Value("savings"))
 
     def sum_by_month_and_type(self, year: int):
         return (
-            self.model.related(self.user)
-            .filter(date__year=year)
+            self.objects.filter(date__year=year)
             .annotate(cnt=Count("saving_type"))
             .values("saving_type")
             .annotate(date=TruncMonth("date"))
@@ -83,17 +76,13 @@ class SavingModelService:
         )
 
     def sum_by_day_and_type(self, year: int, month: int):
-        return (
-            self.model.related(self.user)
-            .day_sum(year=year, month=month)
-            .values("date", "sum", title=F("saving_type__title"))
+        return self.objects.day_sum(year=year, month=month).values(
+            "date", "sum", title=F("saving_type__title")
         )
 
     def sum_by_day(self, year: int, month: int):
-        return (
-            self.model.related(self.user)
-            .day_sum(year=year, month=month)
-            .annotate(title=Value(_("Savings")))
+        return self.objects.day_sum(year=year, month=month).annotate(
+            title=Value(_("Savings"))
         )
 
     def last_months(self, months: int = 6) -> float:
@@ -104,11 +93,7 @@ class SavingModelService:
         # back months to past; if months=6 then end=2019-08-01
         end = (start + timedelta(days=1)) - relativedelta(months=months)
 
-        return (
-            self.model.related(self.user)
-            .filter(date__range=(end, start))
-            .aggregate(sum=Sum("price"))
-        )
+        return self.objects.filter(date__range=(end, start)).aggregate(sum=Sum("price"))
 
 
 class SavingBalanceModelService:
@@ -120,13 +105,15 @@ class SavingBalanceModelService:
             raise ValueError("Authenticated user required")
 
         self.user = user
-        self.model = cast(SavingBalanceQuerySet, SavingBalance.objects)
+        self.objects = cast(SavingBalanceQuerySet, SavingBalance.objects).related(
+            self.user
+        )
 
     def items(self):
-        return self.model.related(self.user)
+        return self.objects
 
     def year(self, year: int, types=None):
-        qs = self.model.related(self.user).filter(year=year)
+        qs = self.objects.filter(year=year)
 
         if types:
             qs = qs.filter(saving_type__type__in=types)
@@ -135,8 +122,7 @@ class SavingBalanceModelService:
 
     def sum_by_type(self):
         return (
-            self.model.related(self.user)
-            .annotate(cnt=Count("saving_type"))
+            self.objects.annotate(cnt=Count("saving_type"))
             .values("saving_type__type")
             .annotate(y=F("year"))
             .values("y")
@@ -150,8 +136,7 @@ class SavingBalanceModelService:
 
     def sum_by_year(self):
         return (
-            self.model.related(self.user)
-            .annotate(y=F("year"))
+            self.objects.annotate(y=F("year"))
             .values("y")
             .annotate(incomes=Sum("incomes"), profit=Sum("profit_sum"))
             .order_by("year")
