e0b62562894d35433922cdebb646d2fa16404953
nezinomas <nezinomas@gmail.com>
2025-11-12 16:51:57 +0200

    imports

--- project/users/models.py
+++ project/users/models.py
@@ -1,6 +1,5 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
-from django.urls import reverse_lazy
 
 from ..journals.models import Journal
 from . import managers
