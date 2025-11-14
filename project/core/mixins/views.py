09c85f76bda54a0d9c27a11f2a5cc945315dc346
nezinomas <nezinomas@gmail.com>
2025-11-14 14:34:16 +0200

    refactored formset mixin

--- project/core/mixins/views.py
+++ project/core/mixins/views.py
@@ -53,11 +53,13 @@ class AddUserToKwargsMixin:
         return self.form_class(data, files, **kwargs)
 
     def get_formset_class(self):
+        # capture methods
         base_form = self.form_class
+        get_kwargs = self.get_form_kwargs
 
         class UserAwareForm(base_form):
             def __init__(self, *args, **form_kwargs):
-                form_kwargs = self.get_form_kwargs(**form_kwargs)
+                form_kwargs = get_kwargs(**form_kwargs) # using captured closure
                 super().__init__(*args, **form_kwargs)
 
         return UserAwareForm
