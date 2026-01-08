e6781ca78a741f031a6a39ea98436cc3fe9c5e1f
nezinomas <nezinomas@gmail.com>
2026-01-08 12:47:09 +0200

    Refactore extracted method

--- project/expenses/services/model_services.py
+++ project/expenses/services/model_services.py
@@ -2,14 +2,27 @@ from datetime import date, timedelta
 from typing import cast
 
 from dateutil.relativedelta import relativedelta
-from django.db.models import Case, Count, F, Q, Sum, Value, When
+from django.db.models import (
+    BooleanField,
+    Case,
+    CharField,
+    Count,
+    F,
+    Func,
+    Q,
+    Sum,
+    Value,
+    When,
+)
 from django.db.models.functions import (
+    Cast,
     Concat,
     ExtractYear,
     TruncDay,
     TruncMonth,
     TruncYear,
 )
+from django.urls import reverse
 
 from ...users.models import User
 from .. import managers, models
@@ -181,4 +194,72 @@ class ExpenseModelService:
             .values("expense_type")
             .annotate(sum=Sum("price"))
             .values("sum", title=F("expense_type__title"))
+        )
+
+    def expenses_list(self, qs):
+        """
+        Accepts ANY filtered queryset (by year, month, or search)
+        and applies the high-performance formatting, annotations, and .values().
+        """
+
+        # 1. Calculate URL patterns (Dynamic Annotation Strategy)
+        dummy_id = 0
+        update_pattern = reverse("expenses:update", args=[dummy_id])
+        delete_pattern = reverse("expenses:delete", args=[dummy_id])
+
+        # Split patterns to get prefix/suffix
+        u_prefix, u_suffix = update_pattern.split(str(dummy_id))
+        d_prefix, d_suffix = delete_pattern.split(str(dummy_id))
+
+        # 2. Apply all Annotations & Optimizations to the incoming QS
+        return (
+            qs.annotate(
+                # Database-level Price Formatting (MariaDB/MySQL)
+                price_str=Func(
+                    F("price") / Value(100.0),
+                    Value(2),
+                    Value("lt_LT"),
+                    function="FORMAT",
+                    output_field=CharField(),
+                ),
+                # PDF detection
+                is_pdf=Case(
+                    When(attachment__endswith=".pdf", then=Value(True)),
+                    default=Value(False),
+                    output_field=BooleanField(),
+                ),
+                # Grouping for "If Month Changed" logic
+                month_group=TruncMonth("date"),
+                # Fast URL Construction
+                url_update=Concat(
+                    Value(u_prefix),
+                    Cast("id", output_field=CharField()),
+                    Value(u_suffix),
+                    output_field=CharField(),
+                ),
+                url_delete=Concat(
+                    Value(d_prefix),
+                    Cast("id", output_field=CharField()),
+                    Value(d_suffix),
+                    output_field=CharField(),
+                ),
+            )
+            .order_by("-date", "expense_type__title", F("expense_name__title").asc())
+            .values(
+                "id",
+                "date",
+                "account__title",
+                "expense_type__pk",
+                "expense_type__title",
+                "expense_name__title",
+                "quantity",
+                "remark",
+                "attachment",
+                "exception",
+                "price_str",
+                "is_pdf",
+                "month_group",
+                "url_update",
+                "url_delete",
+            )
         )
--- project/expenses/views/expenses.py
+++ project/expenses/views/expenses.py
@@ -1,8 +1,6 @@
 from datetime import datetime
 from typing import Any, cast
 
-from django.db.models import BooleanField, Case, CharField, F, Value, When
-from django.db.models.functions import Cast, Concat, TruncMonth
 from django.template.loader import render_to_string
 from django.urls import reverse, reverse_lazy
 from django.utils.translation import gettext_lazy as _
@@ -45,63 +43,12 @@ class Lists(GetMonthMixin, ListViewMixin):
         month = self.get_month()
         user = cast(User, self.request.user)
         year = cast(int, user.year)
-        qs = ExpenseModelService(user).year(year)
-
+        service = ExpenseModelService(user)
+        qs = service.year(year)
         if month in range(1, 13):
             qs = qs.filter(date__month=month)
 
-        # 1. Dummy URL using a unique placeholder ID
-        dummy_id = 0
-
-        update_pattern = reverse("expenses:update", args=[dummy_id])
-        delete_pattern = reverse("expenses:delete", args=[dummy_id])
-
-        # 2. Split the URL around the ID
-        # prefix becomes "/expenses/update/"
-        # suffix becomes "/"
-        u_prefix, u_suffix = update_pattern.split(str(dummy_id))
-        d_prefix, d_suffix = delete_pattern.split(str(dummy_id))
-
-        return (
-            qs.annotate(
-                is_pdf=Case(
-                    When(attachment__endswith='.pdf', then=Value(True)),
-                    default=Value(False),
-                    output_field=BooleanField()
-                ),
-                month_group=TruncMonth("date"),
-                url_update=Concat(
-                    Value(u_prefix),
-                    Cast("id", output_field=CharField()),
-                    Value(u_suffix),
-                    output_field=CharField(),
-                ),
-                url_delete=Concat(
-                    Value(d_prefix),
-                    Cast("id", output_field=CharField()),
-                    Value(d_suffix),
-                    output_field=CharField(),
-                ),
-            )
-            .order_by("-date", "expense_type__title", F("expense_name__title").asc())
-            .values(
-                "id",
-                "date",
-                "account__title",
-                "expense_type__pk",
-                "expense_type__title",
-                "expense_name__title",
-                "price",
-                "quantity",
-                "remark",
-                "attachment",
-                "exception",
-                "is_pdf",
-                "month_group",
-                "url_update",
-                "url_delete",
-            )
-        )
+        return service.expenses_list(qs)
 
     def get_context_data(self, **kwargs):
         month = self.get_month()
@@ -115,8 +62,8 @@ class Lists(GetMonthMixin, ListViewMixin):
             }
 
         edit_col = render_to_string("cotton/td_edit.html", {"url": "[[url]]"})
-
         delete_col = render_to_string("cotton/td_delete.html", {"url": "[[url]]"})
+
         context = {"notice": notice, "edit_col": edit_col, "delete_col": delete_col}
         return super().get_context_data(**kwargs) | context
 
