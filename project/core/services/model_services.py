5b929d020e77cbd15682f5ca2f3dd4e8e75f2dfc
nezinomas <nezinomas@gmail.com>
2026-01-12 14:03:08 +0200

    Abstract BaseModelService class

---
+++ project/core/services/model_services.py
@@ -0,0 +1,29 @@
+from abc import ABC, abstractmethod
+from typing import Generic, TypeVar
+
+from ...users.models import User
+
+_QS = TypeVar("_QS")
+
+
+class BaseModelService(ABC, Generic[_QS]):
+    objects: _QS
+
+    def __init__(self, user: User):
+        if not user:
+            raise ValueError("User required")
+
+        if not user.is_authenticated:
+            raise ValueError("Authenticated user required")
+
+        self.user = user
+        self.objects = self.get_queryset(user)
+
+    @abstractmethod
+    def get_queryset(self, user: User) -> _QS: ...
+
+    @abstractmethod
+    def year(self, year: int): ...
+
+    @abstractmethod
+    def items(self): ...
