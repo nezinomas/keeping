cde313c634195d0b451b561ffa2da981345ed19d
nezinomas <nezinomas@gmail.com>
2026-03-31 10:43:12 +0300

    Rename template tag

--- project/drinks/tests/templatetags/test_templatetags.py
+++ project/drinks/tests/templatetags/test_templatetags.py
@@ -1,6 +1,6 @@
-from ...templatetags.drinks_templatetags import convert
+from ...templatetags.drinks_templatetags import convert_to_quantity
 
 
-def test_convert():
-    actual = convert(2.5, "beer")
+def test_convert_to_quantity():
+    actual = convert_to_quantity(2.5, "beer")
     assert actual == 1
