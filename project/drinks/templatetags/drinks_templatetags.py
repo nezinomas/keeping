1dd5d42bcf81ec059439707f210581e10c648fdd
nezinomas <nezinomas@gmail.com>
2026-03-31 10:49:06 +0300

    New templatetag

--- project/drinks/templatetags/drinks_templatetags.py
+++ project/drinks/templatetags/drinks_templatetags.py
@@ -6,6 +6,11 @@ register = template.Library()
 
 
 @register.filter
-def convert(stdav: float, drink_type: str):
+def convert_to_quantity(stdav: float, drink_type: str):
     node = DrinksOptions(drink_type).get_node()
     return stdav / node.get("stdav", 1)
+
+
+@register.filter
+def convert_to_ml(stdav: float, drink_type: str):
+    return DrinksOptions(drink_type).stdav_to_ml(stdav, drink_type)
