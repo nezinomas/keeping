86e2f5909f62f9ce73537b425058e624d7d01369
nezinomas <nezinomas@gmail.com>
2026-04-02 13:16:26 +0300

    Extract mixin

--- project/core/mixins/views.py
+++ project/core/mixins/views.py
@@ -1,4 +1,3 @@
-from django.core.exceptions import ImproperlyConfigured
 from django.urls import reverse_lazy
 from django.utils.translation import gettext_lazy as _
 from vanilla import (
@@ -13,6 +12,7 @@ from vanilla import (
 
 from .create_update import CreateUpdateMixin
 from .delete import DeleteMixin
+from .queryset import GetQuerysetMixin
 from .search import SearchMixin
 
 
@@ -41,18 +41,7 @@ class AddUserToKwargsMixin:
         return UserAwareForm
 
 
-class GetQuerysetMixin:
-    object = None
 
-    def get_queryset(self):
-        # 1. Protect against developer error (forgetting to set the model)
-        if self.model is None:
-            raise ImproperlyConfigured(
-                f"{self.__class__.__name__} is missing a model definition. "
-                f"Define {self.__class__.__name__}.model."
-            )
-
-        return self.model.objects.related(self.request.user)
 
 
 # -------------------------------------------------------------------------------------
