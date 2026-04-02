941739ec6eee3ce4920b0ed16ebdbd0e821388ee
nezinomas <nezinomas@gmail.com>
2026-04-02 13:17:45 +0300

    Extract mixin

--- project/core/mixins/views.py
+++ project/core/mixins/views.py
@@ -12,41 +12,11 @@ from vanilla import (
 
 from .create_update import CreateUpdateMixin
 from .delete import DeleteMixin
+from .kwargs import AddUserToKwargsMixin
 from .queryset import GetQuerysetMixin
 from .search import SearchMixin
 
 
-# -------------------------------------------------------------------------------------
-#                                                                                Mixins
-# -------------------------------------------------------------------------------------
-class AddUserToKwargsMixin:
-    def get_form_kwargs(self, **kwargs):
-        kwargs["user"] = self.request.user
-        return kwargs
-
-    def get_form(self, data=None, files=None, **kwargs):
-        kwargs = self.get_form_kwargs(**kwargs)
-        return self.form_class(data, files, **kwargs)
-
-    def get_formset_class(self):
-        # capture methods
-        base_form = self.form_class
-        get_kwargs = self.get_form_kwargs
-
-        class UserAwareForm(base_form):
-            def __init__(self, *args, **form_kwargs):
-                form_kwargs = get_kwargs(**form_kwargs)  # using captured closure
-                super().__init__(*args, **form_kwargs)
-
-        return UserAwareForm
-
-
-
-
-
-# -------------------------------------------------------------------------------------
-#                                                                          Views Mixins
-# -------------------------------------------------------------------------------------
 class CreateViewMixin(
     AddUserToKwargsMixin, GetQuerysetMixin, CreateUpdateMixin, CreateView
 ):
