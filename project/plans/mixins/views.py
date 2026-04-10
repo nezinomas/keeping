313af2ea1730f184223e3610890bf7f017703c10
nezinomas <nezinomas@gmail.com>
2026-04-10 16:34:34 +0300

    Code cleanup

--- project/plans/mixins/views.py
+++ project/plans/mixins/views.py
@@ -9,26 +9,20 @@ class CssClassMixin:
 
 class TallRecordMixin:
     def get_tall_queryset(self):
-        # 1. Get the base queryset for this user and service
         qs = self.service_class(self.request.user).items()
 
-        # 2. Filter using exactly the kwargs provided in the URL!
+        # Filter using exactly the kwargs provided in the URL
         # If the URL is incomes/1999/5/, self.kwargs is {'year': 1999, 'income_type_id': 5}
         # qs.filter(**self.kwargs) translates instantly to qs.filter(year=1999, income_type_id=5)
-        q = qs.filter(**self.kwargs)
-        print(f"--------------------------->{q=}\n")
-        return q
+        return qs.filter(**self.kwargs)
 
 
 class TallUpdateMixin(TallRecordMixin):
-    """DRY Mixin for UpdateViews using Tall tables."""
-
     def get_object(self, queryset=None):
-        # We just need ONE row to act as the "Instance" for the Proxy Form to read
-        obj = self.get_tall_queryset().first()
-        if not obj:
-            raise Http404(_("No plans found."))
-        return obj
+        if obj := self.get_tall_queryset().first():
+            return obj
+
+        raise Http404(_("No plans found."))
 
     def url(self):
         if not self.object:
@@ -53,5 +47,4 @@ class TallDeleteMixin(TallRecordMixin):
         if not self.object:
             return None
 
-        # Same ultra-simple logic here
         return reverse_lazy(f"plans:{self.url_name}", kwargs=self.kwargs)
