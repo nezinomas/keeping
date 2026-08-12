d608aa5a70515ebce8006f345c5f4fef5077b794
Audrius <nezinomas@gmail.com>
2026-08-12 21:20:06 +0300

    users: drink_type takes the declared choices

--- project/users/tests/test_models.py
+++ project/users/tests/test_models.py
@@ -1,10 +1,25 @@
 import pytest
+from django.core.exceptions import ValidationError
 
+from ...drinks.lib.drink_types import DrinkType
 from ..models import User
 from ..services.model_services import UserModelService
 from .factories import UserFactory
 
 pytestmark = pytest.mark.django_db
+
+
+def test_drink_type_offers_the_declared_types():
+    field = User._meta.get_field("drink_type")
+
+    assert field.choices == DrinkType.choices
+
+
+def test_an_undeclared_drink_type_does_not_validate():
+    field = User._meta.get_field("drink_type")
+
+    with pytest.raises(ValidationError):
+        field.clean("grog", UserFactory.build())
 
 
 def test_user_str():
