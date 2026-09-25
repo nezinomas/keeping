7944503e46ae2f449f3a1ee58ef45be12df6f3bb
Audrius <nezinomas@gmail.com>
2026-09-25 20:49:47 +0300

    Remove the unused core exceptions module

--- project/core/exceptions.py
+++
@@ -1,6 +0,0 @@
-class NoDataFoundError(Exception):
-    """Exception raised when no data found"""
-
-
-class MethodInvalidError(Exception):
-    """Exception raised when something wrong with method"""
